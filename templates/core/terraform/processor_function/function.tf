9281932aa20f0129cf46da3e681d9946d90fba9c
Denis Cepun <denis.cepun@microsoft.com>
2021-05-24 15:48:01 +0300

    processor function initial commit

---
+++ templates/core/terraform/processor_function/function.tf
@@ -0,0 +1,16 @@
+resource "azurerm_function_app" "procesorfunction" {
+  name                       = "processor-func-${var.resource_name_prefix}-${var.environment}-${var.tre_id}"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  app_service_plan_id        = var.app_service_plan_id
+  storage_account_name       = var.storage_account_name
+  storage_account_access_key = var.storage_account_access_key
+  version = "~3"
+  
+  app_settings = {
+        https_only = true
+        FUNCTIONS_WORKER_RUNTIME = "node"
+        WEBSITE_NODE_DEFAULT_VERSION = "~14"
+        FUNCTION_APP_EDIT_MODE = "readonly"
+    }
+}
