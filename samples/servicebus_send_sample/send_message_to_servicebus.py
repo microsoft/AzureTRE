567ede7a9711722634f342d55f1b8b712d9a238d
Denis Cepun <denis.cepun@microsoft.com>
2021-05-25 09:29:56 +0300

    Create resource processor function app skeleton (#102)
    Creates resource processor function app skeleton, service bus, storage and function app

---
+++ samples/servicebus_send_sample/send_message_to_servicebus.py
@@ -0,0 +1,24 @@
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+
+
+CONNECTION_STR = ""
+QUEUE_NAME = "workspacequeue"
+
+
+def send_single_message(sender):
+    message = ServiceBusMessage("Test Message")
+    sender.send_messages(message)
+    print("Sent a test message")
+
+
+def main():
+    servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
+
+    with servicebus_client:
+        sender = servicebus_client.get_queue_sender(queue_name=QUEUE_NAME)
+        with sender:
+            send_single_message(sender)
+
+
+if __name__ == "__main__":
+    main()
