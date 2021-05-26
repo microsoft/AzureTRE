734531c3847a70f75f2f812d5c9f27fc27701dcf
Tomi Paananen <tomi.paananen@microsoft.com>
2021-05-26 17:14:44 +0300

    Adds the first iteration of the vanilla workspace bundle replacing the workspace base template

---
+++ workspaces/vanilla/terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "azure_tenant_id" {
+  type = string
+}
+
+variable "azure_subscription_id" {
+  type = string
+}
+
+variable "azure_service_principal_client_id" {
+  type = string
+}
+
+variable "azure_service_principal_password" {
+  type = string
+}
+
+variable "core_id" {
+  type        = string
+  description = "Unique 4-digit TRE core ID"
+}
+
+variable "core_name" {
+  type = string
+  description = "TRE core name prefix with environment identifier"
+  default = "tre-dev"
+}
+
+variable "workspace_id" {
+  type        = string
+  description = "Unique 4-digit workspace ID"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure location (region) for deployment of core TRE services"
+}
+
+variable "address_space" {
+  type        = string
+  description = "VNet address space for the workspace services"
+}
