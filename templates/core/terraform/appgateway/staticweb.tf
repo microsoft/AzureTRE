18eb065dda637e49b38f6f88e8a7393434b182dd
Stephen Askew <2727893+askew@users.noreply.github.com>
2021-05-28 11:17:50 +0100

    Setup Application Gateway to use SSL (#103)
    Updates to application gateway configuration and scripts to acquire a Let's Encrypt certificate.

---
+++ templates/core/terraform/appgateway/staticweb.tf
@@ -0,0 +1,27 @@
+data "azurerm_client_config" "deployer" {}
+
+resource "azurerm_storage_account" "staticweb" {
+    name = local.staticweb_storage_name
+    resource_group_name = var.resource_group_name
+    location = var.location
+    account_kind = "StorageV2"
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    enable_https_traffic_only = true
+    allow_blob_public_access = false
+    static_website {
+      index_document = "index.html"
+      error_404_document = "404.html"
+    }
+    tags = {
+        environment = var.environment
+    }
+}
+
+# Assign the identity deploying data contibutor rights.
+# Needed to upload content to static web.
+resource "azurerm_role_assignment" "stgwriter" {
+  scope                = azurerm_storage_account.staticweb.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_client_config.deployer.object_id
+}
---
+++ templates/core/terraform/keyvault/output.tf
@@ -0,0 +1,7 @@
+output "keyvault_id" {
+  value = azurerm_key_vault.kv.id
+}
+
+output "keyvault_name" {
+  value = azurerm_key_vault.kv.name
+}
