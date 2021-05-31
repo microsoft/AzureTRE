09cf14610f1ed7d75a6a8cd8f0ca8a5360db4d6b
Denis Cepun <denis.cepun@microsoft.com>
2021-05-31 21:38:53 +0300

    clean-up init

--- processor_function/createNewWorkspace/__init__.py
+++ processor_function/createNewWorkspace/__init__.py
@@ -8,8 +8,6 @@ from shared import cnab_builder
 
 
 def main(msg: func.ServiceBusMessage):
-    logging.info('Python ServiceBus queue trigger received message: %s',
-                 msg.get_body().decode('utf-8'))
     
     cnab_builder.message = json.loads(msg.get_body().decode('utf-8'))
     
