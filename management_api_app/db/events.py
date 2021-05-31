77c402859c816e1e727ea96019c8105e6b96e0a6
sachin kundu <sakundu@microsoft.com>
2021-05-31 11:47:07 +0000

    fix review comments

--- management_api_app/db/events.py
+++ management_api_app/db/events.py
@@ -1,5 +1,6 @@
-import uuid
+import json
 import logging
+from pathlib import Path
 
 from azure.cosmos import CosmosClient, PartitionKey, DatabaseProxy
 from fastapi import FastAPI
@@ -11,7 +12,7 @@ async def connect_to_db(app: FastAPI) -> None:
     logging.debug(f"Connecting to {config.STATE_STORE_ENDPOINT}")
 
     try:
-        cosmos_client = CosmosClient(config.STATE_STORE_ENDPOINT, config.STATE_STORE_KEY, connection_verify=False)
+        cosmos_client = CosmosClient(config.STATE_STORE_ENDPOINT, config.STATE_STORE_KEY)
         app.state.cosmos_client = cosmos_client
         logging.debug("Connection established")
     except Exception as e:
@@ -20,47 +21,38 @@ async def connect_to_db(app: FastAPI) -> None:
 # Bootstrapping is temporary while the API does not have a register spec api implemented.
 
 
-async def create_bundle_registry(database: DatabaseProxy):
+async def create_resource_specs(database: DatabaseProxy):
     """
-    Creates a bundle spec container if one does not exist and populate it with a canonical spec.
+    Creates a resource spec container if one does not exist and populate it with a canonical spec.
     :param database: DatabaseProxy for STATE_STORE_DATABASE
     :returns: None
     """
-    bundle_spec = {
-        "url": "blah/blah:v1",
-        "id": str(uuid.uuid4()),
-        "fields": [
-            {"name": "location", "required": True},
-            {"name": "tre_id", "required": True},
-            {"name": "workspace_id", "required": True},
-            {"name": "address_space", "required": True}
-        ]
-    }
+    resource_spec_file = Path('db') / "bootstrapping_data" / "resource_specs.json"
+    with open(str(resource_spec_file.resolve())) as f:
+        resource_specs = json.load(f)
+        container_name = config.STATE_STORE_BUNDLE_SPECS_CONTAINER
 
-    bundle_specs = [bundle_spec]
-    container_name = config.STATE_STORE_BUNDLE_SPECS_CONTAINER
+        containers = list(database.query_containers(
+            {
+                "query": "SELECT * FROM r WHERE r.id=@id",
+                "parameters": [
+                    {"name": "@id", "value": container_name}
+                ]
+            }
+        ))
 
-    containers = list(database.query_containers(
-        {
-            "query": "SELECT * FROM r WHERE r.id=@id",
-            "parameters": [
-                {"name": "@id", "value": container_name}
-            ]
-        }
-    ))
-
-    if not len(containers):
-        container = database.create_container_if_not_exists(
-            id=container_name,
-            partition_key=PartitionKey(path="/url"),
-            offer_throughput=400
-        )
-        for spec in bundle_specs:
-            container.create_item(body=spec)
+        if not len(containers):
+            container = database.create_container_if_not_exists(
+                id=container_name,
+                partition_key=PartitionKey(path="/id"),
+                offer_throughput=400
+            )
+            for spec in resource_specs["specs"]:
+                container.create_item(body=spec)
 
 
 async def bootstrap_database(app: FastAPI) -> None:
     client: CosmosClient = app.state.cosmos_client
     if client:
         database_proxy = client.create_database_if_not_exists(id=config.STATE_STORE_DATABASE)
-        await create_bundle_registry(database_proxy)
+        await create_resource_specs(database_proxy)
