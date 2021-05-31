d88852ddb1e7a67be45a23d92439f1c4481e1397
Tomi Paananen <tomi.paananen@microsoft.com>
2021-05-31 13:49:05 +0300

    Adds ServiceBus class to management API

--- management_api_app/core/config.py
+++ management_api_app/core/config.py
@@ -3,14 +3,18 @@ from starlette.config import Config
 
 config = Config(".env")
 
-# api settings
+# API settings
 API_PREFIX = "/api"
 PROJECT_NAME: str = config("PROJECT_NAME", default="Azure TRE API")
 DEBUG: bool = config("DEBUG", cast=bool, default=False)
 VERSION = "0.0.0"
 
 # State store configuration
-STATE_STORE_ENDPOINT: str = config("STATE_STORE_ENDPOINT", default="")      # cosmos db endpoint
-STATE_STORE_KEY: str = config("STATE_STORE_KEY", default="")                # cosmos db access key
+STATE_STORE_ENDPOINT: str = config("STATE_STORE_ENDPOINT", default="")      # Cosmos DB endpoint
+STATE_STORE_KEY: str = config("STATE_STORE_KEY", default="")                # Cosmos DB access key
 STATE_STORE_DATABASE = "AzureTRE"
 STATE_STORE_RESOURCES_CONTAINER = "Resources"
+
+# Service bus configuration
+SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE: str = config("SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE", default="")
+SERVICE_BUS_RESOURCE_REQUEST_QUEUE: str = config("SERVICE_BUS_RESOURCE_REQUEST_QUEUE", default="")
---
+++ management_api_app/service_bus/service_bus.py
@@ -0,0 +1,29 @@
+from azure.identity import EnvironmentCredential
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from core import config
+
+class ServiceBus():
+    """
+    Implements methods to operate the service bus in the core infrastructure of Azure TRE.
+    """
+
+    async def send_resource_request_message(self, resource_request_message: str):
+        """
+        Sends the given message to the resource request queue.
+
+        EnvironmentCredentials class requires environment variables AZURE_TENANT_ID, AZURE_CLIENT_ID, and
+        AZURE_CLIENT_SECRET specified.
+
+        :param resource_request_message: The message to send.
+        :type resource_request_message: str
+        """
+        credential = EnvironmentCredential()
+        service_bus_client = ServiceBusClient(config.SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE, credential)
+
+        async with service_bus_client:
+            sender = service_bus_client.get_queue_sender(queue_name=config.SERVICE_BUS_RESOURCE_REQUEST_QUEUE)
+
+            async with sender:
+                await sender.send_messages(ServiceBusMessage(resource_request_message))
+
+        await credential.close()
