db7277a4212ad21ce64026f83dd2d6bd73b9bb0b
Tomi Paananen <tomi.paananen@microsoft.com>
2021-06-01 12:13:31 +0300

    Adds service bus message sending to create workspace API

--- management_api_app/api/routes/workspaces.py
+++ management_api_app/api/routes/workspaces.py
@@ -7,6 +7,7 @@ from db.repositories.workspaces import WorkspaceRepository
 from models.domain.resource import Resource
 from models.schemas.resource import ResourcesInList, ResourceInResponse, ResourceInCreate
 from resources import strings
+from service_bus import ServiceBus
 
 
 router = APIRouter()
@@ -23,11 +24,22 @@ async def retrieve_active_workspaces(workspace_repo: WorkspaceRepository = Depen
 
 @router.post("/workspaces", status_code=status.HTTP_202_ACCEPTED, response_model=ResourceInResponse, name=strings.API_CREATE_WORKSPACE)
 async def create_workspace(workspace_create: ResourceInCreate, workspace_repo: WorkspaceRepository = Depends(get_repository(WorkspaceRepository))) -> ResourceInResponse:
+    workspace = {}
+
     try:
         workspace = workspace_repo.create_workspace(workspace_create)
-        return ResourceInResponse(resource=workspace)
+        service_bus = ServiceBus()
+        service_bus.send_resource_request_message(workspace.dict())
     except UnableToAccessDatabase:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=strings.STATE_STORE_ENDPOINT_NOT_RESPONDING)
+
+    try:
+        service_bus = ServiceBus()
+        service_bus.send_resource_request_message(str(workspace.dict()))
+    except:
+        raise
+
+    return ResourceInResponse(resource=workspace)
 
 
 @router.get("/workspaces/{workspace_id}", response_model=ResourceInResponse, name=strings.API_GET_WORKSPACE_BY_ID)
