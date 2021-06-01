950d60ae583174103bccad5f73d04f61c9875cb4
Tess Ferrandez <tess.ferrandez@microsoft.com>
2021-06-01 15:23:47 +0200

    remove the error checks for db - already handled by the get_repository dependency

--- management_api_app/api/routes/workspaces.py
+++ management_api_app/api/routes/workspaces.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from starlette import status
 
 from api.dependencies.workspaces import get_repository, get_workspace_by_workspace_id_from_path
-from db.errors import UnableToAccessDatabase
 from db.repositories.workspaces import WorkspaceRepository
 from models.domain.resource import Resource
 from models.schemas.resource import ResourcesInList, ResourceInResponse, ResourceInCreate
@@ -14,20 +13,14 @@ router = APIRouter()
 
 @router.get("/workspaces", response_model=ResourcesInList, name=strings.API_GET_ALL_WORKSPACES)
 async def retrieve_active_workspaces(workspace_repo: WorkspaceRepository = Depends(get_repository(WorkspaceRepository))) -> ResourcesInList:
-    try:
-        workspaces = workspace_repo.get_all_active_workspaces()
-        return ResourcesInList(resources=workspaces)
-    except UnableToAccessDatabase:
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=strings.STATE_STORE_ENDPOINT_NOT_RESPONDING)
+    workspaces = workspace_repo.get_all_active_workspaces()
+    return ResourcesInList(resources=workspaces)
 
 
 @router.post("/workspaces", status_code=status.HTTP_202_ACCEPTED, response_model=ResourceInResponse, name=strings.API_CREATE_WORKSPACE)
 async def create_workspace(workspace_create: ResourceInCreate, workspace_repo: WorkspaceRepository = Depends(get_repository(WorkspaceRepository))) -> ResourceInResponse:
-    try:
-        workspace = workspace_repo.create_workspace(workspace_create)
-        return ResourceInResponse(resource=workspace)
-    except UnableToAccessDatabase:
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=strings.STATE_STORE_ENDPOINT_NOT_RESPONDING)
+    workspace = workspace_repo.create_workspace(workspace_create)
+    return ResourceInResponse(resource=workspace)
 
 
 @router.get("/workspaces/{workspace_id}", response_model=ResourceInResponse, name=strings.API_GET_WORKSPACE_BY_ID)
