9e62ac3a215550ff2a5e6925e9c45536188d9617
Marcus Robinson <marrobi@microsoft.com>
2021-06-02 11:42:40 +0100

    add -o tsv to az acr command (#163)

--- templates/core/terraform/deploy.sh
+++ templates/core/terraform/deploy.sh
@@ -12,7 +12,7 @@ TRE_BACKEND
 ACR_NAME="${TF_VAR_resource_name_prefix}acr"
 export TF_VAR_docker_registry_server="${TF_VAR_resource_name_prefix}acr.azurecr.io"
 export TF_VAR_docker_registry_username="${TF_VAR_resource_name_prefix}acr"
-export TF_VAR_docker_registry_password=$(az acr credential show --name ${TF_VAR_resource_name_prefix}acr --query passwords[0].value | sed 's/"//g')
+export TF_VAR_docker_registry_password=$(az acr credential show --name ${TF_VAR_resource_name_prefix}acr --query passwords[0].value -o tsv | sed 's/"//g')
 export TF_VAR_management_api_image_tag=$TF_VAR_image_tag
 
 terraform init -input=false -backend=true -reconfigure
