293cc4f0d6286eb8f8f47803b569dd41b0740228
Marcus Robinson <marrobi@microsoft.com>
2021-06-03 20:53:33 +0000

    Fix issues in destroy.

--- templates/core/terraform/destroy.sh
+++ templates/core/terraform/destroy.sh
@@ -1,6 +1,6 @@
-export TF_VAR_docker_registry_server=$TF_VAR_acr_name
+export TF_VAR_docker_registry_server="$TF_VAR_acr_name.azurecr.io"
 export TF_VAR_docker_registry_username=$TF_VAR_acr_name
-export TF_VAR_docker_registry_password=$(az acr credential show --name ${TF_VAR_acr_name} -query passwords[0].value -o tsv | sed 's/"//g')
+export TF_VAR_docker_registry_password=$(az acr credential show --name ${TF_VAR_acr_name} --query passwords[0].value -o tsv | sed 's/"//g')
 export TF_VAR_management_api_image_tag=$TF_VAR_image_tag
 
 terraform destroy -auto-approve
