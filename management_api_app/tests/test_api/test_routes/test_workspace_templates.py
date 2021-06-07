460266f06ccf82ae96d97d26c98873222ad8391e
Tess Ferrandez <tess.ferrandez@microsoft.com>
2021-06-07 08:21:56 +0200

    fix test lists

--- management_api_app/tests/test_api/test_routes/test_workspace_templates.py
+++ management_api_app/tests/test_api/test_routes/test_workspace_templates.py
@@ -13,8 +13,12 @@ pytestmark = pytest.mark.asyncio
 # [GET] /workspace-templates
 @patch("api.routes.workspace_templates.WorkspaceTemplateRepository.get_workspace_template_names")
 async def test_workspace_templates_returns_template_names(get_workspace_template_names_mock, app: FastAPI, client: AsyncClient):
-    get_workspace_template_names_mock.return_value = ["template1", "template2"]
+    expected_template_names = ["template1", "template2"]
+    get_workspace_template_names_mock.return_value = expected_template_names
 
     response = await client.get(app.url_path_for(strings.API_GET_WORKSPACE_TEMPLATES))
 
-    assert response.json()["templateNames"] == ["template1", "template2"]
+    actual_template_names = response.json()["templateNames"]
+    assert len(actual_template_names) == len(expected_template_names)
+    for name in expected_template_names:
+        assert name in actual_template_names
