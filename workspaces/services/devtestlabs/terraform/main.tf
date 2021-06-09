f203eb5a747dc12fcc4ac148e1d620a003b4741a
Marcus Robinson <marrobi@microsoft.com>
2021-06-09 21:34:06 +0000

    remove comments, correct param label

--- workspaces/services/devtestlabs/terraform/main.tf
+++ workspaces/services/devtestlabs/terraform/main.tf
@@ -34,15 +34,12 @@ data "local_file" "dtl-vnet" {
   filename = "${path.module}/dtl.json"
 }
 
-# need to add existing VNET
 resource "azurerm_template_deployment" "dtl" {
   name                = "dpl-${local.service_resource_name_suffix}"
   resource_group_name = data.azurerm_resource_group.ws.name
 
   template_body = data.local_file.dtl-vnet.content
 
-
-  # these key-value pairs are passed into the ARM Template's `parameters` block
   parameters = {
     "dtlName"  = "dtl-${local.service_resource_name_suffix}"
     "vnetId"   = data.azurerm_virtual_network.ws.id
