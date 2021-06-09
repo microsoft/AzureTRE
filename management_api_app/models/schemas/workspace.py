baf9f986e8a2a4f71ddfafb042c2153c1078f039
Tess Ferrandez <tferrand@microsoft.com>
2021-06-09 11:16:59 +0200

    fix location->azure_location (#225)

--- management_api_app/models/schemas/workspace.py
+++ management_api_app/models/schemas/workspace.py
@@ -12,7 +12,7 @@ def get_sample_workspace(workspace_id: str, spec_workspace_id: str = "0001") -> 
         "resourceTemplateName": "tre-workspace-vanilla",
         "resourceTemplateVersion": "0.1.0",
         "resourceTemplateParameters": {
-            "location": "westeurope",
+            "azure_location": "westeurope",
             "workspace_id": spec_workspace_id,
             "tre_id": "mytre-dev-1234",
             "address_space": "10.2.1.0/24"
