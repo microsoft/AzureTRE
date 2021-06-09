1ec5c02f050219c825ecdf502f42fdeef9b1a7f7
Marcus Robinson <marrobi@microsoft.com>
2021-06-09 10:12:41 +0100

    Create Workpace Bundle with Azure ML (#184)

---
+++ workspaces/services/azureml/terraform/keyvault/main.tf
@@ -0,0 +1,61 @@
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "ws" {
+  name = "rg-${var.tre_id}-ws-${var.workspace_id}"
+}
+
+data "azurerm_virtual_network" "ws" {
+  name                = "vnet-${var.tre_id}-ws-${var.workspace_id}"
+  resource_group_name = data.azurerm_resource_group.ws.name
+}
+
+data "azurerm_subnet" "services" {
+  name                 = "ServicesSubnet"
+  virtual_network_name = data.azurerm_virtual_network.ws.name
+  resource_group_name  = data.azurerm_virtual_network.ws.resource_group_name
+}
+
+locals {
+  service_resource_name_suffix = "${var.tre_id}-ws-${var.workspace_id}-svc-${var.service_id}"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                     = "kv-${var.tre_id}${var.workspace_id}${var.service_id}"
+  location                 = data.azurerm_resource_group.ws.location
+  resource_group_name      = data.azurerm_resource_group.ws.name
+  sku_name                 = "standard"
+  purge_protection_enabled = true
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+}
+
+resource "azurerm_private_dns_zone" "vaultcore" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = data.azurerm_resource_group.ws.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vaultcorelink" {
+  name                  = "vaultcorelink-${local.service_resource_name_suffix}"
+  resource_group_name   = data.azurerm_resource_group.ws.name
+  private_dns_zone_name = azurerm_private_dns_zone.vaultcore.name
+  virtual_network_id    = data.azurerm_virtual_network.ws.id
+}
+
+resource "azurerm_private_endpoint" "kvpe" {
+  name                = "kvpe-${local.service_resource_name_suffix}"
+  location            = data.azurerm_resource_group.ws.location
+  resource_group_name = data.azurerm_resource_group.ws.name
+  subnet_id           = data.azurerm_subnet.services.id
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.vaultcore.id]
+  }
+
+  private_service_connection {
+    name                           = "kvpescv-${local.service_resource_name_suffix}"
+    private_connection_resource_id = azurerm_key_vault.kv.id
+    is_manual_connection           = false
+    subresource_names              = ["Vault"]
+  }
+}
