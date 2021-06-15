b656f4d700074f66f3a195cfb2770bfc0c3ec414
TessFerrandez <tferrand@microsoft.com>
2021-06-15 16:08:44 +0200

    static typing for workspaces repo

--- management_api_app/tests/test_db/test_repositories/test_workpaces_repository.py
+++ management_api_app/tests/test_db/test_repositories/test_workpaces_repository.py
@@ -25,7 +25,13 @@ def test_get_all_active_workspaces_calls_db_with_correct_query(cosmos_client_moc
 def test_get_workspace_by_id_calls_db_with_correct_query(cosmos_client_mock):
     workspace_repo = db.repositories.workspaces.WorkspaceRepository(cosmos_client_mock)
     workspace_id = uuid.uuid4()
-    workspace_repo.container.query_items = MagicMock(return_value=[{"id": str(workspace_id)}])
+    return_workspace = {
+        "id": str(workspace_id),
+        "resourceTemplateName": "some-template-name",
+        "resourceTemplateVersion": "1.0",
+        "deployment": {"status": Status.NotDeployed, "message": ""}
+    }
+    workspace_repo.container.query_items = MagicMock(return_value=[return_workspace])
     expected_query = f'SELECT * FROM c WHERE c.resourceType = "workspace" AND c.isDeleted = false AND c.id="{str(workspace_id)}"'
 
     workspace_repo.get_workspace_by_workspace_id(workspace_id)
