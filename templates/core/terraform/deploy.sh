a85b66ee321ae113e9c801a21a000e81a34c10e5
Marcus Robinson <marrobi@microsoft.com>
2021-06-15 21:22:56 +0000

    Fix acr_name missing underscore.

--- templates/core/terraform/deploy.sh
+++ templates/core/terraform/deploy.sh
@@ -9,9 +9,9 @@ terraform {
 }
 TRE_BACKEND
 
-export TF_VAR_docker_registry_server="$TF_VAR_acrname.azurecr.io"
-export TF_VAR_docker_registry_username=$TF_VAR_acrname
-export TF_VAR_docker_registry_password=$(az acr credential show --name ${TF_VAR_acrname} --query passwords[0].value -o tsv | sed 's/"//g')
+export TF_VAR_docker_registry_server="$TF_VAR_acr_name.azurecr.io"
+export TF_VAR_docker_registry_username=$TF_VAR_acr_name
+export TF_VAR_docker_registry_password=$(az acr credential show --name ${TF_VAR_acr_name} --query passwords[0].value -o tsv | sed 's/"//g')
 export TF_VAR_management_api_image_tag=$TF_VAR_management_api_image_tag
 
 terraform init -input=false -backend=true -reconfigure
