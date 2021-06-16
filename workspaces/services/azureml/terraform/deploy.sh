d373f1129fa3296ef44b34851ec4839007021a23
Marcus Robinson <marrobi@microsoft.com>
2021-06-16 14:59:55 +0100

    Error in tre_deploy, Set to use TF_VAR_ variables as purely a TF step (#269)

--- workspaces/services/azureml/terraform/deploy.sh
+++ workspaces/services/azureml/terraform/deploy.sh
@@ -4,7 +4,7 @@ terraform {
     resource_group_name  = "$TF_VAR_mgmt_resource_group_name"
     storage_account_name = "$TF_VAR_mgmt_storage_account_name"
     container_name       = "$TF_VAR_terraform_state_container_name"
-    key                  = "${TRE_ID}${WORKSPACE_ID}azureml"
+    key                  = "${TF_VAR_tre_id}${TF_VAR_workspace_id}azureml"
   }
 }
 TRE_BACKEND
