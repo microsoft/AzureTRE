62ee1b55ca11f06e7c0a42fe9428d33e61df873c
TessFerrandez <tferrand@microsoft.com>
2021-06-21 09:52:24 +0200

    change error string to be more pythonic

--- management_api_app/api/errors/generic_error.py
+++ management_api_app/api/errors/generic_error.py
@@ -9,11 +9,7 @@ from resources import strings
 
 async def generic_error_handler(_: Request, exception: Exception) -> PlainTextResponse:
     logging.debug("=====================================")
-    logging.debug("=====================================")
     logging.exception(exception)
     logging.debug("=====================================")
-    logging.debug("=====================================")
-    if config.DEBUG:
-        return PlainTextResponse(exception, status_code=500)
-    else:
-        return PlainTextResponse(strings.UNABLE_TO_PROCESS_REQUEST, status_code=500)
+    error_string = exception if config.DEBUG else strings.UNABLE_TO_PROCESS_REQUEST
+    return PlainTextResponse(error_string, status_code=500)
