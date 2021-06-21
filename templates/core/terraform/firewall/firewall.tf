ffe00a9ae6f8a0a99a6eb3ab359fd561af59200f
Marcus Robinson <marrobi@microsoft.com>
2021-06-21 15:41:49 +0100

    [Task] Add logging to firewall (#330)
    Fixes #328

--- templates/core/terraform/firewall/firewall.tf
+++ templates/core/terraform/firewall/firewall.tf
@@ -1,20 +1,58 @@
 resource "azurerm_public_ip" "fwpip" {
-  name                  = "pip-fw-${var.tre_id}"
-  resource_group_name   = var.resource_group_name
-  location              = var.location
-  allocation_method     = "Static"
-  sku                   = "Standard"
+  name                = "pip-fw-${var.tre_id}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_firewall" "fw" {
-  depends_on            = [azurerm_public_ip.fwpip]
-  name                  = "fw-${var.tre_id}"
-  resource_group_name   = var.resource_group_name
-  location              = var.location
+  depends_on          = [azurerm_public_ip.fwpip]
+  name                = "fw-${var.tre_id}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
   ip_configuration {
-    name        = "fw-ip-configuration"
-    subnet_id   = var.firewall_subnet
+    name                 = "fw-ip-configuration"
+    subnet_id            = var.firewall_subnet
     public_ip_address_id = azurerm_public_ip.fwpip.id
   }
 }
 
+data "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = "log-${var.tre_id}"
+  resource_group_name = var.resource_group_name
+}
+
+
+resource "azurerm_monitor_diagnostic_setting" "firewall" {
+  name                       = "diagnostics-firewall-${var.tre_id}"
+  target_resource_id         = azurerm_firewall.fw.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+  log {
+    category = "AzureFirewallApplicationRule"
+    enabled  = true
+
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  log {
+
+    category = "AzureFirewallNetworkRule"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+}
