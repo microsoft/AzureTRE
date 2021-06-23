47760664de6cc11b7e948ab432d5f5a825f0d54b
Marcus Robinson <marrobi@microsoft.com>
2021-06-23 09:47:03 +0100

    Add innereye deep learning workspace (#342)

---
+++ workspaces/services/innereye_deeplearning/terraform/locals.tf
@@ -0,0 +1,11 @@
+# Random unique id
+resource "random_string" "unique_id" {
+  length      = 4
+  min_numeric = 4
+}
+
+locals {
+  service_id                   = random_string.unique_id.result
+  service_resource_name_suffix = "${var.tre_id}-ws-${var.workspace_id}-svc-${local.service_id}"
+  aml_compute_id = substr("${var.tre_id}${var.workspace_id}${local.service_id}",-12,-1)
+}
