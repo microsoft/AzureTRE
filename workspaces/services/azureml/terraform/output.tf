2840007d8687b0f31711fa4c96b87cba01253bf9
Marcus Robinson <marrobi@microsoft.com>
2021-06-29 09:20:19 +0100

    Add ACR outputs from Azure ML

--- workspaces/services/azureml/terraform/output.tf
+++ workspaces/services/azureml/terraform/output.tf
@@ -9,3 +9,11 @@ output "computeinstance_name" {
 output "computecluster_name" {
     value = "cl-${local.service_resource_name_suffix}"
 }
+
+output "azureml_acr_id" {
+    value = module.acr.id
+}
+
+output "azureml_storage_account_id" {
+    value = module.storage.storage_account_id
+}
