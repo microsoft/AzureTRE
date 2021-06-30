c4785dbd16a446d5992f8c06e5b0ea6f63b8a299
Marcus Robinson <marrobi@microsoft.com>
2021-06-30 11:28:21 +0000

    inference deploys

--- workspaces/innereye_deeplearning_inference/install_service_devtestlabs.sh
+++ workspaces/innereye_deeplearning_inference/install_service_devtestlabs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-porter install tre-service-innereye-devtestlabs --reference "${ACR_NAME}.azurecr.io/tre-service-innereye-devtestlabs:v0.1.0" \
+porter install tre-service-innereye-devtestlabs --reference "${ACR_NAME}.azurecr.io/tre-service-devtestlabs:v0.1.0" \
     --cred ./azure.json \
-    --parameter-set ./parameters_service_innereye-devtestlabs.json \
+    --parameter-set ./parameters_service_devtestlabs.json \
     --debug
--- workspaces/innereye_deeplearning_inference/install_service_innereye_deeplearning.sh
+++ workspaces/innereye_deeplearning_inference/install_service_innereye_deeplearning.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-export AZUREML_WORKSPACE_NAME=$(porter installations output show azureml_workspace_name -i service-azureml-devtestlabs | tr -d '"')
-export AZUREML_ACR_ID=$(porter installations output show azureml_acr_id -i service-azureml-devtestlabs | tr -d '"')
+export AZUREML_WORKSPACE_NAME=$(porter installations output show azureml_workspace_name -i tre-service-azureml | tr -d '"')
+export AZUREML_ACR_ID=$(porter installations output show azureml_acr_id -i tre-service-azureml | tr -d '"')
 
 porter install tre-service-innereye-deeplearning --reference "${ACR_NAME}.azurecr.io/tre-service-innereye-deeplearning:v0.1.0" \
     --cred ./azure.json \
