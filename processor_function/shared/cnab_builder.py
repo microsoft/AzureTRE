286277d5b648a81221c6076be848a775a77e2997
sachin kundu <sakundu@microsoft.com>
2021-07-08 20:53:48 +0300

    Add ORYX build when deploying the function Fix syntax error in python cnab_builder Fix the broken build

--- processor_function/shared/cnab_builder.py
+++ processor_function/shared/cnab_builder.py
@@ -43,7 +43,7 @@ class CNABBuilder:
         acr_name = os.environ["REGISTRY_SERVER"].split(".")[0]
 
         command = [
-            + "/bin/bash", "-c", "az login --identity "
+            "/bin/bash", "-c", "az login --identity "
             + f"&& TOKEN=$(az acr login --name {acr_name} --expose-token --output tsv --query accessToken) "
             + f"&& docker login {os.environ['REGISTRY_SERVER']} --username 00000000-0000-0000-0000-000000000000 --password $TOKEN "
             + "&& porter "
