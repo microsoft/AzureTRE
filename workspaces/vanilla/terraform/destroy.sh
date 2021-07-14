5da7fabca4374c89b4e15af78ca5cf7f252551c3
Sachin Kundu <sachinkundu@gmail.com>
2021-07-14 17:07:55 +0300

    Enable E2E tests  (#460)
    * Refactor tests and add new action workflows
    
    * add notifications
    
    Co-authored-by: Marcus Robinson <marrobi@microsoft.com>

--- workspaces/vanilla/terraform/destroy.sh
+++ workspaces/vanilla/terraform/destroy.sh
@@ -4,5 +4,5 @@ terraform init -input=false -backend=true -reconfigure \
     -backend-config="storage_account_name=$TF_VAR_mgmt_storage_account_name" \
     -backend-config="container_name=$TF_VAR_terraform_state_container_name" \
     -backend-config="key=${TF_VAR_tre_id}-ws-${TF_VAR_workspace_id}"
-terraform plan
-terraform apply -auto-approve
+    
+terraform destroy -auto-approve
