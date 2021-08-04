925c019ee7e35626fdd3f7046d23f0bd005308bb
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2021-08-04 15:01:15 +0000

    remove obsolete storage service

--- templates/services/storage_service/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  type        = string
-  description = "Azure region for deployment of core TRE services"
-}
-
-variable "workspace_id" {
-  type        = string
-  description = "ID of the TRE Workspace (e.g. tre-dev-1111)"
-}
