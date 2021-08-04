59c9001134bd32214dc79d67b148089092096432
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2021-08-04 11:02:10 +0300

    Adding a guacamole user resource service template (a Windows DSVM) (#530)
    * Adding a guacamole user resource service template (a Windows DSVM)

---
+++ workspaces/services/guacamole-azure-dsvm/terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "workspace_id" {}
+variable "tre_id" {}
+variable "arm_client_id" {}
+variable "arm_client_secret" {}
+variable "arm_tenant_id" {}
+variable "arm_use_msi" {}
+variable "mgmt_resource_group_name" {}
+variable "parent_service_id" {}
