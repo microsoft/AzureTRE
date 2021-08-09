1dc7157e8bb19499c45cd78f247d5f1ec2465665
LizaShak <lishakur@microsoft.com>
2021-08-09 11:19:50 +0300

    Add workspace service installation api.

--- management_api_app/api/routes/workspaces.py
+++ management_api_app/api/routes/workspaces.py
@@ -55,6 +55,39 @@ async def create_workspace(workspace_create: WorkspaceInCreate, workspace_repo: 
     return WorkspaceIdInResponse(workspaceId=workspace.id)
 
 
+@router.post("/workspaces/{workspace_id}/workspace-services", status_code=status.HTTP_202_ACCEPTED,response_model=WorkspaceIdInResponse, name=strings.API_CREATE_WORKSPACE)
+async def create_workspace(workspace_create: WorkspaceInCreate,
+                           workspace_repo: WorkspaceRepository = Depends(get_repository(WorkspaceRepository)),
+                           user: User = Depends(get_current_user),
+                           workspace: Workspace = Depends(get_workspace_by_workspace_id_from_path)
+                           ) -> WorkspaceIdInResponse:
+
+    access_service = get_access_service()
+    if access_service.get_workspace_role(user, workspace) == WorkspaceRole.Owner:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=strings.ACCESS_USER_IS_NOT_OWNER)
+
+    try:
+        workspace = workspace_repo.create_workspace_item(workspace_create)
+    except (ValidationError, ValueError) as e:
+        logging.error(f"Failed create workspace model instance: {e}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+    try:
+        workspace_repo.save_workspace(workspace)
+    except Exception as e:
+        logging.error(f"Failed save workspace instance in DB: {e}")
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=strings.STATE_STORE_ENDPOINT_NOT_RESPONDING)
+
+    try:
+        await send_resource_request_message(workspace)
+    except Exception as e:
+        # TODO: Rollback DB change, issue #154
+        logging.error(f"Failed send workspace resource request message: {e}")
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=strings.SERVICE_BUS_GENERAL_ERROR_MESSAGE)
+
+    return WorkspaceIdInResponse(workspaceId=workspace.id)
+
+
 @router.get("/workspaces/{workspace_id}", response_model=WorkspaceInResponse, name=strings.API_GET_WORKSPACE_BY_ID)
 async def retrieve_workspace_by_workspace_id(
         user: User = Depends(get_current_user),
