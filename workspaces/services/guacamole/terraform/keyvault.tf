5587f3f4c8845e7ab24ac98534a8fd8955b62d19
Anat Balzam <anatbalzam@microsoft.com>
2021-08-15 14:05:19 +0000

    Fix formatting

--- workspaces/services/guacamole/terraform/keyvault.tf
+++ workspaces/services/guacamole/terraform/keyvault.tf
@@ -52,7 +52,7 @@ resource "azurerm_key_vault_access_policy" "current" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
-  secret_permissions      = ["Get", "List", "Set", "Delete"]
+  secret_permissions = ["Get", "List", "Set", "Delete"]
 }
 
 resource "azurerm_key_vault_access_policy" "guacamole" {
