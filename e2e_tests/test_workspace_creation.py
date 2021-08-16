c65110edad06c19903a2630c40a0b9394ebb2f5d
Denis Cepun <denis.cepun@microsoft.com>
2021-08-16 12:58:21 +0300

    limit to only names

--- e2e_tests/test_workspace_creation.py
+++ e2e_tests/test_workspace_creation.py
@@ -101,9 +101,8 @@ async def test_get_workspace_templates(template_name, token, verify) -> None:
             f"https://{config.TRE_ID}.{config.RESOURCE_LOCATION}.cloudapp.azure.com{strings.API_WORKSPACE_TEMPLATES}",
             headers=headers)
 
-        all_templates = [value for item in response.json()["templates"]
-                         for value in item.values()]
-        assert (template_name in all_templates), f"No {template_name} template found"
+        template_names = [templates["name"] for templates in response.json()["templates"]]
+        assert (template_name in template_names), f"No {template_name} template found"
 
 
 @pytest.mark.smoke
