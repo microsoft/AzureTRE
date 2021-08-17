5510afe6a3f149a8c3da1a5e2de0332221fb59c7
Denis Cepun <denis.cepun@microsoft.com>
2021-08-17 09:52:33 +0300

    set API acr_use_managed_identity_credentials=true

--- templates/core/terraform/api-webapp/api-webapp.tf
+++ templates/core/terraform/api-webapp/api-webapp.tf
@@ -58,6 +58,7 @@ resource "azurerm_app_service" "management_api" {
     linux_fx_version            = "DOCKER|${var.docker_registry_server}/${var.management_api_image_repository}:${var.management_api_image_tag}"
     remote_debugging_enabled    = false
     scm_use_main_ip_restriction = true
+    acr_use_managed_identity_credentials = true
 
     cors {
       allowed_origins     = []
