c85659dd4621867970b152209fbd23f9cad70a24
Tess Ferrandez <tferrand@microsoft.com>
2021-08-26 16:02:43 +0200

    change to exclude none (#728)

--- management_api_app/api/routes/workspace_templates.py
+++ management_api_app/api/routes/workspace_templates.py
@@ -39,7 +39,7 @@ async def get_workspace_templates(template_repo=Depends(get_repository(ResourceT
     return ResourceTemplateInformationInList(templates=templates_infos)
 
 
-@router.post("/workspace-templates", status_code=status.HTTP_201_CREATED, response_model=WorkspaceTemplateInResponse, response_model_exclude_unset=True, name=strings.API_CREATE_WORKSPACE_TEMPLATES)
+@router.post("/workspace-templates", status_code=status.HTTP_201_CREATED, response_model=WorkspaceTemplateInResponse, response_model_exclude_none=True, name=strings.API_CREATE_WORKSPACE_TEMPLATES)
 async def register_workspace_template(template_input: WorkspaceTemplateInCreate, template_repo=Depends(get_repository(ResourceTemplateRepository))) -> ResourceTemplateInResponse:
     try:
         return template_repo.create_and_validate_template(template_input, ResourceType.Workspace)
@@ -47,7 +47,7 @@ async def register_workspace_template(template_input: WorkspaceTemplateInCreate,
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=strings.WORKSPACE_TEMPLATE_VERSION_EXISTS)
 
 
-@router.get("/workspace-templates/{workspace_template_name}", response_model=WorkspaceTemplateInResponse, name=strings.API_GET_WORKSPACE_TEMPLATE_BY_NAME, response_model_exclude_unset=True)
+@router.get("/workspace-templates/{workspace_template_name}", response_model=WorkspaceTemplateInResponse, name=strings.API_GET_WORKSPACE_TEMPLATE_BY_NAME, response_model_exclude_none=True)
 async def get_current_workspace_template_by_name(workspace_template_name: str, template_repo=Depends(get_repository(ResourceTemplateRepository))) -> WorkspaceTemplateInResponse:
     template = get_current_template_by_name(workspace_template_name, template_repo, ResourceType.Workspace)
     return parse_obj_as(WorkspaceTemplateInResponse, template)
