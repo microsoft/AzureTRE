218f2018d1ad03e9bc31d6938379d729c34450e1
Tomi Paananen <tomi.paananen@microsoft.com>
2021-09-08 11:26:56 +0000

    Adds inbound internet rule to App Gateway NSG

--- templates/core/terraform/network/network_security_groups.tf
+++ templates/core/terraform/network/network_security_groups.tf
@@ -125,6 +125,18 @@ resource "azurerm_network_security_group" "app_gw" {
     source_address_prefix      = "GatewayManager"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowInboundInternet"
+    priority                   = 3801
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "app_gw" {
