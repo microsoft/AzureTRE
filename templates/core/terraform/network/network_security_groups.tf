fcf3482f84002f0dbd6a39435ce04760886411d6
Tomi Paananen <tomi.paananen@microsoft.com>
2021-09-10 15:33:23 +0300

    Allow inbound port 80 rule to App Gateway NSG

--- templates/core/terraform/network/network_security_groups.tf
+++ templates/core/terraform/network/network_security_groups.tf
@@ -133,7 +133,7 @@ resource "azurerm_network_security_group" "app_gw" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "443"
+    destination_port_ranges     = ["80", "443"]
     source_address_prefix      = "Internet"
     destination_address_prefix = "*"
   }
