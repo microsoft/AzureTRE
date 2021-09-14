78bdb5d1ca326cc6b47ecdc585c7b9fd98caf527
Tomi Paananen <tomi.paananen@microsoft.com>
2021-09-14 14:19:28 +0000

    Adds check for required env vars in Porter config generation script

--- resource_processor/vmss_porter/generate_porter_config.sh
+++ resource_processor/vmss_porter/generate_porter_config.sh
@@ -2,6 +2,17 @@
 
 # Generates required configuration for Porter Azure plugin
 # The output of this script should be appended to ~/.porter/config.toml
+>&2 printenv
+
+if [[ -z "${MGMT_RESOURCE_GROUP_NAME}" ]]; then
+  >&2 echo "Environment variable for TRE management resource group name missing"
+  exit 1
+fi
+
+if [[ -z "${MGMT_STORAGE_ACCOUNT_NAME}" ]]; then
+  >&2 echo "Environment variable for TRE management storage account name missing"
+  exit 1
+fi
 
 cat << EOF
 default-storage = "azurestorage"
