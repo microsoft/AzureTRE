49bbd5b7965ca475d193749770c171462878f240
Denis Cepun <denis.cepun@microsoft.com>
2021-09-14 15:16:05 +0300

    up version

--- templates/workspaces/azureml_devtestlabs/install_service_azureml.sh
+++ templates/workspaces/azureml_devtestlabs/install_service_azureml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-porter install tre-service-azureml --reference "${ACR_NAME}.azurecr.io/tre-service-azureml:v0.1.5" \
+porter install tre-service-azureml --reference "${ACR_NAME}.azurecr.io/tre-service-azureml:v0.1.6" \
     --cred ./azure.json \
     --parameter-set ./parameters_service_azureml.json \
     --debug
