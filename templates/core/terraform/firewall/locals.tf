1fb6b5c7c578edca27ce13cd157a4ceef5afd8be
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2021-09-14 05:58:39 +0000

    remove space from FQDNs

--- templates/core/terraform/firewall/locals.tf
+++ templates/core/terraform/firewall/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  nexus_allowed_fqdns_list = distinct(compact(split(",", var.nexus_allowed_fqdns)))
-  gitea_allowed_fqdns_list = distinct(compact(split(",", var.gitea_allowed_fqdns)))
+  nexus_allowed_fqdns_list = distinct(compact(split(",", replace(var.nexus_allowed_fqdns, " ", ""))))
+  gitea_allowed_fqdns_list = distinct(compact(split(",", replace(var.gitea_allowed_fqdns, " ", ""))))
 }
