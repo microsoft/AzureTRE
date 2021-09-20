855f5088d010394f76b3a964a466462274c788e2
marrobi <marrobi@microsoft.com>
2021-09-20 21:49:35 +0000

    update kv url

--- templates/workspace_services/guacamole/terraform/locals.tf
+++ templates/workspace_services/guacamole/terraform/locals.tf
@@ -7,6 +7,6 @@ locals {
   core_resource_group_name     = "rg-${var.tre_id}"
   aad_tenant_id                = data.azurerm_app_service.api_core.app_settings["AAD_TENANT_ID"]
   issuer                       = "https://login.microsoftonline.com/${local.aad_tenant_id}/v2.0"
-  kv_url                       = "https://kv-guac-${var.tre_id}-${local.short_workspace_id}.vault.azure.net"
+  kv_url                       = "https://kv-${var.tre_id}-${local.short_workspace_id}-${local.short_service_id}.vault.azure.net"
   api_url                      = "https://api-${var.tre_id}.azurewebsites.net"
 }
