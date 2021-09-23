a8fa2fd8de1d56a34ba64f1e769e7d83d10a9506
marrobi <marrobi@microsoft.com>
2021-09-23 13:49:45 +0000

    Move out KV and Storage, Move out private DNS zones, remove modules, new parameter model.

--- templates/workspace_services/azureml/terraform/firewall.tf
+++ templates/workspace_services/azureml/terraform/firewall.tf
@@ -7,10 +7,26 @@ data "azurerm_firewall" "fw" {
 
 }
 
-resource "null_resource" "az_login" {
+resource "null_resource" "az_login_sp" {
+
+  count = var.arm_use_msi == true ? 0 : 1
+  provisioner "local-exec" {
+    command = "az login --service-principal --username ${var.arm_client_id} --password ${var.arm_client_secret} --tenant ${var.arm_tenant_id}"
+  }
+
+  triggers = {
+    timestamp = timestamp()
+  }
+  
+}
+
+resource "null_resource" "az_login_msi" {
+
+  count = var.arm_use_msi == true ? 1 : 0
   provisioner "local-exec" {
     command = "az login --identity -u '${data.azurerm_client_config.current.client_id}'"
   }
+
   triggers = {
     timestamp = timestamp()
   }
@@ -25,7 +41,8 @@ data "external" "rule_priorities" {
     service_resource_name_suffix = local.service_resource_name_suffix
   }
   depends_on = [
-    null_resource.az_login
+    null_resource.az_login_sp,
+    null_resource.az_login_msi
   ]
 }
 
