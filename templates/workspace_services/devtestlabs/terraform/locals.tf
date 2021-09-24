ace8e2130b0e6b4b669855f8fe38f69d7051694b
marrobi <marrobi@microsoft.com>
2021-09-24 20:04:43 +0000

    Refactor devtestlabs template

--- templates/workspace_services/devtestlabs/terraform/locals.tf
+++ templates/workspace_services/devtestlabs/terraform/locals.tf
@@ -1,11 +1,8 @@
-# Random unique id
-resource "random_string" "unique_id" {
-  length      = 4
-  min_numeric = 4
-}
-
-
 locals {
-  service_id                   = random_string.unique_id.result
-  service_resource_name_suffix = "${var.tre_id}-ws-${var.workspace_id}-svc-${local.service_id}"
+
+  short_service_id               = substr(var.tre_resource_id, -4, -1)
+  short_workspace_id             = substr(var.workspace_id, -4, -1)
+  core_resource_group_name       = "rg-${var.tre_id}"
+  workspace_resource_name_suffix = "${var.tre_id}-ws-${local.short_workspace_id}"
+  service_resource_name_suffix   = "${var.tre_id}-ws-${local.short_workspace_id}-svc-${local.short_service_id}"
 }
