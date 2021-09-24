039cfdff950dca3d9ff047397f55bef7c7bd4909
marrobi <marrobi@microsoft.com>
2021-09-24 12:48:49 +0000

    Remove debug

--- templates/workspace_services/innereye/install_service_azureml.sh
+++ templates/workspace_services/innereye/install_service_azureml.sh
@@ -4,5 +4,5 @@ set -e
 
 porter install tre-service-azureml --reference "${MGMT_ACR_NAME}.azurecr.io/tre-service-azureml:v0.1.8" \
     --cred ./azure.json \
-    --parameter-set ./parameters_service_azureml.json \
-    --debug
+    --parameter-set ./parameters_service_azureml.json
+    
