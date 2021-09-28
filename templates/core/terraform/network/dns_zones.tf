c859c1e41097cb1d02c0341e6540ce828526899d
Marcus Robinson <marrobi@microsoft.com>
2021-09-28 11:42:54 +0000

    fix linting issue

--- templates/core/terraform/network/dns_zones.tf
+++ templates/core/terraform/network/dns_zones.tf
@@ -102,14 +102,14 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vaultcore" {
 
 resource "azurerm_private_dns_zone" "azurecr" {
   name                = "privatelink.azurecr.io"
-  resource_group_name =  var.resource_group_name
+  resource_group_name = var.resource_group_name
 
   lifecycle { ignore_changes = [tags] }
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "acrlink" {
   name                  = "acrcorelink"
-  resource_group_name   =  var.resource_group_name
+  resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.azurecr.name
   virtual_network_id    = azurerm_virtual_network.core.id
 
