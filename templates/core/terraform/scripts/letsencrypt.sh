4adfbb6adbf63c98782a3c4176a0d1cc32cc3bb9
Tomi Paananen <tomi.paananen@microsoft.com>
2021-10-04 15:45:05 +0000

    Restores the network rule temp removal to letsencrypt script

--- templates/core/terraform/scripts/letsencrypt.sh
+++ templates/core/terraform/scripts/letsencrypt.sh
@@ -3,6 +3,19 @@ set -e
 
 script_dir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 
+if [[ -z ${STORAGE_ACCOUNT} ]]; then
+  echo "STORAGE_ACCOUNT not set"
+  exit 1
+fi
+
+IPADDR=$(curl ipecho.net/plain; echo)
+
+echo "Creating network rule on storage account ${STORAGE_ACCOUNT} for $IPADDR"
+az storage account network-rule add --account-name "${STORAGE_ACCOUNT}" --ip-address $IPADDR
+echo "Waiting for network rule to take effect"
+sleep 30s
+echo "Created network rule on storage account"
+
 ledir=$(pwd)/letsencrypt
 
 mkdir -p "${ledir}/logs"
@@ -44,7 +57,6 @@ if [[ -n ${KEYVAULT} ]]; then
         --gateway-name "${APPLICATION_GATEWAY}" \
         --name 'cert-primary' \
         --key-vault-secret-id "${sid}"
-
 else
     az network application-gateway ssl-cert update \
         --resource-group "${RESOURCE_GROUP}" \
@@ -53,3 +65,7 @@ else
         --cert-file "${CERT_DIR}/aci.pfx" \
         --cert-password "${CERT_PASSWORD}"
 fi
+
+echo "Removing network rule on storage account"
+az storage account network-rule remove --account-name ${STORAGE_ACCOUNT} --ip-address ${IPADDR}
+echo "Removed network rule on storage account"
