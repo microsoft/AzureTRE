1c1bd77c0add10db101d2d7f36eb47bf8d1b668f
Denis Cepun <denis.cepun@microsoft.com>
2021-10-05 17:21:54 +0300

    Shared services setup (#981)
    Shared services setup

--- templates/shared_services/gitea/terraform/gitea-webapp.tf
+++ templates/shared_services/gitea/terraform/gitea-webapp.tf
@@ -30,11 +30,11 @@ resource "azurerm_app_service" "gitea" {
     WEBSITE_DNS_SERVER                  = "168.63.129.16" # required to access storage over private endpoints
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
 
-    GITEA_USERNAME = "gitea_admin"
+    GITEA_USERNAME = "giteaadmin"
     GITEA_PASSWD   = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.gitea_password.id})"
     GITEA_EMAIL    = "giteaadmin@tre.com"
 
-    GITEA__server__ROOT_URL              = "https://${local.webapp_name}.azurewebsites.net/gitea/"
+    GITEA__server__ROOT_URL              = "https://${local.webapp_name}.azurewebsites.net/"
     GITEA__log_0x2E_console__COLORIZE    = "false" # Azure monitor doens't show colors, so this is easier to read.
     GITEA__picture__DISABLE_GRAVATAR     = "true"  # external avaters are not available due to network restrictions
     GITEA__security__INSTALL_LOCK        = true
