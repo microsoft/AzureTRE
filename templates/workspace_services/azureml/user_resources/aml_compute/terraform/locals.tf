b3934633694d3981aeed3e18ae71468c0553a4f9
Marcus Robinson <marrobi@microsoft.com>
2021-10-06 09:36:37 +0000

    Split compute instnace into user resource. Failing on node agent deployment.

--- templates/workspace_services/azureml/user_resources/aml_compute/terraform/locals.tf
+++ templates/workspace_services/azureml/user_resources/aml_compute/terraform/locals.tf
@@ -1,13 +1,15 @@
 # Random unique id
 
 locals {
-  short_service_id               = substr(var.tre_resource_id, -4, -1)
+
+  short_user_resource_id               = substr(var.tre_resource_id, -4, -1)
+  short_service_id               = substr(var.parent_service_id, -4, -1)
   short_workspace_id             = substr(var.workspace_id, -4, -1)
   core_resource_group_name       = "rg-${var.tre_id}"
   workspace_resource_name_suffix = "${var.tre_id}-ws-${local.short_workspace_id}"
   service_resource_name_suffix   = "${var.tre_id}-ws-${local.short_workspace_id}-svc-${local.short_service_id}"
+  user_resource_name_suffix   = "${local.service_resource_name_suffix}-${local.short_user_resource_id}"
   aml_workspace_name             = lower("ml-${substr(local.service_resource_name_suffix, -30, -1)}")
-  aml_compute_id                 = substr("${var.tre_id}${var.workspace_id}${local.short_service_id}", -12, -1)
-  aml_compute_cluster_name       = "cp-${local.aml_compute_id}"
-  azureml_acr_name               = lower(replace("acr${substr(local.service_resource_name_suffix, -8, -1)}", "-", ""))
+  aml_compute_id                 = "${local.short_service_id}${local.short_user_resource_id}"
+  aml_compute_instance_name      = "ci-${local.aml_compute_id}"
 }
