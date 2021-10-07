a22130ef3e4c984e5dde8361c658e323f75e256f
Denis Cepun <denis.cepun@microsoft.com>
2021-10-07 12:15:02 +0300

    fix id for storage

--- templates/workspace_services/azureml/terraform/locals.tf
+++ templates/workspace_services/azureml/terraform/locals.tf
@@ -12,7 +12,7 @@ locals {
   workspace_resource_name_suffix = "${var.tre_id}-ws-${local.short_workspace_id}"
   service_resource_name_suffix   = "${var.tre_id}-ws-${local.short_workspace_id}-svc-${local.short_service_id}"
   allowed_urls                   = ["graph.windows.net", "ml.azure.com", "login.microsoftonline.com", "aadcdn.msftauth.net", "graph.microsoft.com", "management.azure.com", "viennaglobal.azurecr.io"]
-  allowed_service_tags           = ["${data.azurerm_network_service_tags.storage}", "AzureContainerRegistry"]
+  allowed_service_tags           = ["${data.azurerm_network_service_tags.storage.id}", "AzureContainerRegistry"]
   workspace_name                 = lower("ml-${substr(local.service_resource_name_suffix, -30, -1)}")
   acr_name                       = lower(replace("acr${substr(local.service_resource_name_suffix, -8, -1)}", "-", ""))
   keyvault_name                  = lower("kv-${substr(local.workspace_resource_name_suffix, -20, -1)}")
