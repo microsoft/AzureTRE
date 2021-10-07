421f7e834af3c5753db2708e457251096b43bdc5
anatbalzam <anatbalzam@microsoft.com>
2021-10-07 09:43:20 +0000

    Guacamole uses ws keyvault

--- templates/workspace_services/guacamole/terraform/locals.tf
+++ templates/workspace_services/guacamole/terraform/locals.tf
@@ -1,6 +1,7 @@
 locals {
   short_service_id             = substr(var.tre_resource_id, -4, -1)
   short_workspace_id           = substr(var.workspace_id, -4, -1)
+  workspace_resource_name_suffix = "${var.tre_id}-ws-${local.short_workspace_id}"
   service_resource_name_suffix = "${var.tre_id}-ws-${local.short_workspace_id}-svc-${local.short_service_id}"
   webapp_name                  = "guacamole-${local.service_resource_name_suffix}"
   core_vnet                    = "vnet-${var.tre_id}"
@@ -9,4 +10,5 @@ locals {
   issuer                       = "https://login.microsoftonline.com/${local.aad_tenant_id}/v2.0"
   kv_url                       = "https://kv-${var.tre_id}-${local.short_workspace_id}-${local.short_service_id}.vault.azure.net"
   api_url                      = "https://api-${var.tre_id}.azurewebsites.net"
+  keyvault_name                  = lower("kv-${substr(local.workspace_resource_name_suffix, -20, -1)}")
 }
