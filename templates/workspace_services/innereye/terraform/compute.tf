cf51bc3e86e095e354d4dc4ee6db5a974433d107
Marcus Robinson <marrobi@microsoft.com>
2021-10-07 12:11:30 +0100

    Move AML compute instance to user resource (#1007)

--- templates/workspace_services/innereye/terraform/compute.tf
+++ templates/workspace_services/innereye/terraform/compute.tf
@@ -2,10 +2,6 @@ data "local_file" "deploypl_compute_cluster" {
   filename = "${path.module}/nopipcompute/deploypl_compute_cluster.json"
 }
 
-data "local_file" "deploypl_compute_instance" {
-  filename = "${path.module}/nopipcompute/deploypl_compute_instance.json"
-}
-
 # need to add existing VNET
 resource "azurerm_resource_group_template_deployment" "deploy_compute_cluster" {
   name                = "dpl-${local.service_resource_name_suffix}_deploy_compute_cluster"
@@ -45,41 +41,6 @@ resource "azurerm_resource_group_template_deployment" "deploy_compute_cluster" {
     },
     "max_node_count" = {
       "value" = 1
-    }
-  })
-
-  deployment_mode = "Incremental"
-}
-
-
-resource "azurerm_resource_group_template_deployment" "deploy_compute_instance" {
-  name                = "dpl-${local.service_resource_name_suffix}-deploy_compute_instance"
-  resource_group_name = data.azurerm_resource_group.ws.name
-
-  template_content = data.local_file.deploypl_compute_instance.content
-
-  # these key-value pairs are passed into the ARM Template's `parameters` block
-  parameters_content = jsonencode({
-
-
-    "vnet_name" = {
-      "value" = data.azurerm_virtual_network.ws.name
-    },
-    "location" = {
-      "value" = data.azurerm_resource_group.ws.location
-    },
-    "workspace_name" = {
-      "value" = local.aml_workspace_name
-    },
-    "instance_name" = {
-      "value" = local.aml_compute_instance_name
-    },
-    "subnet_name" = {
-      "value" = data.azurerm_subnet.services.name
-    },
-    "vm_size_sku" = {
-      "value" = "Standard_D4_v2"
-
     }
   })
 
