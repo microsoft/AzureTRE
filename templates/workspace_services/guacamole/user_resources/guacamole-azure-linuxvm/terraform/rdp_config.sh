87029ddfc1ce445f0f18f78e329ae01a6835a5fa
James Griffin <me@JamesGriff.in>
2022-02-03 13:41:10 +0000

    Working rdp script (potentially)

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/rdp_config.sh
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/rdp_config.sh
@@ -2,18 +2,14 @@
 NEXUS_URL="https://nexus-${tre_id}.azurewebsites.net"
 sudo rm -r /var/lib/apt/lists/*
 sudo rm /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo/ bionic main restricted" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo/ bionic universe" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo/ bionic multiverse" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo/ bionic main restricted" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo/ bionic universe" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo/ bionic multiverse" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/pypi-proxy-repo/ bionic main restricted" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/pypi-proxy-repo/ bionic universe" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/pypi-proxy-repo/ bionic multiverse" >> /etc/apt/sources.list
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install ubuntu-gnome-desktop -yq
-sudo apt-get install xrdp -y
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo/ bionic main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo/ bionic main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/pypi-proxy-repo/ bionic main restricted universe multiverse" >> /etc/apt/sources.list
+sudo apt update
+sudo apt install xorg xfce4 xfce4-goodies dbus-x11 x11-xserver-utils -y
+echo xfce4-session > ~/.xsession
+sudo apt install xrdp -y
 sudo adduser xrdp ssl-cert
 sudo systemctl enable xrdp
-sudo reboot
+sudo systemctl restart xrdp
