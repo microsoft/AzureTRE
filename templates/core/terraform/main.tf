089dd4770dde50440a66a77e06034cc1370f7f11
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-02-03 13:50:43 +0000

    add env cleanup workflow

--- templates/core/terraform/main.tf
+++ templates/core/terraform/main.tf
@@ -23,9 +23,9 @@ resource "azurerm_resource_group" "core" {
   location = var.location
   name     = "rg-${var.tre_id}"
   tags = {
-    project = "Azure Trusted Research Environment"
-    tre_id  = var.tre_id
-    source  = "https://github.com/microsoft/AzureTRE/"
+    project    = "Azure Trusted Research Environment"
+    tre_id     = var.tre_id
+    source     = "https://github.com/microsoft/AzureTRE/"
     ci_git_ref = var.ci_git_ref # TODO: not include if empty
   }
 
