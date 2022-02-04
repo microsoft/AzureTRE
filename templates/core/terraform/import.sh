5e8e8a66491f5e8143e0fda969df80d053224f48
marrobi <marrobi@microsoft.com>
2022-02-04 13:12:08 +0000

    Update deploy to import

--- templates/core/terraform/import.sh
+++ templates/core/terraform/import.sh
@@ -12,11 +12,11 @@ terraform init -input=false -backend=true -reconfigure -upgrade \
     -backend-config="container_name=$TF_VAR_terraform_state_container_name" \
     -backend-config="key=${TRE_ID}"
 
-terraform import module.jumpbox.azurerm_key_vault_secret.jumpbox_credentials "https://kv-${TRE_ID}.vault.azure.net/secrets/vm-${TRE_ID}-jumpbox-admin-credentials/e6ae209811894395bd50a846e18d1782"
+# terraform import module.jumpbox.azurerm_key_vault_secret.jumpbox_credentials "https://kv-${TRE_ID}.vault.azure.net/secrets/vm-${TRE_ID}-jumpbox-admin-credentials/e6ae209811894395bd50a846e18d1782"
 
-terraform import module.appgateway.azurerm_key_vault_certificate.tlscert "https://kv-${TRE_ID}.vault.azure.net/certificates/letsencrypt/6e5a17562a464283804cc9795479cfb7"
+# terraform import module.appgateway.azurerm_key_vault_certificate.tlscert "https://kv-${TRE_ID}.vault.azure.net/certificates/letsencrypt/6e5a17562a464283804cc9795479cfb7"
 
-terraform import module.resource_processor_vmss_porter[0].azurerm_key_vault_secret.resource_processor_vmss_password "https://kv-${TRE_ID}.vault.azure.net/secrets/resource-processor-vmss-password/27d8225bd9ba49369cf40d607732bd9c"
+# terraform import module.resource_processor_vmss_porter[0].azurerm_key_vault_secret.resource_processor_vmss_password "https://kv-${TRE_ID}.vault.azure.net/secrets/resource-processor-vmss-password/27d8225bd9ba49369cf40d607732bd9c"
 
 terraform import module.gitea[0].azurerm_key_vault_secret.gitea_password "https://kv-${TRE_ID}.vault.azure.net/secrets/gitea-${TRE_ID}-admin-password/666122b4526447fda428b2bc3f1fff23"
 
