217b6ddf34f7861b77e65ea7c816e6507043cd46
ross-p-smith <rosmith@microsoft.com>
2022-02-05 22:29:44 +0000

    Added script to register workspace

--- devops/scripts/register-aad-workspace.sh
+++ devops/scripts/register-aad-workspace.sh
@@ -4,12 +4,14 @@ set -e
 : ${AAD_TENANT_ID?"You have not set you AAD_TENANT_ID in ./templates/core/.env"}
 
 LOGGED_IN_TENANT_ID=$(az account show --query tenantId -o tsv)
+CHANGED_TENANT=0
 
 if [ "${LOGGED_IN_TENANT_ID}" != "${AAD_TENANT_ID}" ]; then
-  echo "Attempting to sign you onto ${AAD_TENANT_ID} to add an App Registration"
+  echo "Attempting to sign you onto ${AAD_TENANT_ID} to add an App Registration."
 
   # First we need to login to the AAD tenant (as it could be different to the subscription tenant)
   az login --tenant ${AAD_TENANT_ID} --allow-no-subscriptions
+  CHANGED_TENANT=1
 fi
 
 # Then register an App
