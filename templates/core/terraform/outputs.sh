fc2b1f026c2f82ea122aa743775579e803c58f4d
ross-p-smith <rosmith@microsoft.com>
2022-02-07 09:20:28 +0000

    Copy templates/core/.env rather than output values

--- templates/core/terraform/outputs.sh
+++ templates/core/terraform/outputs.sh
@@ -20,12 +20,6 @@ fi
 # Add a few extra values to the file to help us
 # These are mainly ENV_VARS that have been named differently
 echo "SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE=sb-${TRE_ID}.servicebus.windows.net" >> ../tre.env
-# Add the ones from ./templates/core.env
-echo "TRE_ID=${TRE_ID}" >> ../tre.env
-echo "AAD_TENANT_ID=${AAD_TENANT_ID}" >> ../tre.env
-echo "API_CLIENT_ID=${API_CLIENT_ID}" >> ../tre.env
-echo "API_CLIENT_SECRET=${API_CLIENT_SECRET}" >> ../tre.env
-echo "SWAGGER_UI_CLIENT_ID=${SWAGGER_UI_CLIENT_ID}" >> ../tre.env
 # These next ones from Check Dependencies
 echo "SUBSCRIPTION_ID=${SUB_ID}" >> ../tre.env
 echo "AZURE_SUBSCRIPTION_ID=${SUB_ID}" >> ../tre.env
