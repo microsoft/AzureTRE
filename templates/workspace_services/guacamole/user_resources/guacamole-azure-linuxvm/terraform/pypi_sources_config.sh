bdf2d41348ceda2e9804745e007946a74b4f03ac
James Griffin <me@JamesGriff.in>
2022-02-14 17:02:12 +0000

    Pip install working for linux

---
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/pypi_sources_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo tee /etc/pip.conf > dev/null <<'EOF'
+[global]
+index = ${nexus_proxy_url}/repository/pypi/pypi
+index-url = ${nexus_proxy_url}/repository/pypi/simple
+trusted-host = ${nexus_proxy_url}
+EOF
