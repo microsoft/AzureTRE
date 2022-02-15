03b6bcff887b87c5aec8210d7576c8acce5dca3f
ross-p-smith <rosmith@microsoft.com>
2022-02-15 15:51:11 +0000

    typo

--- templates/workspace_services/guacamole/guacamole-server/docker/maven_package_and_exit_succesfully.sh
+++ templates/workspace_services/guacamole/guacamole-server/docker/maven_package_and_exit_succesfully.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Remove any previously run failed flag
-# 2. Run pytest, but capture the exit code so we always succeed
+# 2. Run maven, but capture the exit code so we always succeed
 # 3. Output a file if the tests are not successful.
 rm -f /target/surefire-reports/guacamole_package_failed
 pytest_result=$(mvn package)
