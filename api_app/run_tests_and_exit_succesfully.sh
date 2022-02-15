b22dd61c7665dc47e3f68a744f8c95785a07595a
Ross Smith <ross-p-smith@users.noreply.github.com>
2022-02-15 17:43:49 +0000

    Unit Tests only run if api_app has changed and are executed from docker build (#1304)
    * Add unit tests into tasks
    
    * Move Unit Tests back into Docker build
    
    * workflow dispatch condition
    
    * Catch the exit code
    
    * Always output the test results
    
    * Putting Unit Tests back to previous behaviour
    
    * missed debug
    
    * Allow pytest to fail, but build carries on
    
    * Force failure
    
    * Discover directory
    
    * Check file existence
    
    * Remove failing test
    
    * Make Java fail
    
    * Remove the failed java test
    
    * typo
    
    * Does the workflow fail on the publish step only
    
    * Some linting
    
    * Linting problems!
    
    * Missing URI
    
    * More missing URI

---
+++ api_app/run_tests_and_exit_succesfully.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1. Remove any previously run failed flag
+# 2. Run pytest, but capture the exit code so we always succeed
+# 3. Output a file if the tests are not successful.
+rm -f ../test-results/pytest_unit_failed
+pytest_result=$(pytest --junit-xml ../test-results/pytest_api_unit.xml --ignore e2e_tests)
+
+if [ $? != 0 ]; then
+  touch ../test-results/pytest_api_unit_failed
+fi
