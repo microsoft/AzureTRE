9414a494e32d528c60ca87cd075235dd1e6be287
oliver7598 <oliver.a@live.co.uk>
2022-02-16 14:36:22 +0000

    Set cmdkey add as start-up script

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/vm_config.ps1
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/vm_config.ps1
@@ -3,5 +3,6 @@ if( ${SharedStorageAccess} -eq 1 )
   $password = ConvertTo-SecureString -String ${StorageAccountKey} -AsPlainText -Force
   $credential = New-Object System.Management.Automation.PSCredential -ArgumentList "AZURE\${StorageAccountName}", $password
   New-PSDrive -Name Z -PSProvider FileSystem -Root "\\${StorageAccountName}.file.core.windows.net\${FileShareName}" -Credential $credential -Persist -Scope Global
-  runas /user:${AdminUser} "cmdkey /add:${StorageAccountName}.file.core.windows.net /user:Azure\${StorageAccountName} /pass:${StorageAccountKey}"
+  $command = "powershell -WindowStyle hidden ""cmdkey /add:${StorageAccountName}.file.core.windows.net /user:Azure\${StorageAccountName} /pass:${StorageAccountKey}"""
+  $command | Out-File "C:\Users\${AdminUser}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AddCredentials.cmd"
 }
