7e9a59bb7f8a7473166fb862fdc04a44ed53fe97
James Griffin <me@JamesGriff.in>
2022-02-28 13:17:57 +0000

    Amended API permissions

--- api_app/api/routes/workspaces.py
+++ api_app/api/routes/workspaces.py
@@ -164,10 +164,8 @@ async def delete_workspace(response: Response, workspace=Depends(get_workspace_b
     return OperationInResponse(operation=operation)
 
 
-@workspaces_core_router.post("/workspaces/{workspace_id}/invoke-action", status_code=status.HTTP_202_ACCEPTED, response_model=OperationInResponse, name=strings.API_INVOKE_ACTION_ON_WORKSPACE)
-async def invoke_action_on_workspace(response: Response, action: str, workspace=Depends(get_workspace_by_id_from_path), resource_template_repo=Depends(get_repository(ResourceTemplateRepository)), operations_repo=Depends(get_repository(OperationRepository)), user=Depends(get_current_workspace_owner_or_researcher_user)) -> OperationInResponse:
-    validate_user_is_workspace_owner_or_resource_owner(user, workspace)
-
+@workspaces_core_router.post("/workspaces/{workspace_id}/invoke-action", status_code=status.HTTP_202_ACCEPTED, response_model=OperationInResponse, name=strings.API_INVOKE_ACTION_ON_WORKSPACE, dependencies=[Depends(get_current_admin_user)])
+async def invoke_action_on_workspace(response: Response, action: str, workspace=Depends(get_workspace_by_id_from_path), resource_template_repo=Depends(get_repository(ResourceTemplateRepository)), operations_repo=Depends(get_repository(OperationRepository))) -> OperationInResponse:
     operation = await send_custom_action_message(workspace, action, ResourceType.Workspace, operations_repo, resource_template_repo)
     response.headers["Location"] = construct_location_header(operation)
 
@@ -237,10 +235,8 @@ async def delete_workspace_service(response: Response, workspace=Depends(get_wor
     return OperationInResponse(operation=operation)
 
 
-@workspace_services_workspace_router.post("/workspaces/{workspace_id}/workspace-services/{service_id}/invoke-action", status_code=status.HTTP_202_ACCEPTED, response_model=OperationInResponse, name=strings.API_INVOKE_ACTION_ON_WORKSPACE_SERVICE)
-async def invoke_action_on_workspace_service(response: Response, action: str, workspace_service=Depends(get_workspace_service_by_id_from_path), resource_template_repo=Depends(get_repository(ResourceTemplateRepository)), operations_repo=Depends(get_repository(OperationRepository)), user=Depends(get_current_workspace_owner_or_researcher_user)) -> OperationInResponse:
-    validate_user_is_workspace_owner_or_resource_owner(user, workspace_service)
-
+@workspace_services_workspace_router.post("/workspaces/{workspace_id}/workspace-services/{service_id}/invoke-action", status_code=status.HTTP_202_ACCEPTED, response_model=OperationInResponse, name=strings.API_INVOKE_ACTION_ON_WORKSPACE_SERVICE, dependencies=[Depends(get_current_workspace_owner_user)])
+async def invoke_action_on_workspace_service(response: Response, action: str, workspace_service=Depends(get_workspace_service_by_id_from_path), resource_template_repo=Depends(get_repository(ResourceTemplateRepository)), operations_repo=Depends(get_repository(OperationRepository))) -> OperationInResponse:
     operation = await send_custom_action_message(workspace_service, action, ResourceType.WorkspaceService, operations_repo, resource_template_repo)
     response.headers["Location"] = construct_location_header(operation)
 
@@ -328,7 +324,6 @@ async def patch_user_resource(user_resource_patch: ResourcePatch, user=Depends(g
 @user_resources_workspace_router.post("/workspaces/{workspace_id}/workspace-services/{service_id}/user-resources/{resource_id}/invoke-action", status_code=status.HTTP_202_ACCEPTED, response_model=OperationInResponse, name=strings.API_INVOKE_ACTION_ON_USER_RESOURCE)
 async def invoke_action_on_user_resource(response: Response, action: str, user_resource=Depends(get_user_resource_by_id_from_path), workspace_service=Depends(get_workspace_service_by_id_from_path), resource_template_repo=Depends(get_repository(ResourceTemplateRepository)), operations_repo=Depends(get_repository(OperationRepository)), user=Depends(get_current_workspace_owner_or_researcher_user)) -> OperationInResponse:
     validate_user_is_workspace_owner_or_resource_owner(user, user_resource)
-
     operation = await send_custom_action_message(user_resource, action, ResourceType.UserResource, operations_repo, resource_template_repo, workspace_service.templateName)
     response.headers["Location"] = construct_location_header(operation)
 
