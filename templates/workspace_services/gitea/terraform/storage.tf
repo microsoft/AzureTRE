24889523c86e23998c6115c6a01b52bdd1100632
marrobi <marrobi@microsoft.com>
2022-03-02 23:20:56 +0000

    Gitea workspace service

---
+++ templates/workspace_services/gitea/terraform/storage.tf
@@ -0,0 +1,38 @@
+resource "azurerm_storage_account" "gitea" {
+  name                     = local.storage_name
+  resource_group_name      = data.azurerm_resource_group.ws.name
+  location                 = data.azurerm_resource_group.ws.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  lifecycle { ignore_changes = [tags] }
+}
+
+resource "azurerm_storage_account_network_rules" "stgrules" {
+  storage_account_id = azurerm_storage_account.gitea.id
+
+  default_action = "Deny"
+  bypass         = []
+}
+
+resource "azurerm_private_endpoint" "stgfilepe" {
+  name                = "stgfilepe-${local.service_resource_name_suffix}"
+  location            = data.azurerm_resource_group.ws.location
+  resource_group_name = data.azurerm_resource_group.ws.name
+  subnet_id           = data.azurerm_subnet.services.id
+
+  lifecycle { ignore_changes = [tags] }
+
+  private_dns_zone_group {
+    name                 = "private-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.filecore.id]
+  }
+
+  private_service_connection {
+    name                           = "stgfilepesc-${local.service_resource_name_suffix}"
+    private_connection_resource_id = azurerm_storage_account.gitea.id
+    is_manual_connection           = false
+    subresource_names              = ["File"]
+  }
+}
+
---
+++ templates/workspace_services/gitea/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "workspace_id" {}
+variable "tre_id" {}
+variable "id" {}
+variable "mgmt_resource_group_name" {}
+variable "mgmt_acr_name" {}
+variable "gitea_storage_limit" {
+  type        = number
+  description = "Space allocated in GB for the Gitea data in Azure Files Share"
+  default     = 1024
+}
