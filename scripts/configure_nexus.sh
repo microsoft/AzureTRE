2893c48987c1523ad37c55fd99013307d78aa945
oliver7598 <oliver.a@live.co.uk>
2022-03-02 14:18:10 +0000

    Anaconda proxy added

--- scripts/configure_nexus.sh
+++ scripts/configure_nexus.sh
@@ -113,3 +113,16 @@ if [[ ${STATUS_CODE} == 404 ]]
     -H 'Content-Type: application/json' \
     -d '@./scripts/pypi_proxy_conf.json'
 fi
+
+#Check if the repo already exists
+export STATUS_CODE=$(curl -iu admin:$NEXUS_PASS -X "GET" "${NEXUS_URL}/service/rest/v1/repositories/proxy/conda" -H "accept: application/json" -k -s -w "%{http_code}" -o /dev/null)
+
+if [[ ${STATUS_CODE} == 404 ]]
+ then
+    # Let's create pypi proxy
+    curl -iu admin:$NEXUS_PASS -XPOST \
+    $NEXUS_URL/service/rest/v1/repositories/conda/proxy \
+    -H 'accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -d '@./scripts/conda_proxy_conf.json'
+fi
