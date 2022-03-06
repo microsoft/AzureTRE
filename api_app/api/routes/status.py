8fe54d1132b52dd68d90b9a1127d036802d0e964
Anat Balzam <anatbalzam@microsoft.com>
2022-03-06 16:11:06 +0200

    Add tests

--- api_app/api/routes/status.py
+++ api_app/api/routes/status.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from models.schemas.status import HealthCheck, ServiceStatus
 from resources import strings
-from services.health_checker import create_state_store_status
+from services.health_checker import create_state_store_status, create_service_bus_status
 
 
 router = APIRouter()
@@ -10,6 +10,8 @@ router = APIRouter()
 
 @router.get("/status", name=strings.API_GET_STATUS_OF_SERVICES)
 async def health_check() -> HealthCheck:
-    status, message = create_state_store_status()
-    services = [ServiceStatus(service=strings.COSMOS_DB, status=status, message=message)]
+    cosmos_status, cosmos_message = create_state_store_status()
+    sb_status, sb_message = await create_service_bus_status()
+    services = [ServiceStatus(service=strings.COSMOS_DB, status=cosmos_status, message=cosmos_message),
+                ServiceStatus(service=strings.SERVICE_BUS, status=sb_status, message=sb_message)]
     return HealthCheck(services=services)
--- api_app/services/health_checker.py
+++ api_app/services/health_checker.py
@@ -1,13 +1,25 @@
+from contextlib import asynccontextmanager
+
 from azure.core import exceptions
 from azure.cosmos import CosmosClient
 from azure.identity.aio import DefaultAzureCredential
-from azure.servicebus import ServiceBusClient
+from azure.servicebus.aio import ServiceBusClient
 from azure.servicebus.exceptions import ServiceBusConnectionError
 
 from api.dependencies.database import get_store_key
 from core import config
 from models.schemas.status import StatusEnum
 from resources import strings
+
+
+@asynccontextmanager
+async def default_credentials():
+    """
+    Context manager which yields the default credentials.
+    """
+    credential = DefaultAzureCredential(managed_identity_client_id=config.MANAGED_IDENTITY_CLIENT_ID)
+    yield credential
+    await credential.close()
 
 
 def create_state_store_status() -> (StatusEnum, str):
@@ -17,31 +29,33 @@ def create_state_store_status() -> (StatusEnum, str):
     try:
         primary_master_key = get_store_key()
 
-        cosmos_client = CosmosClient(config.STATE_STORE_ENDPOINT, primary_master_key , connection_verify=debug)
+        cosmos_client = CosmosClient(config.STATE_STORE_ENDPOINT, primary_master_key, connection_verify=debug)
         list(cosmos_client.list_databases())
     except exceptions.ServiceRequestError:
         status = StatusEnum.not_ok
         message = strings.STATE_STORE_ENDPOINT_NOT_RESPONDING
-    except:     # noqa: E722 flake8 - no bare excepts
+    except:  # noqa: E722 flake8 - no bare excepts
         status = StatusEnum.not_ok
         message = strings.UNSPECIFIED_ERROR
     return status, message
 
 
-def create_service_bus_status() -> (StatusEnum, str):
+async def create_service_bus_status() -> (StatusEnum, str):
     status = StatusEnum.ok
     message = ""
     try:
-        credential = DefaultAzureCredential(managed_identity_client_id=config.MANAGED_IDENTITY_CLIENT_ID)
-
-        service_bus_client = ServiceBusClient(config.SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE, credential)
-        with service_bus_client.get_queue_receiver(queue_name=config.SERVICE_BUS_DEPLOYMENT_STATUS_UPDATE_QUEUE):
-            pass
+        async with default_credentials() as credential:
+            service_bus_client = ServiceBusClient(config.SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE, credential,
+                                                  retry_total=0)
+            async with service_bus_client:
+                receiver = service_bus_client.get_queue_receiver(
+                    queue_name=config.SERVICE_BUS_DEPLOYMENT_STATUS_UPDATE_QUEUE)
+                async with receiver:
+                    pass
     except ServiceBusConnectionError:
         status = StatusEnum.not_ok
         message = strings.SERVICE_BUS_NOT_RESPONDING
-    except:     # noqa: E722 flake8 - no bare excepts
+    except:  # noqa: E722 flake8 - no bare excepts
         status = StatusEnum.not_ok
-        message = strings.SERVICE_BUS_GENERAL_ERROR_MESSAGE
+        message = strings.UNSPECIFIED_ERROR
     return status, message
-
