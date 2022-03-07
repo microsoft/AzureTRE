a06e81411a20806741102f626c31a1c884e337f9
Tanya Borisova <tborisova@microsoft.com>
2022-03-07 17:00:12 +0000

    WIP (doesnt work): Shared services repository

--- api_app/db/repositories/shared_services.py
+++ api_app/db/repositories/shared_services.py
@@ -8,7 +8,7 @@ from db.repositories.resource_templates import ResourceTemplateRepository
 from db.repositories.resources import ResourceRepository, IS_ACTIVE_CLAUSE
 from models.domain.shared_service import SharedService
 from models.schemas.resource import ResourcePatch
-from models.schemas.shared_service_template import SharedServiceInCreate
+from models.schemas.shared_service_template import SharedServiceTemplateInCreate
 from models.domain.resource import ResourceType
 
 
@@ -18,13 +18,13 @@ class SharedServiceRepository(ResourceRepository):
 
     @staticmethod
     def shared_services_query(shared_service_id: str):
-        return f'SELECT * FROM c WHERE c.resourceType = "{ResourceType.SharedService}" AND c.sharedServiceId = "{shared_service_id}"'
+        return f'SELECT * FROM c WHERE c.resourceType = "{ResourceType.SharedService}" AND c.id = "{shared_service_id}"'
 
     @staticmethod
     def active_shared_services_query(shared_service_id: str):
-        return f'SELECT * FROM c WHERE {IS_ACTIVE_CLAUSE} AND c.resourceType = "{ResourceType.SharedService}" AND c.sharedId = "{shared_service_id}"'
+        return f'SELECT * FROM c WHERE {IS_ACTIVE_CLAUSE} AND c.resourceType = "{ResourceType.SharedService}" AND c.id = "{shared_service_id}"'
 
-    def get_active_shared_services_for_shared(self, shared_service_id: str) -> List[SharedService]:
+    def get_active_shared_services(self, shared_service_id: str) -> List[SharedService]:
         """
         returns list of "non-deleted" shared services linked to this shared
         """
@@ -35,16 +35,15 @@ class SharedServiceRepository(ResourceRepository):
     def get_shared_service_spec_params(self):
         return self.get_resource_base_spec_params()
 
-    def create_shared_service_item(self, shared_service_input: SharedServiceInCreate, shared_service_id: str) -> SharedService:
+    def create_shared_service_item(self, shared_service_input: SharedServiceTemplateInCreate, shared_service_id: str) -> SharedService:
         full_shared_service_id = str(uuid.uuid4())
 
         template_version = self.validate_input_against_template(shared_service_input.templateName, shared_service_input, ResourceType.SharedService)
 
-        # we don't want something in the input to overwrite the system parameters, so dict.update can't work.
         resource_spec_parameters = {**shared_service_input.properties, **self.get_shared_service_spec_params()}
 
         shared_service = SharedService(
-            sharedServiceIid=full_shared_service_id,
+            id=full_shared_service_id,
             templateName=shared_service_input.templateName,
             templateVersion=template_version,
             properties=resource_spec_parameters,
