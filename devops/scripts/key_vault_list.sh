75ec4446cc82223f486c2795079e0cb3312ff1ae
David Moore <damoo@microsoft.com>
2022-03-09 14:49:02 +0000

    keyvault script wip

---
+++ devops/scripts/key_vault_list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z ${TRE_ID:-} ]]; then
+    echo "TRE_ID environment variable must be set."
+    exit 1
+fi
+
+echo "az keyvault show"
+az keyvault show --name kv-${TRE_ID}
+
+echo "az keyvault secret list"
+az keyvault secret list --vault-name kv-${TRE_ID}
+
+echo "az keyvault secret list-deleted"
+az keyvault secret list-deleted --vault-name kv-${TRE_ID}
