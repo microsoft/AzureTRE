87761b74c4e7828f00d72f4c60cc9d910fc5d496
James Griffin <me@JamesGriff.in>
2022-03-11 16:46:20 +0000

    Add nexus config to persistent volume

--- templates/shared_services/sonatype-nexus/terraform/variables.tf
+++ templates/shared_services/sonatype-nexus/terraform/variables.tf
@@ -8,20 +8,8 @@ variable "location" {
   description = "Azure location (region) for deployment of core TRE services"
 }
 
-variable "nexus_storage_limit" {
-  type        = number
-  description = "Space allocated in GB for the Nexus data in Azure Files Share"
-  default     = 1024
-}
-
 variable "nexus_allowed_fqdns" {
   type        = string
   description = "comma seperated string of allowed FQDNs for Nexus"
   default     = "*pypi.org,files.pythonhosted.org,security.ubuntu.com,archive.ubuntu.com,repo.anaconda.com"
-}
-
-variable "nexus_properties_path" {
-  type        = string
-  description = "relative path of nexus properties file"
-  default     = "/cnab/app/nexus.properties"
 }
