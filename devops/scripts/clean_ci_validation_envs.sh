c234bd9a053cb97ceeabe71d627d729530c383ae
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-03-13 06:34:59 +0000

    fix delete e2e workspace

--- devops/scripts/clean_ci_validation_envs.sh
+++ devops/scripts/clean_ci_validation_envs.sh
@@ -65,10 +65,9 @@ while read -r rg_name rg_ref_name; do
   fi
 done
 
-# check if any workflows run on the main branch (except the cleanup=current one)
+# check if any workflows run on the main branch (except the cleanup one)
 # to prevent us deleting a workspace for which an E2E (on main) is currently running
-echo "workflow name is: ${GITHUB_WORKFLOW}"
-if [[ -z $(gh api "https://api.github.com/repos/microsoft/AzureTRE/actions/runs?branch=main&status=in_progress" | jq --arg GITHUB_WORKFLOW '.workflow_runs | select(.[].name != "$GITHUB_WORKFLOW")') ]]
+if [[ -z $(gh api "https://api.github.com/repos/microsoft/AzureTRE/actions/runs?branch=main&status=in_progress" | jq '.workflow_runs | select(.[].name != "Clean Validation Environments")') ]]
 then
   # if not, we can delete old workspace resource groups that were left due to errors.
   az group list --query "[?starts_with(name, 'rg-${MAIN_TRE_ID}-')].name" -o tsv |
