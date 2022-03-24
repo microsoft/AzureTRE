bcff99aae5e4442f8b713601272714b6086f46e1
Stuart Leeks <stuartle@microsoft.com>
2022-03-24 13:16:04 +0000

    Output healthcheck response to aid debugging

--- devops/scripts/api_healthcheck.sh
+++ devops/scripts/api_healthcheck.sh
@@ -3,12 +3,17 @@ set -e
 
 response=$(curl -k "https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/health")
 
+echo "Got response from https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/health:"
+echo "$response"
+echo
+
 not_ok_count=$(echo "${response}"  | jq -r '[.services | .[] | select(.status!="OK")] | length')
 
 if [[ "$not_ok_count" == "0" ]]; then
-  echo "API Healthy"
+  echo "*** API healthy ***"
 else
-  echo "API _not_ healthy. Unhealthy services:"
+  echo "*** API _not_ healthy ***"
+  echo "Unhealthy services:"
   echo "${response}"  | jq -r '[.services | .[] | select(.status!="OK")]'
   exit 1
 fi
