d6abaaf287627d9c24da9b516d396bebaa35d117
Tanya Borisova <tborisova@microsoft.com>
2022-03-25 17:16:24 +0000

    Revert changes in keyvault

--- templates/workspaces/base/terraform/keyvault.tf
+++ templates/workspaces/base/terraform/keyvault.tf
@@ -35,7 +35,7 @@ resource "azurerm_private_endpoint" "kvpe" {
   }
 }
 
-/*
+
 data "azurerm_user_assigned_identity" "resource_processor_vmss_id" {
   name                = "id-vmss-${var.tre_id}"
   resource_group_name = "rg-${var.tre_id}"
@@ -47,4 +47,4 @@ resource "azurerm_key_vault_access_policy" "resource_processor" {
   object_id    = data.azurerm_user_assigned_identity.resource_processor_vmss_id.principal_id
 
   secret_permissions = ["Get", "List", "Set", "Delete"]
-} */
+}
