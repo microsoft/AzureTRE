bed4a6ec6d3ea1178690ff1214cdde693a937f06
James Griffin <me@JamesGriff.in>
2022-03-29 22:07:26 +0000

    Jetty configuration

--- templates/shared_services/sonatype-nexus/terraform/vm.tf
+++ templates/shared_services/sonatype-nexus/terraform/vm.tf
@@ -150,6 +150,7 @@ data "template_file" "nexus_config" {
     nexus_admin_password = random_password.nexus_admin_password.result
     msi_id = azurerm_user_assigned_identity.nexus_msi.id
     vault_name = data.azurerm_key_vault.kv.name
-    ssl_cert_name = "nexus-letsencrypt"
+    ssl_cert_name = data.azurerm_key_vault_certificate.nexus_cert.name
+    ssl_cert_password = data.azurerm_key_vault_secret.nexus_cert_password.value
   }
 }
