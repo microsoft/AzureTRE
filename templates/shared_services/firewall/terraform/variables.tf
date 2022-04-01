417ffaafb4a3bb7b4c3b1e0866ba3230b799ef1a
Tanya Borisova <tborisova@microsoft.com>
2022-04-01 12:16:01 +0100

    Remove location in favor of referencing resource group

--- templates/shared_services/firewall/terraform/variables.tf
+++ templates/shared_services/firewall/terraform/variables.tf
@@ -8,8 +8,3 @@ variable "stateful_resources_locked" {
   default     = true
   description = "Used to add locks on resources with state"
 }
-
-variable "location" {
-  type        = string
-  description = "Azure location (region) for deployment of core TRE services"
-}
