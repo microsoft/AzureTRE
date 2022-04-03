744f285453c8df789c87208c18e7713d5b444c67
Anat Balzam <anatbalzam@microsoft.com>
2022-04-03 10:06:14 +0000

    add vm_size to linux vm

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/locals.tf
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/locals.tf
@@ -10,6 +10,13 @@ locals {
   keyvault_name                  = lower("kv-${substr(local.workspace_resource_name_suffix, -20, -1)}")
   storage_name                   = lower(replace("stg${substr(local.workspace_resource_name_suffix, -8, -1)}", "-", ""))
   nexus_proxy_url                = "https://nexus-${var.tre_id}.azurewebsites.net"
+  vm_size = {
+    "1 CPU | 3.5GB RAM" = { value = "Standard_DS1_v2" },
+    "2 CPU | 7GB RAM"   = { value = "Standard_DS2_v2" },
+    "4 CPU | 14GB RAM"  = { value = "Standard_DS3_v2" },
+    "8 CPU | 28GB RAM"  = { value = "Standard_DS4_v2" },
+    "16 CPU | 56GB RAM" = { value = "Standard_DS5_v2" }
+  }
   image_ref = {
     "Ubuntu 18.04" = {
       "publisher"    = "canonical"
