f7ac643d3acee1638a1db677b6deeef5a7ef061f
James Griffin <me@JamesGriff.in>
2022-04-04 23:55:00 +0000

    Amended networking to use module

--- templates/workspaces/base/terraform/network/data.tf
+++ templates/workspaces/base/terraform/network/data.tf
@@ -81,3 +81,8 @@ data "azurerm_private_dns_zone" "postgres" {
   name                = "privatelink.postgres.database.azure.com"
   resource_group_name = local.core_resource_group_name
 }
+
+data "azurerm_private_dns_zone" "nexus" {
+  name                = "nexus-${var.tre_id}.${var.location}.cloudapp.azure.com"
+  resource_group_name = local.core_resource_group_name
+}
