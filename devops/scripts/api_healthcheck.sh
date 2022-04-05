7b42d642c1d46856a1c5266ccb38257ae8ca543e
Stuart Leeks <stuartle@microsoft.com>
2022-04-05 11:09:35 +0000

    Handle non-success error codes in healthcheck
    Only attempt to parse response as JSON for 200 success responses
    Avoids attempting to parse HTML from 502 Bad Gateway responses

--- devops/scripts/api_healthcheck.sh
+++ devops/scripts/api_healthcheck.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 set -e
 
-response=$(curl -k "https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/health")
+echo "Calling /health endpoint..."
+response_code=$(curl --insecure --silent --output api-response.txt --write-out "%{http_code}" "https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/health")
 
 echo "Got response from https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/health:"
-echo "$response"
+cat "api-response.txt"
 echo
 
-not_ok_count=$(echo "${response}"  | jq -r '[.services | .[] | select(.status!="OK")] | length')
+if [[ "$response_code" != "200" ]]; then
+  echo "*** ⚠️ API _not_ healthy ***"
+  echo "Non-success code returned: $response_code"
+  exit 1
+fi
+
+not_ok_count=$(jq -r '[.services | .[] | select(.status!="OK")] | length' < api-response.txt)
 
 if [[ "$not_ok_count" == "0" ]]; then
   echo "*** ✅ API healthy ***"
 else
   echo "*** ⚠️ API _not_ healthy ***"
   echo "Unhealthy services:"
-  echo "${response}"  | jq -r '[.services | .[] | select(.status!="OK")]'
+  jq -r '[.services | .[] | select(.status!="OK")]' < api-response.txt
   exit 1
 fi
