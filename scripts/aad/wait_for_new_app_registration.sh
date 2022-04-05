9482022d011f6bd06ad38b662eed7bc14355b982
Ross Smith <ross-p-smith@users.noreply.github.com>
2022-04-05 14:04:53 +0100

    AAD App reg script fixup (#1644)
    * Missing declare
    
    * Pulled functions out of mani aad script
    
    * Amending help
    
    * extra space

--- scripts/aad/wait_for_new_app_registration.sh
+++ scripts/aad/wait_for_new_app_registration.sh
@@ -3,21 +3,25 @@ set -e
 
 # This script polls looking for an app registration with the given ID.
 # If after the number of retries no app registration is found, the function exits.
-appId=$1
-retries=10
-counter=0
-objectId=$(az ad app list --filter "appId eq '${appId}'" --query '[0].objectId' --output tsv)
+function wait_for_new_app_registration()
+{
+  appId=$1
+  retries=10
+  counter=0
 
-while [[ -z $objectId && $counter -lt $retries ]]; do
-    counter=$((counter+1))
-    echo "Waiting for app registration with ID ${appId} to show up (${counter}/${retries})..."
-    sleep 5
-    objectId=$(az ad app list --filter "appId eq '${appId}'" --query '[0].objectId' --output tsv)
-done
+  objectId=$(az ad app list --filter "appId eq '${appId}'" --query '[0].objectId' --output tsv)
 
-if [[ -z $objectId ]]; then
-    echo "Failed"
-    exit 1
-fi
+  while [[ -z $objectId && $counter -lt $retries ]]; do
+      counter=$((counter+1))
+      echo "Waiting for app registration with ID ${appId} to show up (${counter}/${retries})..."
+      sleep 5
+      objectId=$(az ad app list --filter "appId eq '${appId}'" --query '[0].objectId' --output tsv)
+  done
 
-echo "App registration with ID ${appId} found"
+  if [[ -z $objectId ]]; then
+      echo "Failed"
+      exit 1
+  fi
+
+  echo "App registration with ID ${appId} found"
+}
