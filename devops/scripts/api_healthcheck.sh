bc915c3056e41d9f6aa74a9d20b7b820071333da
Stuart Leeks <stuartle@microsoft.com>
2022-04-06 07:46:25 +0000

    Update healthcheck script to use tmp output folder
    Set .gitignore in tmp folder (and remove from main gitignore)
    Tidy handling of showing output

--- devops/scripts/api_healthcheck.sh
+++ devops/scripts/api_healthcheck.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 set -e
 
-echo "Calling /health endpoint..."
-response_code=$(curl --insecure --silent --output api-response.txt --write-out "%{http_code}" "https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/health")
+# Get the directory that this script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Create a .gitignore'd  directory for temp output
+mkdir -p "$DIR/script_tmp"
+echo '*' > "$DIR/script_tmp/.gitignore"
+api_response_file="$DIR/script_tmp/api_response.txt"
 
-echo "Got response from https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/health:"
-cat "api-response.txt"
+echo "Calling /health endpoint..."
+response_code=$(curl --insecure --silent --output "$api_response_file" --write-out "%{http_code}" "https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/health")
+
 echo
 
 if [[ "$response_code" != "200" ]]; then
   echo "*** ⚠️ API _not_ healthy ***"
   echo "Non-success code returned: $response_code"
+  echo "Response:"
+  cat "$api_response_file"
   exit 1
 fi
 
-not_ok_count=$(jq -r '[.services | .[] | select(.status!="OK")] | length' < api-response.txt)
+not_ok_count=$(jq -r '[.services | .[] | select(.status!="OK")] | length' < "$api_response_file")
 
 if [[ "$not_ok_count" == "0" ]]; then
   echo "*** ✅ API healthy ***"
 else
   echo "*** ⚠️ API _not_ healthy ***"
   echo "Unhealthy services:"
-  jq -r '[.services | .[] | select(.status!="OK")]' < api-response.txt
+  jq -r '[.services | .[] | select(.status!="OK")]' < "$api_response_file"
   exit 1
 fi
