3c5ede780571d9cbde002a59a977ae4081996b4b
Stuart Leeks <stuartle@microsoft.com>
2022-04-06 15:29:56 +0000

    Add tests for labelAsExternalIfAuthorDoesNotHaveWriteAccess

--- .github/scripts/build.test.js
+++ .github/scripts/build.test.js
@@ -1,6 +1,6 @@
-const { getCommandFromComment } = require('./build.js')
+const { getCommandFromComment, labelAsExternalIfAuthorDoesNotHaveWriteAccess } = require('./build.js')
 
-// run
+mockGithubRestIssuesAddLabels = jest.fn();
 
 const github = {
   request: async (route, data) => {
@@ -12,28 +12,36 @@ const github = {
       } else {
         throw {
           status: 404,
-        }
+        };
       }
     }
-  }
-};
-function createCommentContext(username, commentBody) {
-  return {
-    payload: {
-      comment: {
-        user: {
-          login: username,
-        },
-        body: commentBody,
-      },
-      repository: {
-        full_name: 'someOwner/SomeRepo'
-      }
+  },
+  rest: {
+    issues: {
+      addLabels: mockGithubRestIssuesAddLabels,
     },
-  };
-}
+  },
+};
+
+const core = {};
 
 describe('getCommandFromComment', () => {
+
+  function createCommentContext(username, commentBody) {
+    return {
+      payload: {
+        comment: {
+          user: {
+            login: username,
+          },
+          body: commentBody,
+        },
+        repository: {
+          full_name: 'someOwner/SomeRepo'
+        }
+      },
+    };
+  }
 
   describe('with non-contributor', () => {
     test(`should return 'none' for '/test'`, async () => {
@@ -44,12 +52,12 @@ describe('getCommandFromComment', () => {
   });
 
   describe('with contributor', () => {
-
     test(`should return 'none' if doesn't start with '/'`, async () => {
       context = createCommentContext('admin', 'foo');
       var command = await getCommandFromComment({ context, github })
       expect(command).toBe('none');
     });
+
 
     describe('and single line comments', () => {
       test(`should return 'run-tests' for '/test'`, async () => {
@@ -57,27 +65,32 @@ describe('getCommandFromComment', () => {
         var command = await getCommandFromComment({ context, github })
         expect(command).toBe('run-tests');
       });
+
       test(`should return 'run-tests-extended' for '/test-extended'`, async () => {
         context = createCommentContext('admin', '/test-extended');
         var command = await getCommandFromComment({ context, github })
         expect(command).toBe('run-tests-extended');
       });
+
       test(`should return 'test-force-approve' for '/test-force-approve'`, async () => {
         context = createCommentContext('admin', '/test-force-approve');
         var command = await getCommandFromComment({ context, github })
         expect(command).toBe('test-force-approve');
       });
+
       test(`should return 'test-destroy-env' for '/test-destroy-env'`, async () => {
         context = createCommentContext('admin', '/test-destroy-env');
         var command = await getCommandFromComment({ context, github })
         expect(command).toBe('test-destroy-env');
       });
+
       test(`should return 'show-help' for '/help'`, async () => {
         context = createCommentContext('admin', '/help');
         var command = await getCommandFromComment({ context, github })
         expect(command).toBe('show-help');
       });
     });
+
 
     describe('and multi-line comments', () => {
       test(`should return 'run-tests' if first line of comment is '/test'`, async () => {
@@ -87,6 +100,7 @@ goes here`);
         var command = await getCommandFromComment({ context, github })
         expect(command).toBe('run-tests');
       });
+
       test(`should return 'none' if first line of comment is a command even if later lines contain '/test'`, async () => {
         context = createCommentContext('admin', `Non-command comment
 /test
@@ -98,4 +112,41 @@ goes here`);
     });
 
   });
+});
+
+
+describe('labelAsExternalIfAuthorDoesNotHaveWriteAccess', () => {
+
+  function createPullRequestContext(username, pullRequestNumber) {
+    return {
+      payload: {
+        pull_request: {
+          user: {
+            login: username,
+          },
+          number: pullRequestNumber,
+        },
+        repository: {
+          full_name: 'someOwner/SomeRepo'
+        }
+      },
+      repo : {
+        owner: 'someOwner',
+        repo: 'someRepo'
+      }
+    };
+  }
+
+  test(`should return not apply the 'external' label for contributor author`, async () => {
+    context = createPullRequestContext('admin', 123);
+    await labelAsExternalIfAuthorDoesNotHaveWriteAccess({ core, context, github })
+    expect(mockGithubRestIssuesAddLabels).toHaveBeenCalledTimes(0); // shouldn't set the label for contributor
+  });
+
+  test(`should apply the 'external' label for non-contributor author`, async () => {
+    context = createPullRequestContext('non-contributor', 123);
+    await labelAsExternalIfAuthorDoesNotHaveWriteAccess({ core, context, github })
+    expect(mockGithubRestIssuesAddLabels).toHaveBeenCalled(); // should set the label for non-contributor
+  });
+
 });
