dc4d697bf1ad54ccf7573b5eee479bbd6906b43f
Tanya Borisova <tborisova@microsoft.com>
2022-04-12 17:48:54 +0100

    WIP: working on e2e test

--- e2e_tests/test_shared_services.py
+++ e2e_tests/test_shared_services.py
@@ -1,12 +1,6 @@
 import pytest
 
-from httpx import
-from httpx import AsyncClient
-from starlette import status
-
-import config
-from urllib.parse import urljoin
-from helpers import get_auth_header, get_template
+from helpers import post_resource
 from resources import strings
 
 
@@ -16,18 +10,16 @@ shared_service_templates = [
     (strings.NEXUS_SHARED_SERVICE),
 ]
 
-@pytest.mark.smoke
+@pytest.mark.extended
 @pytest.mark.parametrize("shared_service_template_name", shared_service_templates)
-async def test_patch_shared_service(template_name, admin_token, verify)
-    async with AsyncClient(verify=verify) as client:
-        patch_payload = {
-            "properties": {
-                "display_name": "Updated Guac Name",
-            }
+async def test_patch_shared_service(template_name, admin_token, verify):
+    patch_payload = {
+        "properties": {
+            "display_name": "Updated Firewall Name",
         }
+    }
 
-        # TODO: my API is stupid and can't get an item by name :(
-        shared_service_path = urljoin('/api/')
+    shared_service_path = f'/shared-services/{template_name}'
 
-        # TODO: path has shared service in it?
-        await post_resource(patch_payload, f'/api{workspace_service_path}', 'shared_service', admin_token, None, verify, method="PATCH")
+    # TODO: path has shared service in it?
+    await post_resource(patch_payload, f'/api{shared_service_path}', 'shared_service', admin_token, None, verify, method="PATCH")
