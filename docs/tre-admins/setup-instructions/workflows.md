e779d396c9ded15ed7f86a5058d3d8401fe44e80
Anat Balzam <anatbalzam@microsoft.com>
2022-04-12 12:52:33 +0000

    TEST_WORKSPACE_SECRET

--- docs/tre-admins/setup-instructions/workflows.md
+++ docs/tre-admins/setup-instructions/workflows.md
@@ -102,7 +102,7 @@ Configure the TEST_WORKSPACE_APP_ID repository secret
 | <div style="width: 230px">Secret name</div> | Description |
 | ----------- | ----------- |
 | `TEST_WORKSPACE_APP_ID` | The application (client) ID of the Workspaces app. |
-| `TEST_WORKSPACE_SECRET` | The application (client) secret of the Workspaces app. |
+| `TEST_WORKSPACE_APP_SECRET` | The application (client) secret of the Workspaces app. |
 
 ### Create a Teams Webhook for deployment notifications
 
--- templates/core/terraform/outputs.sh
+++ templates/core/terraform/outputs.sh
@@ -22,6 +22,7 @@ source ../.env
 # Add a few extra values to the file to help us (i.e. for local debugging api_app and resource processor)
 echo "SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE='sb-${TRE_ID}.servicebus.windows.net'" >> ../private.env
 echo "TEST_WORKSPACE_APP_ID='${WORKSPACE_API_CLIENT_ID}'" >> ../private.env
+echo "TEST_WORKSPACE_APP_SECRET='${WORKSPACE_API_CLIENT_SECRET}'" >> ../private.env
 
 # These next ones from Check Dependencies
 echo "SUBSCRIPTION_ID='${SUB_ID}'" >> ../private.env
