653ad53c05af4f884b8f25c82ca88f4d3c3157ed
Tanya Borisova <tborisova@microsoft.com>
2022-04-12 12:46:24 +0100

    Fix test

--- api_app/tests_ma/test_db/test_repositories/test_shared_service_repository.py
+++ api_app/tests_ma/test_db/test_repositories/test_shared_service_repository.py
@@ -86,7 +86,6 @@ def test_get_active_shared_services_for_shared_queries_db(shared_service_repo):
 
     shared_service_repo.query.assert_called_once_with(query=SharedServiceRepository.active_shared_services_query())
 
-
 @patch('db.repositories.shared_services.SharedServiceRepository.validate_input_against_template')
 @patch('core.config.TRE_ID', "1234")
 def test_create_shared_service_item_creates_a_shared_with_the_right_values(validate_input_mock, shared_service_repo, basic_shared_service_request, basic_shared_service_template):
@@ -105,18 +104,19 @@ def test_create_shared_service_item_creates_a_shared_with_the_right_values(valid
 
 @patch('db.repositories.shared_services.SharedServiceRepository.validate_input_against_template')
 @patch('core.config.TRE_ID', "1234")
-def test_create_shared_service_item_with_the_same_name_twice_fails(validate_input_mock, shared_service_repo, basic_shared_service_request, basic_shared_service_template):
+def test_create_shared_service_item_with_the_same_name_twice_fails(validate_input_mock, shared_service_repo, basic_shared_service_request):
     shared_service_to_create = basic_shared_service_request
 
-    # resource_template = basic_shared_service_template
     validate_input_mock.return_value = basic_shared_service_request.templateName
 
-    shared_service = shared_service_repo.create_shared_service_item(shared_service_to_create)
+    shared_service = shared_service_repo.create_shared_service_item(basic_shared_service_request)
+    shared_service_repo.save_item(shared_service)
 
-    assert shared_service.templateName == basic_shared_service_request.templateName
-    assert shared_service.resourceType == ResourceType.SharedService
+    shared_service_repo.query = MagicMock()
+    shared_service_repo.query.return_value = [shared_service]
 
-    shared_service = shared_service_repo.create_shared_service_item(shared_service_to_create)
+    with pytest.raises(DuplicateEntity):
+        shared_service = shared_service_repo.create_shared_service_item(shared_service_to_create)
 
 
 @patch('db.repositories.shared_services.SharedServiceRepository.validate_input_against_template', side_effect=ValueError)
