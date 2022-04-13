41d1df81e4ed053520a9666f36388ad86909620c
Tanya Borisova <tborisova@microsoft.com>
2022-04-14 00:36:14 +0100

    Finish test for shared services

--- e2e_tests/test_shared_services.py
+++ e2e_tests/test_shared_services.py
@@ -1,26 +1,43 @@
 import pytest
 
-from helpers import post_resource
+from helpers import disable_and_delete_resource, post_resource
 from resources import strings
 
+@pytest.mark.extended
+async def test_patch_firewall(admin_token, verify):
+    template_name = "tre-shared-service-firewall"
 
-shared_service_templates = [
-    (strings.FIREWALL_SHARED_SERVICE),
+    patch_payload = {
+        "properties": {
+            "display_name": "TEST",
+        },
+        "templateName": template_name,
+    }
+
+    shared_service_path = f'/shared-services/{template_name}'
+
+    await post_resource(patch_payload, f'/api{shared_service_path}', 'shared_service', admin_token, None, verify, method="PATCH")
+
+
+shared_service_templates_to_create = [
     (strings.GITEA_SHARED_SERVICE),
     (strings.NEXUS_SHARED_SERVICE),
 ]
 
 @pytest.mark.extended
-@pytest.mark.parametrize("shared_service_template_name", shared_service_templates)
-async def test_patch_shared_service(shared_service_template_name, admin_token, verify):
-    patch_payload = {
+@pytest.mark.timeout(30 * 60)
+@pytest.mark.parametrize("template_name", shared_service_templates_to_create)
+async def test_create_shared_service(template_name, admin_token, workspace_owner_token, verify) -> None:
+    post_payload = {
+        "templateName": template_name,
         "properties": {
-            "display_name": "TEST",
-        },
-        "templateName": shared_service_template_name,
+            "display_name": f"Shared service {template_name}",
+            "description": f"{template_name} deployed via e2e tests"
+        }
     }
 
-    shared_service_path = f'/shared-services/{shared_service_template_name}'
+    shared_service_path = f'/shared-services/{template_name}'
 
-    # TODO: path has shared service in it?
-    await post_resource(patch_payload, f'/api{shared_service_path}', 'shared_service', admin_token, None, verify, method="PATCH")
+    await post_resource(post_payload, f'/api{shared_service_path}', 'shared_service', admin_token, None, verify)
+
+    await disable_and_delete_resource(f'/api{shared_service_path}', 'workspace_service', workspace_owner_token, None, verify)
