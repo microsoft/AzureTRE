93de622f97090c1ffde1eaa9c5f23fdebdfb2a9a
ross-p-smith <rosmith@microsoft.com>
2022-04-20 10:06:50 +0000

    Missing Api Id returns 500

--- api_app/services/authentication.py
+++ api_app/services/authentication.py
@@ -7,11 +7,10 @@ from services.aad_authentication import AzureADAuthorization
 from services.access_service import AccessService, AuthConfigValidationError
 
 
-def extract_auth_information(app_id: str) -> dict:
+def extract_auth_information(workspace_creation_properties: dict) -> dict:
     access_service = get_access_service('AAD')
     try:
-        auth_config = {'app_id': app_id}
-        return access_service.extract_workspace_auth_information(auth_config)
+        return access_service.extract_workspace_auth_information(workspace_creation_properties)
     except AuthConfigValidationError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
