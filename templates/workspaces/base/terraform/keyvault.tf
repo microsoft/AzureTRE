ae4b4fd9178b1ed64fe0bf7288f85f9fec1a4856
ross-p-smith <rosmith@microsoft.com>
2022-04-20 13:29:43 +0000

    Upgrade workspace AzureRM provider

--- templates/workspaces/base/terraform/keyvault.tf
+++ templates/workspaces/base/terraform/keyvault.tf
@@ -3,12 +3,12 @@ resource "azurerm_key_vault" "kv" {
   location                 = azurerm_resource_group.ws.location
   resource_group_name      = azurerm_resource_group.ws.name
   sku_name                 = "standard"
-  purge_protection_enabled = true
+  purge_protection_enabled = var.keyvault_purge_protection_enabled
   tenant_id                = data.azurerm_client_config.current.tenant_id
 
   network_acls {
     bypass         = "AzureServices"
-    default_action = "Deny"
+    default_action = var.enable_local_debugging ? "Allow" : "Deny"
   }
 
   lifecycle { ignore_changes = [tags] }
@@ -50,5 +50,15 @@ resource "azurerm_key_vault_access_policy" "resource_processor" {
   tenant_id    = data.azurerm_user_assigned_identity.resource_processor_vmss_id.tenant_id
   object_id    = data.azurerm_user_assigned_identity.resource_processor_vmss_id.principal_id
 
-  secret_permissions = ["Get", "List", "Set", "Delete"]
+  secret_permissions = ["Get", "List", "Set", "Delete", "Purge"]
+}
+
+# If running the terraform locally
+resource "azurerm_key_vault_access_policy" "deployer" {
+  count        = var.enable_local_debugging ? 1 : 0
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = ["Get", "List", "Set", "Delete", "Purge"]
 }
