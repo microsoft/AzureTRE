22258251bdd1f5110572cc147594adb7ef897c7d
Tanya Borisova <tborisova@microsoft.com>
2022-04-21 09:51:06 +0000

    Revert change from different PR

--- api_app/api/routes/shared_services.py
+++ api_app/api/routes/shared_services.py
@@ -58,23 +58,14 @@ async def create_shared_service(
     return OperationInResponse(operation=operation)
 
 
-@shared_services_router.patch("/shared-services/{shared_service_id}", status_code=status.HTTP_202_ACCEPTED, response_model=OperationInResponse, name=strings.API_UPDATE_SHARED_SERVICE, dependencies=[Depends(get_current_admin_user), Depends(get_shared_service_by_id_from_path)])
-async def patch_shared_service(
-    shared_service_patch: ResourcePatch,
-    response: Response,
-    user=Depends(get_current_admin_user),
-    shared_service_repo=Depends(get_repository(SharedServiceRepository)),
-    shared_service=Depends(get_shared_service_by_id_from_path),
-    resource_template_repo=Depends(get_repository(ResourceTemplateRepository)),
-    operations_repo=Depends(get_repository(OperationRepository)),
-    etag: str = Header(None)
-) -> OperationInResponse:
+@shared_services_router.patch("/shared-services/{shared_service_id}", response_model=SharedServiceInResponse, name=strings.API_UPDATE_SHARED_SERVICE, dependencies=[Depends(get_current_admin_user), Depends(get_shared_service_by_id_from_path)])
+async def patch_shared_service(shared_service_patch: ResourcePatch, response: Response, user=Depends(get_current_admin_user), shared_service_repo=Depends(get_repository(SharedServiceRepository)), shared_service=Depends(get_shared_service_by_id_from_path), resource_template_repo=Depends(get_repository(ResourceTemplateRepository)), operations_repo=Depends(get_repository(OperationRepository)), etag: str = Header(None)) -> SharedServiceInResponse:
     check_for_etag(etag)
     try:
         patched_shared_service = shared_service_repo.patch_shared_service(shared_service, shared_service_patch, etag, resource_template_repo, user)
         operation = await send_resource_request_message(resource=patched_shared_service, operations_repo=operations_repo, user=user, action=RequestAction.Upgrade)
         response.headers["Location"] = construct_location_header(operation)
-        return OperationInResponse(operation=operation)
+        return SharedServiceInResponse(operation=operation)
     except CosmosAccessConditionFailedError:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=strings.ETAG_CONFLICT)
     except ValidationError as v:
