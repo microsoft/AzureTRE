b1c04a1e9576fe58acfd5362e3362c3497ca7cb1
Ross Smith <ross-p-smith@users.noreply.github.com>
2022-04-25 13:56:40 +0100

    Workspace auth migration script (#1726)
    * workspace auth migration script
    
    * Add semantic version check to run later
    
    * Add the db-migrate as part of the build

--- scripts/db_migrations.py
+++ scripts/db_migrations.py
@@ -3,9 +3,14 @@
 from datetime import datetime
 import os
 from azure.cosmos.cosmos_client import CosmosClient
+from azure.mgmt.cosmosdb import CosmosDBManagementClient
 from azure.cosmos import PartitionKey
+from azure.identity import DefaultAzureCredential
 import json
+import semantic_version
 import uuid
+
+STATE_STORE_DATABASE = "AzureTRE"
 
 
 class TRECosmosDBMigrations:
@@ -13,9 +18,20 @@ class TRECosmosDBMigrations:
     def __init__(self):
 
         url = os.environ['STATE_STORE_ENDPOINT']
-        key = os.environ['STATE_STORE_KEY']
+        key = self.get_store_key()
         self.client = CosmosClient(url=url, credential=key)
-        self.database = self.client.get_database_client(database=os.environ['COSMOSDB_DATABASE_NAME'])
+        self.database = self.client.get_database_client(STATE_STORE_DATABASE)
+
+    def get_store_key(self) -> str:
+        if 'STATE_STORE_KEY' in os.environ:
+            primary_master_key = os.getenv('STATE_STORE_KEY')
+        else:
+            credential = DefaultAzureCredential()
+            cosmosdb_client = CosmosDBManagementClient(credential, subscription_id=os.environ['SUBSCRIPTION_ID'])
+            database_keys = cosmosdb_client.database_accounts.list_keys(resource_group_name=os.environ['RESOURCE_GROUP_NAME'], account_name=os.environ['COSMOSDB_ACCOUNT_NAME'])
+            primary_master_key = database_keys.primary_master_key
+
+        return primary_master_key
 
     def renameCosmosDBFields(self, container_name, old_field_name, new_field_name):
 
@@ -57,6 +73,25 @@ class TRECosmosDBMigrations:
                 resources_container.upsert_item(item)
                 print(f'Moved deployment from resource id {item["id"]} to operations')
 
+    def moveAuthInformationToProperties(self, resources_container_name):
+        resources_container = self.database.get_container_client(resources_container_name)
+
+        for item in resources_container.query_items(query='SELECT * FROM c', enable_cross_partition_query=True):
+            template_version = semantic_version.Version(item["templateVersion"])
+            if (template_version > semantic_version.Version('0.2.0') and "authInformation" in item):
+                print(f'Found workspace {item["id"]} that needs migrating')
+
+                # Rename app_id to be client_id
+                item["properties"]["client_id"] = item["properties"]["app_id"]
+                del item["properties"]["app_id"]
+                del item["authInformation"]["app_id"]
+
+                # merge authInformation into properties
+                item["properties"] = {**item["authInformation"], **item["properties"]}
+                del item["authInformation"]
+                resources_container.upsert_item(item)
+                print(f'Upgraded authentication info for workspace id {item["id"]}')
+
 
 def main():
     migrations = TRECosmosDBMigrations()
@@ -72,6 +107,9 @@ def main():
 
     # Operations History
     migrations.moveDeploymentsToOperations("Resources", "Operations")
+
+    # Authentication needs to be in properties so we can update them.
+    migrations.moveAuthInformationToProperties("Resources")
 
 
 if __name__ == "__main__":
