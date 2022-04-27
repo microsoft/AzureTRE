87c73d2424c3ad1e188340a4a472c460e5029979
Stuart Leeks <stuartle@microsoft.com>
2022-04-27 13:46:17 +0100

    Require commit SHA for /test and /test-extended on external PRs (#1731)
    * Add `/test <sha` command
    
    Add test helpers to improve test readability
    
    * Make command tests more consistent
    
    * Add comment on running test, update run-test.sh
    
    * Extract /test command handling to separate function
    
    * Add SHA validation for external PRs for /test-extended
    
    * Update maintainers.md with /test sha requirements
    
    * Add tests for non-mergeable PRs
    
    * Handle extra spaces between /test and sha
    
    When copy/pasting SHA in UI, it's easy to add an extra space
    This change makes the pr-bot tolerant of that
    
    * PR feedback

--- .github/scripts/run-tests.sh
+++ .github/scripts/run-tests.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
-set -e
-
+set -o errexit
+set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [[ $(command -v node > /dev/null; echo $?) == 0 ]]; then
-  echo "node already installed"
-else
-  echo "node not found - installing..."
-  "$DIR/install-node.sh"
+function usage() {
+    cat <<USAGE
+
+    This script runs the GitHub action linter and unit tests for build.js
+    Dependencies (e.g. node, actionlint) will be installed if not present
+
+    Usage: $0 [--watch]
+
+    Options:
+        --watch   Run tests in watch mode
+USAGE
+    exit 1
+}
+
+
+watch_tests=false
+while [ "$1" != "" ]; do
+    case $1 in
+    --watch)
+        watch_tests=true
+        ;;
+    *)
+        echo "Unexpected argument: '$1'"
+        usage
+        ;;
+    esac
+
+    if [[ -z "$2" ]]; then
+      # if no more args then stop processing
+      break
+    fi
+
+    shift # remove the current value for `$1` and use the next
+done
+test_command="test"
+if ${watch_tests}
+then
+  test_command="test-watch"
 fi
 
-echo "Running JavaScript build tests..."
-(cd "$DIR" && yarn test)
 
 script_temp_dir="$DIR/script_temp"
 if [[ -x "$script_temp_dir/actionlint" ]]; then
@@ -26,4 +57,16 @@ else
 fi
 echo "Running actionlint..."
 "$script_temp_dir/actionlint"
+
+
+if [[ $(command -v node > /dev/null; echo $?) == 0 ]]; then
+  echo "node already installed"
+else
+  echo "node not found - installing..."
+  "$DIR/install-node.sh"
+fi
+
+echo "Running JavaScript build tests..."
+(cd "$DIR" && yarn install && yarn "$test_command")
+
 echo "Tests complete"
