93e1a7a61643c4ac81d612e738f8f4094723e7b7
ross-p-smith <rosmith@microsoft.com>
2022-05-05 12:42:23 +0000

    Return list of migrations

--- api_app/db/migrations/shared_services.py
+++ api_app/db/migrations/shared_services.py
@@ -8,11 +8,15 @@ class SharedServiceMigration(SharedServiceRepository):
     def __init__(self, client: CosmosClient):
         super().__init__(client)
 
-    async def deleteDuplicatedSharedServices(self):
+    def deleteDuplicatedSharedServices(self) -> bool:
         template_names = ['tre-shared-service-firewall', 'tre-shared-service-nexus', 'tre-shared-service-gitea']
 
+        migrated = False
         for template_name in template_names:
             for item in self.query(query=f'SELECT * FROM c WHERE c.resourceType = "shared-service" AND c.templateName = "{template_name}" \
-                                                                ORDER BY c.updatedWhen ASC OFFSET 1 LIMIT 10000', enable_cross_partition_query=True):
+                                                                ORDER BY c.updatedWhen ASC OFFSET 1 LIMIT 10000'):
                 logging.INFO(f"Deleting element {item}")
                 self.delete_item(item, partition_key=item["id"])
+                migrated = True
+
+        return migrated
--- api_app/db/migrations/workspaces.py
+++ api_app/db/migrations/workspaces.py
@@ -9,10 +9,11 @@ class WorkspaceMigration(WorkspaceRepository):
     def __init__(self, client: CosmosClient):
         super().__init__(client)
 
-    async def moveAuthInformationToProperties(self):
+    def moveAuthInformationToProperties(self) -> bool:
+        migrated = False
         for item in self.query(query=WorkspaceRepository.workspaces_query_string()):
             template_version = semantic_version.Version(item["templateVersion"])
-            if (template_version > semantic_version.Version('0.3.0') and "authInformation" in item):
+            if (template_version < semantic_version.Version('0.2.5') and "authInformation" in item):
                 logging.INFO(f'Found workspace {item["id"]} that needs migrating')
 
                 # Rename app_id to be client_id
@@ -28,3 +29,6 @@ class WorkspaceMigration(WorkspaceRepository):
                 del item["authInformation"]
                 self.update_item_dict(item)
                 logging.INFO(f'Upgraded authentication info for workspace id {item["id"]}')
+                migrated = True
+
+            return migrated
