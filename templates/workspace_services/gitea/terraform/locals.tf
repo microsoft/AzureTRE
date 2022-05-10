710b2155db221bf143a57e972d2cea26db663425
Ross Smith <ross-p-smith@users.noreply.github.com>
2022-05-10 16:15:42 +0100

    Gitea/Guacamole should be able to access AAD_TENANT_ID (#1798)
    * Add auth-tenant-id to ws keyvault
    
    * Bump versions
    
    * linting
    
    * re-instating the deploy/destroy files
    
    * Linting
    
    * Update deploy.sh
    
    * Update destroy.sh

--- templates/workspace_services/gitea/terraform/locals.tf
+++ templates/workspace_services/gitea/terraform/locals.tf
@@ -7,7 +7,7 @@ locals {
   webapp_name                    = "gitea-${local.service_resource_name_suffix}"
   core_vnet                      = "vnet-${var.tre_id}"
   core_resource_group_name       = "rg-${var.tre_id}"
-  aad_tenant_id                  = data.azurerm_app_service.api_core.app_settings["AAD_TENANT_ID"]
+  aad_tenant_id                  = data.azurerm_key_vault_secret.aad_tenant_id.value
   issuer                         = "https://login.microsoftonline.com/${local.aad_tenant_id}/v2.0"
   keyvault_name                  = lower("kv-${substr(local.workspace_resource_name_suffix, -20, -1)}")
   version                        = replace(replace(replace(data.local_file.version.content, "__version__ = \"", ""), "\"", ""), "\n", "")
