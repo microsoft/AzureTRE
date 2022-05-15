e901b3e60d0edcd611d3a299c9d8186a24ef8f72
Elad <13205761+eladiw@users.noreply.github.com>
2022-05-15 21:46:13 +0300

    fix

--- templates/core/terraform/airlock/service-bus.tf
+++ templates/core/terraform/airlock/service-bus.tf
@@ -46,5 +46,26 @@ resource "azurerm_servicebus_queue" "scan_result_queue" {
   enable_partitioning = false
 }
 
+resource "azurerm_servicebus_queue" "accepted_import" {
+  name         = "accepted_import_blob_created"
+  namespace_id = azurerm_servicebus_namespace.airlock_sb.id
+
+  enable_partitioning = false
+}
+
+resource "azurerm_servicebus_queue" "inprogress_export" {
+  name         = "inprogress_export_blob_created"
+  namespace_id = azurerm_servicebus_namespace.airlock_sb.id
+
+  enable_partitioning = false
+}
+
+resource "azurerm_servicebus_queue" "rejected_export" {
+  name         = "rejected_export_blob_created"
+  namespace_id = azurerm_servicebus_namespace.airlock_sb.id
+
+  enable_partitioning = false
+}
+
 
 
