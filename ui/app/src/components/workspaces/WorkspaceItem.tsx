785cea59093ca11ea85e840c25361eb9518ef3c4
Christofer Löf <166592+christoferlof@users.noreply.github.com>
2022-05-19 16:08:41 +0200

    Initial ResourcePropertyPanel (#1869)
    * ResourcePropertyPanel implemented to display resource properties in a consistent manner.
    * ResourcePropertyPanel included in WorkspaceItem, WorkspaceServiceItem and UserResourceItem
    * Property values starting with https:// rendered as links.
    * updatedWhen formatted using moment.

--- ui/app/src/components/workspaces/WorkspaceItem.tsx
+++ ui/app/src/components/workspaces/WorkspaceItem.tsx
@@ -4,6 +4,7 @@ import { WorkspaceRoleName } from '../../models/roleNames';
 import { Workspace } from '../../models/workspace';
 import { SecuredByRole } from '../shared/SecuredByRole';
 import { ResourceDebug } from '../shared/ResourceDebug';
+import { ResourcePropertyPanel } from '../shared/ResourcePropertyPanel';
 import { WorkspaceRolesContext } from './WorkspaceRolesContext';
 
 // TODO:
@@ -27,7 +28,8 @@ export const WorkspaceItem: React.FunctionComponent<WorkspaceItemProps> = (props
             'data-title': 'Overview',
           }}
         >
-          <h3>--Workspace details panel here--</h3>
+          
+          <ResourcePropertyPanel resource={props.workspace}/>
 
           <h3>Roles:</h3>
           <ul>
