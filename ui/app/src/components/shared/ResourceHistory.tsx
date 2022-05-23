be092c30e5e8bc6da5dba92317f33cc9060c50be
ross-p-smith <rosmith@microsoft.com>
2022-05-23 22:48:36 +0000

    DetailsList

--- ui/app/src/components/shared/ResourceHistory.tsx
+++ ui/app/src/components/shared/ResourceHistory.tsx
@@ -1,72 +1,115 @@
 import * as React from 'react';
-import { DefaultPalette, Stack, Text } from "@fluentui/react";
-import { List } from '@fluentui/react/lib/List';
+import { DetailsList, DetailsListLayoutMode, IColumn, Text } from "@fluentui/react";
+import { CheckboxVisibility } from "@fluentui/react/lib/DetailsList";
 import { HistoryItem } from '../../models/resource';
 import { ResourcePropertyPanelItem } from './ResourcePropertyPanel';
+import moment from 'moment';
 
-interface ResourceHistoryProps {
-  history: Array<HistoryItem>
+interface IResourceHistoryProps {
+  history: HistoryItem[]
 }
 
-const headerStyles: React.CSSProperties = {
-  padding: '5px 10px',
-  fontSize: '1.3rem',
-};
+export const ResourceHistory: React.FunctionComponent<IResourceHistoryProps> = (props: IResourceHistoryProps) => {
 
-const bodyStyles: React.CSSProperties = {
-  borderBottom: '1px #ccc solid',
-  padding: '5px 10px',
-  minHeight: '70px'
-}
-
-const footerStyles: React.CSSProperties = {
-  backgroundColor: DefaultPalette.white,
-  padding: '5px 10px',
-  minHeight: '15px'
-}
-
-const onRenderCell = (item: any, index: number | undefined): JSX.Element => {
-  var propertyBag = {...item.properties}
-  delete propertyBag['display_name']
-  delete propertyBag['description']
-
-  function userFriendlyKey(key: String){
+  function userFriendlyKey(key: string){
     let friendlyKey = key.replaceAll('_', ' ');
     return friendlyKey.charAt(0).toUpperCase() + friendlyKey.slice(1).toLowerCase();
   }
 
-  return (
-  <Stack wrap horizontal>
-    <Stack grow>
-    <Stack.Item style={headerStyles}>
-      <Text>{item.properties.display_name}</Text>
-      <Text>{item.updatedWhen}</Text>
-      <Text>{item.resourceVersion}</Text>
-      <Text>{item.isEnabled}</Text>
-    </Stack.Item>
-    <Stack.Item grow={3} style={bodyStyles}>
-      <Text>
-      {item.properties.description}
-      {
-        Object.keys(propertyBag).map((key) => {
-            let val = (propertyBag as any)[key].toString();
-            return (
-              <ResourcePropertyPanelItem header={userFriendlyKey(key)} val={val} key={key}/>
-            )
-        })
-      }
-      </Text>
-    </Stack.Item>
-    <Stack.Item style={footerStyles}>
-          &nbsp;
-        </Stack.Item>
-    </Stack>
-  </Stack>
-  );
-};
+  const columns: IColumn[] = [
+    {
+      key: 'column1',
+      name: 'Version',
+      fieldName: 'resourceVersion',
+      minWidth: 35,
+      maxWidth: 40,
+      isRowHeader: true,
+      isResizable: true,
+      isSorted: true,
+      isSortedDescending: true,
+      data: 'number',
+      isPadded: true,
+      onRender: (item: HistoryItem) => (
+        <Text>{item.resourceVersion}</Text>
+      ),
+    },
+    {
+      key: 'column2',
+      name: 'Name',
+      fieldName: 'name',
+      minWidth: 100,
+      maxWidth: 150,
+      isResizable: true,
+      isMultiline: true,
+      data: 'number',
+      onRender: (item: HistoryItem) => {
+        return <Text>{item.properties.display_name} ({item.properties.description})</Text>;
+      },
+      isPadded: true,
+    },
+    {
+      key: 'column3',
+      name: 'Properties',
+      fieldName: 'properties',
+      minWidth: 300,
+      maxWidth: 350,
+      isResizable: true,
+      isCollapsible: true,
+      isMultiline: true,
+      data: 'string',
+      onRender: (item: HistoryItem) => {
+        var propertyBag = {...item.properties}
+        delete propertyBag['display_name']
+        delete propertyBag['description']
 
-export const ResourceHistory: React.FunctionComponent<ResourceHistoryProps> = (props: ResourceHistoryProps) => {
+        return <Text>
+          {
+            Object.keys(propertyBag).map((key) => {
+              let val = (propertyBag as any)[key].toString();
+              return <ResourcePropertyPanelItem header={userFriendlyKey(key)} val={val} />
+            })
+          }
+          </Text>
+      },
+      isPadded: true,
+    },
+    {
+      key: 'column5',
+      name: 'Date Modified',
+      fieldName: 'updatedWhen',
+      minWidth: 70,
+      maxWidth: 90,
+      isResizable: true,
+      data: 'number',
+      onRender: (item: HistoryItem) => {
+        return <Text>{moment.unix(item.updatedWhen).toDate().toUTCString()}</Text>;
+      },
+      isPadded: true,
+    },
+    {
+      key: 'column6',
+      name: 'Enabled',
+      fieldName: 'isEnabled',
+      minWidth: 30,
+      maxWidth: 35,
+      isResizable: true,
+      isCollapsible: true,
+      data: 'boolean',
+      onRender: (item: HistoryItem) => {
+        return <Text>{item.isEnabled.toString()}</Text>;
+      },
+      isPadded: true,
+    }
+  ];
+
   return (
-    <List items={props.history} onRenderCell={onRenderCell} />
+    <DetailsList
+      items={props.history}
+      checkboxVisibility={CheckboxVisibility.hidden}
+      columns={columns}
+      setKey="none"
+      layoutMode={DetailsListLayoutMode.justified}
+      isHeaderVisible={true}            
+    />
   )
 };
