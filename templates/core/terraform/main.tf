6af3d9a42e01c7fb28f7f57da63089a712d5c1a1
ross-p-smith <rosmith@microsoft.com>
2022-05-25 09:31:22 +0000

    Minimum upgrade to AzureRM 3.5

--- templates/core/terraform/main.tf
+++ templates/core/terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.97.0"
+      version = "=3.5.0"
     }
   }
 
@@ -13,8 +13,9 @@ terraform {
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy    = var.keyvault_purge_protection_enabled ? false : true
-      recover_soft_deleted_key_vaults = false
+      purge_soft_delete_on_destroy      = false
+      recover_soft_deleted_secrets      = true
+      recover_soft_deleted_certificates = true
     }
   }
 }
