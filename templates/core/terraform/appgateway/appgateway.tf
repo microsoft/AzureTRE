01666e8103ad3e1a1a2a7c26dcee191b2029cb4f
Tanya Borisova <tborisova@microsoft.com>
2022-05-27 17:33:50 +0100

    Ignore changes in zones

--- templates/core/terraform/appgateway/appgateway.tf
+++ templates/core/terraform/appgateway/appgateway.tf
@@ -6,7 +6,7 @@ resource "azurerm_public_ip" "appgwpip" {
   sku                 = "Standard"
   domain_name_label   = var.tre_id
 
-  lifecycle { ignore_changes = [tags] }
+  lifecycle { ignore_changes = [tags, zones] }
 }
 
 resource "azurerm_user_assigned_identity" "agw_id" {
