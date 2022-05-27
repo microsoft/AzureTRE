f219a5416642919278e0b97faa9c00bc2c9a02f9
Tanya Borisova <tborisova@microsoft.com>
2022-05-27 14:48:17 +0100

    revert all keyvault stuff

--- templates/core/terraform/main.tf
+++ templates/core/terraform/main.tf
@@ -13,8 +13,7 @@ terraform {
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy    = var.keyvault_purge_protection_enabled ? false : true
-      recover_soft_deleted_key_vaults = false
+      purge_soft_delete_on_destroy = false
     }
   }
 }
--- templates/workspaces/base/terraform/providers.tf
+++ templates/workspaces/base/terraform/providers.tf
@@ -20,10 +20,7 @@ terraform {
 provider "azurerm" {
   features {
     key_vault {
-      # Don't purge secrets on destroy (this would fail due to purge protection being enabled on keyvault)
-      purge_soft_deleted_secrets_on_destroy = false
-      # When recreating a shared service, recover any previously soft deleted secrets
-      recover_soft_deleted_secrets = true
+      purge_soft_delete_on_destroy = false
     }
   }
 }
