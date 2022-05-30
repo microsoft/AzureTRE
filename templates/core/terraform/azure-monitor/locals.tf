4419514f41303dfd930fcedc7f914c62c7b3a97a
Guy Bertental <gubert@microsoft.com>
2022-05-30 09:58:26 +0000

    merge locals

--- templates/core/terraform/azure-monitor/locals.tf
+++ templates/core/terraform/azure-monitor/locals.tf
@@ -1,8 +1,5 @@
 locals {
   app_insights_name = "appi-${var.tre_id}"
-}
-
-locals {
   tre_core_tags = {
     tre_id              = var.tre_id
     tre_core_service_id = var.tre_id
--- templates/core/terraform/resource_processor/vmss_porter/locals.tf
+++ templates/core/terraform/resource_processor/vmss_porter/locals.tf
@@ -1,7 +1,5 @@
 locals {
   version = replace(replace(replace(data.local_file.version.content, "__version__ = \"", ""), "\"", ""), "\n", "")
-}
-locals {
   tre_core_tags = {
     tre_id              = var.tre_id
     tre_core_service_id = var.tre_id
