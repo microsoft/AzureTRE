980b955de3116a734fd89f7bc0dd6ee7b12241aa
ross-p-smith <rosmith@microsoft.com>
2022-06-01 20:40:40 +0000

    Linter

--- templates/core/terraform/deploy.sh
+++ templates/core/terraform/deploy.sh
@@ -5,11 +5,15 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
+# This variables are loaded in for us
+# shellcheck disable=SC2154
 export TF_VAR_docker_registry_server="$TF_VAR_acr_name.azurecr.io"
 export TF_VAR_docker_registry_username="${TF_VAR_acr_name}"
-export TF_VAR_docker_registry_password=$(az acr credential show --name "${TF_VAR_acr_name}" --query passwords[0].value -o tsv | sed 's/"//g')
+TF_VAR_docker_registry_password=$(az acr credential show --name "${TF_VAR_acr_name}" --query passwords[0].value -o tsv | sed 's/"//g')
+export TF_VAR_docker_registry_password
 
 # This is where we can migrate any Terraform before we plan and apply
+# For instance deprecated Terraform resources
 ./migrate.sh
 
 PLAN_FILE="tfplan$$"
