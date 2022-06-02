b11954c88a07f9988f46825ceab475d1c72b6ab0
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2022-06-02 09:12:26 +0300

    Airlock processor - function app based - Base (#1950)

---
+++ templates/core/terraform/airlock/identity.tf
@@ -0,0 +1,43 @@
+data "azurerm_cosmosdb_account" "tre-db-account" {
+  name                = "cosmos-${var.tre_id}"
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_container_registry" "mgmt_acr" {
+  name                = var.mgmt_acr_name
+  resource_group_name = var.mgmt_resource_group_name
+}
+
+resource "azurerm_user_assigned_identity" "airlock_id" {
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = local.tre_core_tags
+
+  name = "id-airlock-${var.tre_id}"
+
+  lifecycle { ignore_changes = [tags] }
+}
+
+resource "azurerm_role_assignment" "acrpull_role" {
+  scope                = data.azurerm_container_registry.mgmt_acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.airlock_id.principal_id
+}
+
+resource "azurerm_role_assignment" "servicebus_sender" {
+  scope                = data.azurerm_servicebus_namespace.airlock_sb.id
+  role_definition_name = "Azure Service Bus Data Sender"
+  principal_id         = azurerm_user_assigned_identity.airlock_id.principal_id
+}
+
+resource "azurerm_role_assignment" "servicebus_receiver" {
+  scope                = data.azurerm_servicebus_namespace.airlock_sb.id
+  role_definition_name = "Azure Service Bus Data Receiver"
+  principal_id         = azurerm_user_assigned_identity.airlock_id.principal_id
+}
+
+resource "azurerm_role_assignment" "cosmos_contributor" {
+  scope                = data.azurerm_cosmosdb_account.tre-db-account.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_user_assigned_identity.airlock_id.principal_id
+}
