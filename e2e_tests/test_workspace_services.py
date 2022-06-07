66474c020ad4908cbceec2d23ecfd45d5ebd7b57
ross-p-smith <rosmith@microsoft.com>
2022-06-07 09:16:44 +0000

    Purge Protection

--- e2e_tests/test_workspace_services.py
+++ e2e_tests/test_workspace_services.py
@@ -68,7 +68,7 @@ async def test_create_guacamole_service_into_base_workspace(admin_token, verify)
     await disable_and_delete_resource(f'/api{workspace_path}', admin_token, verify)
 
 
-@pytest.mark.extended
+@pytest.mark.extended_aad
 @pytest.mark.timeout(3000)
 async def test_create_guacamole_service_into_aad_workspace(admin_token, verify) -> None:
     """This test will create a Guacamole service but will create a workspace and automatically register the AAD Application"""
