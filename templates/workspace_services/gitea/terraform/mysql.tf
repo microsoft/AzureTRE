0d534516380e008a081a70f3e2872cf6cd693962
Ciprian Maftei <ciprian.maftei@mhra.gov.uk>
2022-06-08 21:46:03 +0000

    update tflint-ignore due bug

--- templates/workspace_services/gitea/terraform/mysql.tf
+++ templates/workspace_services/gitea/terraform/mysql.tf
@@ -14,6 +14,9 @@ resource "azurerm_mysql_server" "gitea" {
   administrator_login_password      = random_password.password.result
   sku_name                          = "GP_Gen5_2"
   storage_mb                        = 5120
+  # Ignoring tflint due to a bug in it.
+  # TODO: https://github.com/microsoft/AzureTRE/issues/1944
+  # tflint-ignore: azurerm_mysql_server_invalid_version
   version                           = "8.0"
   auto_grow_enabled                 = true
   backup_retention_days             = 7
