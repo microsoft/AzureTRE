0e292e1ca5c3165013a920defec9e49a75d2ec6a
Ciprian Maftei <ciprian.maftei@mhra.gov.uk>
2022-06-08 21:05:31 +0000

    changed mysql version back to 8.0

--- templates/workspace_services/gitea/terraform/mysql.tf
+++ templates/workspace_services/gitea/terraform/mysql.tf
@@ -14,7 +14,7 @@ resource "azurerm_mysql_server" "gitea" {
   administrator_login_password      = random_password.password.result
   sku_name                          = "GP_Gen5_2"
   storage_mb                        = 5120
-  version                           = "8.0.21"
+  version                           = "8.0"
   auto_grow_enabled                 = true
   backup_retention_days             = 7
   geo_redundant_backup_enabled      = false
