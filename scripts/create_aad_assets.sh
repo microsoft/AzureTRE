428e5ffce31cd864326e93cb04af377b2ecf0f0c
Stuart Leeks <stuartle@microsoft.com>
2022-06-14 10:12:53 +0100

    Update create_aad_assets.sh for switch changes in aad-app-reg.sh

--- scripts/create_aad_assets.sh
+++ scripts/create_aad_assets.sh
@@ -36,7 +36,7 @@ fi
 # Then register an App for the TRE Core.
 ./scripts/aad/aad-app-reg.sh \
   --name "${TRE_ID}" \
-  --swaggerui-redirecturl "https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/docs/oauth2-redirect" \
+  --tre-url "https://${TRE_ID}.${LOCATION}.cloudapp.azure.com/api/docs/oauth2-redirect" \
   --admin-consent --automation-account
 
 echo "Please copy the values above into your /templates/core/.env."
