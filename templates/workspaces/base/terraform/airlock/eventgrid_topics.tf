5f004db55436aae63168689018646fd14de696a3
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-06-30 07:01:03 +0000

    add temp fix - delay between system topics

--- templates/workspaces/base/terraform/airlock/eventgrid_topics.tf
+++ templates/workspaces/base/terraform/airlock/eventgrid_topics.tf
@@ -37,6 +37,20 @@ resource "azurerm_role_assignment" "servicebus_sender_import_approved_blob_creat
   ]
 }
 
+# TEMPRARY MITIGATION. Should be removed with https://github.com/microsoft/AzureTRE/issues/2164
+resource "null_resource" "wait_for_import_approved_blob_created" {
+  provisioner "local-exec" {
+    command    = "bash -c \"sleep 60s\""
+    on_failure = fail
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  depends_on = [azurerm_eventgrid_system_topic.import_approved_blob_created]
+}
+
 resource "azurerm_eventgrid_system_topic" "export_inprogress_blob_created" {
   name                   = local.export_inprogress_sys_topic_name
   location               = var.location
@@ -56,7 +70,8 @@ resource "azurerm_eventgrid_system_topic" "export_inprogress_blob_created" {
   }
 
   depends_on = [
-    azurerm_storage_account.sa_export_inprogress
+    azurerm_storage_account.sa_export_inprogress,
+    null_resource.wait_for_import_approved_blob_created
   ]
 
   lifecycle { ignore_changes = [tags] }
@@ -71,6 +86,21 @@ resource "azurerm_role_assignment" "servicebus_sender_export_inprogress_blob_cre
     azurerm_eventgrid_system_topic.export_inprogress_blob_created
   ]
 }
+
+# TEMPRARY MITIGATION. Should be removed with https://github.com/microsoft/AzureTRE/issues/2164
+resource "null_resource" "wait_for_export_inprogress_blob_created" {
+  provisioner "local-exec" {
+    command    = "bash -c \"sleep 60s\""
+    on_failure = fail
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  depends_on = [azurerm_eventgrid_system_topic.export_inprogress_blob_created]
+}
+
 
 resource "azurerm_eventgrid_system_topic" "export_rejected_blob_created" {
   name                   = local.export_rejected_sys_topic_name
@@ -91,7 +121,8 @@ resource "azurerm_eventgrid_system_topic" "export_rejected_blob_created" {
   }
 
   depends_on = [
-    azurerm_storage_account.sa_export_rejected
+    azurerm_storage_account.sa_export_rejected,
+    null_resource.wait_for_export_inprogress_blob_created
   ]
 
   lifecycle { ignore_changes = [tags] }
@@ -106,6 +137,7 @@ resource "azurerm_role_assignment" "servicebus_sender_export_rejected_blob_creat
     azurerm_eventgrid_system_topic.export_rejected_blob_created
   ]
 }
+
 
 ## Subscriptions
 resource "azurerm_eventgrid_event_subscription" "import_approved_blob_created" {
