b96999695544856d2ec5f7119e73307b7431f86b
Liza Shakury <lishakur@Lizas-MacBook-Pro.local>
2022-07-03 19:18:28 +0300

    Fix lint

--- api_app/tests_ma/test_api/test_routes/test_airlock_resource_helpers.py
+++ api_app/tests_ma/test_api/test_routes/test_airlock_resource_helpers.py
@@ -1,13 +1,9 @@
-from azure.mgmt.storage import StorageManagementClient
-from azure.mgmt.storage.v2015_06_15.operations import StorageAccountsOperations
-from azure.mgmt.storage.v2016_01_01.models import StorageAccountListKeysResult, StorageAccountKey
 from fastapi import HTTPException, status
 import pytest
-from mock import AsyncMock, patch, MagicMock, Mock
+from mock import AsyncMock, patch, MagicMock
 
 from api.routes.airlock_resource_helpers import save_airlock_review, save_and_publish_event_airlock_request, \
-    update_status_and_publish_event_airlock_request, validate_user_is_allowed_to_access_sa, get_storage_account_key, \
-    get_airlock_request_container_sas_token, RequestAccountDetails, get_required_permission
+    update_status_and_publish_event_airlock_request, validate_user_is_allowed_to_access_sa, get_required_permission
 from tests_ma.test_api.conftest import create_workspace_owner_user, create_workspace_researcher_user
 from db.repositories.airlock_reviews import AirlockReviewRepository
 from db.repositories.airlock_requests import AirlockRequestRepository
@@ -111,8 +107,8 @@ async def test_save_and_publish_event_airlock_request_raises_503_if_save_to_db_f
 
 
 @patch("event_grid.helpers.EventGridPublisherClient", return_value=AsyncMock())
-async def test_save_and_publish_event_airlock_request_raises_503_if_publish_event_fails(
-    event_grid_publisher_client_mock, airlock_request_repo_mock):
+async def test_save_and_publish_event_airlock_request_raises_503_if_publish_event_fails(event_grid_publisher_client_mock,
+                                                                                        airlock_request_repo_mock):
     airlock_request_mock = sample_airlock_request()
     airlock_request_repo_mock.save_item = MagicMock(return_value=None)
     # When eventgrid fails, it deletes the saved request
@@ -153,8 +149,7 @@ async def test_update_status_and_publish_event_airlock_request_updates_item(even
     assert (actual_status_changed_event.data == status_changed_event_mock.data)
 
 
-async def test_update_status_and_publish_event_airlock_request_raises_400_if_status_update_invalid(
-    airlock_request_repo_mock):
+async def test_update_status_and_publish_event_airlock_request_raises_400_if_status_update_invalid(airlock_request_repo_mock):
     airlock_request_mock = sample_airlock_request()
 
     with pytest.raises(HTTPException) as ex:
@@ -168,8 +163,8 @@ async def test_update_status_and_publish_event_airlock_request_raises_400_if_sta
 
 
 @patch("event_grid.helpers.EventGridPublisherClient", return_value=AsyncMock())
-async def test_update_status_and_publish_event_airlock_requestt_raises_503_if_publish_event_fails(
-    event_grid_publisher_client_mock, airlock_request_repo_mock):
+async def test_update_status_and_publish_event_airlock_request_raises_503_if_publish_event_fails(event_grid_publisher_client_mock,
+                                                                                                 airlock_request_repo_mock):
     airlock_request_mock = sample_airlock_request()
     updated_airlock_request_mock = sample_airlock_request(status=AirlockRequestStatus.Submitted)
     airlock_request_repo_mock.update_airlock_request_status = MagicMock(return_value=updated_airlock_request_mock)
