441664ccdcc19300658ff25a08d8f11bf1addc1e
Liza Shakury <42377481+LizaShak@users.noreply.github.com>
2022-07-04 21:45:11 +0300

    Add api to get SAS  token (#2155)
    * Add api to get SAS  token.
    
    * Move token retrieval to helpers
    
    * Add reader role assignment to api managed identity
    
    * Mark the test out
    
    * CR changes
    
    * CR changes
    
    * Update Api version
    
    * Fix test
    
    * Fix lint
    
    * Fix lint
    
    * Fix lint
    
    * Move airlock methods to service
    
    * Fix lint
    
    * Fix lint
    
    * Use delagted key comment
    
    * Upgrade workspace base version
    
    * CR changes
    
    * Fix Tests
    
    * Fix Tests
    
    * Fix api role
    
    * Integration fixes
    
    * Update workspace version
    
    Co-authored-by: Liza Shakury <lishakur@Lizas-MacBook-Pro.local>
    Co-authored-by: Anat Balzam <anatbaz@gmail.com>
    Co-authored-by: Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>

--- templates/workspaces/base/terraform/airlock/storage_accounts.tf
+++ templates/workspaces/base/terraform/airlock/storage_accounts.tf
@@ -285,3 +285,28 @@ resource "azurerm_role_assignment" "sa_export_blocked" {
   principal_id         = data.azurerm_user_assigned_identity.airlock_id.principal_id
 }
 
+
+resource "azurerm_role_assignment" "sa_import_approved_reader" {
+  scope                = azurerm_storage_account.sa_import_approved.id
+  role_definition_name = "Reader and Data Access"
+  principal_id         = data.azurerm_user_assigned_identity.api_id.principal_id
+}
+
+
+resource "azurerm_role_assignment" "sa_export_internal_reader" {
+  scope                = azurerm_storage_account.sa_export_internal.id
+  role_definition_name = "Reader and Data Access"
+  principal_id         = data.azurerm_user_assigned_identity.api_id.principal_id
+}
+
+resource "azurerm_role_assignment" "sa_export_inprogress_reader" {
+  scope                = azurerm_storage_account.sa_export_inprogress.id
+  role_definition_name = "Reader and Data Access"
+  principal_id         = data.azurerm_user_assigned_identity.api_id.principal_id
+}
+
+resource "azurerm_role_assignment" "sa_export_rejected_reader" {
+  scope                = azurerm_storage_account.sa_export_rejected.id
+  role_definition_name = "Reader and Data Access"
+  principal_id         = data.azurerm_user_assigned_identity.api_id.principal_id
+}
