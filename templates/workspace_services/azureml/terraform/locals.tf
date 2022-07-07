2baf72b83f40f9f840c3c0928e62c948d7825372
pedro-pelegrin-nttdata <104428697+pedro-pelegrin-nttdata@users.noreply.github.com>
2022-07-07 18:48:46 +0200

    AML bundle fails due to missing tags (#2209)
    * AML bundle fails due to missing tags
    
    * Submodule
    
    * Submodule cprd
    
    * Fixing azureml/acr and devtestlabs
    
    * Submodule
    
    * Upgrade porter version
    
    * Delete devtestlabs folder
    
    Co-authored-by: Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>

--- templates/workspace_services/azureml/terraform/locals.tf
+++ templates/workspace_services/azureml/terraform/locals.tf
@@ -18,7 +18,7 @@ locals {
   storage_name                   = lower(replace("stg${substr(local.service_resource_name_suffix, -8, -1)}", "-", ""))
   tre_workspace_service_tags = {
     tre_id                   = var.tre_id
-    tre_workspace_id         = var.tre_resource_id
-    tre_workspace_service_id = var.workspace_id
+    tre_workspace_id         = var.workspace_id
+    tre_workspace_service_id = var.tre_resource_id
   }
 }
