2a7676a8ee9cac474b822390c58590bbdb1d61f2
David Moore <35696285+damoodamoo@users.noreply.github.com>
2022-07-18 13:45:27 +0100

    Shared services limited to TRE admin in API and UI (#2300)
    * shared services limited to TRE admin in API and UI
    
    * moved shared service GET test to admin auth class
    
    * unwound api changes
    
    * unwound api changes

--- ui/app/src/components/root/LeftNav.tsx
+++ ui/app/src/components/root/LeftNav.tsx
@@ -1,46 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Nav, INavLinkGroup } from '@fluentui/react/lib/Nav';
 import { useNavigate } from 'react-router-dom';
+import { AppRolesContext } from '../../contexts/AppRolesContext';
+import { RoleName } from '../../models/roleNames';
 
-const navLinkGroups: INavLinkGroup[] = [
-  {
-    links: [
-      {
-        name: 'Workspaces',
-        url: '/',
-        key: '/',
-        icon: 'WebAppBuilderFragment'
-      },
+export const LeftNav: React.FunctionComponent = () => {
+  const navigate = useNavigate();
+  const appRolesCtx = useContext(AppRolesContext);
+
+  const navLinkGroups: INavLinkGroup[] = [
+    {
+      links: [
+        {
+          name: 'Workspaces',
+          url: '/',
+          key: '/',
+          icon: 'WebAppBuilderFragment'
+        }
+      ],
+    },
+  ];
+
+  // show shared-services link if TRE Admin
+  if (appRolesCtx.roles.includes(RoleName.TREAdmin)) {
+    navLinkGroups[0].links.push(
       {
         name: 'Shared Services',
         url: '/shared-services',
         key: 'shared-services',
         icon: 'Puzzle'
-      }/*,
-      {
-        name: 'Settings',
-        url: '/settings',
-        key: 'settings',
-        disabled: true,
-        icon: 'Settings'
-      },
-      {
-        name: 'Admin',
-        url: '/admin',
-        key: 'admin',
-        disabled: false,
-        icon: 'AdminALogoFill32'
-      },*/
-    ],
-  },
-];
-
-export const LeftNav: React.FunctionComponent = () => {
-  const navigate = useNavigate();
+      });
+  }
 
   return (
     <Nav
-      onLinkClick={(e, item) => {e?.preventDefault(); item?.url && navigate(item.url)}}
+      onLinkClick={(e, item) => { e?.preventDefault(); item?.url && navigate(item.url) }}
       ariaLabel="TRE Left Navigation"
       groups={navLinkGroups}
     />
