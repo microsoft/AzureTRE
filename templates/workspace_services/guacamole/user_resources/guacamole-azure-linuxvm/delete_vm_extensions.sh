af248f33172985b0cca97477ad12964f8992f900
Tanya Borisova <tborisova@microsoft.com>
2022-07-18 13:48:45 +0100

    remove misleading comment

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/delete_vm_extensions.sh
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/delete_vm_extensions.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+# Uncomment this line to see each command for debugging
 # set -o xtrace
 
 
