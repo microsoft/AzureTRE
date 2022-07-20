6a96e4177d49baa6617dfa72c2e4f303296e9661
sharon <sharon.hart@microsoft.com>
2022-07-20 14:56:25 +0300

    revert change

--- templates/workspaces/base/terraform/variables.tf
+++ templates/workspaces/base/terraform/variables.tf
@@ -107,3 +107,5 @@ variable "workspace_owner_object_id" {
   default     = ""
   description = "The Object Id of the user that you wish to be the Workspace Owner. E.g. the TEST_AUTOMATION_ACCOUNT."
 }
+
+
