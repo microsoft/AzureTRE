d4e3018b0ce0a719e642eb9be632964bcce8069d
Anat Balzam <anatbalzam@microsoft.com>
2022-07-25 15:50:20 +0000

    Solve local debugging issues

--- templates/core/terraform/outputs.tf
+++ templates/core/terraform/outputs.tf
@@ -89,6 +89,10 @@ output "event_grid_status_changed_topic_resource_id" {
   value = module.airlock_resources.event_grid_status_changed_topic_resource_id
 }
 
+output "event_grid_airlock_notification_topic_resource_id" {
+  value = module.airlock_resources.event_grid_airlock_notification_topic_resource_id
+}
+
 output "service_bus_step_result_queue" {
   value = module.airlock_resources.service_bus_step_result_queue
 }
