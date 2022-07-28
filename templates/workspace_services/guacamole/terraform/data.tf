d39724ca4c4ddd7d1ab14c45a75c72152310319f
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-07-28 05:49:23 +0000

    remove unused data objects

--- templates/workspace_services/guacamole/terraform/data.tf
+++ templates/workspace_services/guacamole/terraform/data.tf
@@ -46,11 +46,6 @@ data "azurerm_private_dns_zone" "azurewebsites" {
   resource_group_name = local.core_resource_group_name
 }
 
-data "azurerm_private_dns_zone" "vaultcore" {
-  name                = "privatelink.vaultcore.azure.net"
-  resource_group_name = local.core_resource_group_name
-}
-
 data "azurerm_container_registry" "mgmt_acr" {
   name                = var.mgmt_acr_name
   resource_group_name = var.mgmt_resource_group_name
@@ -58,16 +53,6 @@ data "azurerm_container_registry" "mgmt_acr" {
 
 data "azurerm_log_analytics_workspace" "tre" {
   name                = "log-${var.tre_id}"
-  resource_group_name = local.core_resource_group_name
-}
-
-data "azurerm_network_security_group" "ws" {
-  name                = "nsg-ws"
-  resource_group_name = data.azurerm_virtual_network.ws.resource_group_name
-}
-
-data "azurerm_private_dns_zone" "filecore" {
-  name                = "privatelink.file.core.windows.net"
   resource_group_name = local.core_resource_group_name
 }
 
