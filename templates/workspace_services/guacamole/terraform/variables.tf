764b0046edbb6ecaa5b8c18fc10225d44e75c940
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-07-31 10:08:24 +0300

    Guacamole updates firewall with deployment pipeline (#2371)
    * Guacamole updates firewall with the pipeline
    
    * tf format
    
    * remove unused data objects
    
    * update changelog
    
    Co-authored-by: David Moore <35696285+damoodamoo@users.noreply.github.com>

--- templates/workspace_services/guacamole/terraform/variables.tf
+++ templates/workspace_services/guacamole/terraform/variables.tf
@@ -1,9 +1,5 @@
 variable "workspace_id" {}
 variable "tre_id" {}
-variable "arm_client_id" {}
-variable "arm_client_secret" {}
-variable "arm_tenant_id" {}
-variable "arm_use_msi" {}
 variable "mgmt_resource_group_name" {}
 variable "mgmt_acr_name" {}
 variable "image_name" {}
