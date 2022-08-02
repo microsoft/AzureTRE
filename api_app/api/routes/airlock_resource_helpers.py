f47b1d16dd3567f5c33afa3831c956c8adb59877
Yuval Yaron <yuvalyaron@microsoft.com>
2022-08-02 20:27:20 +0300

    fix test

--- api_app/api/routes/airlock_resource_helpers.py
+++ api_app/api/routes/airlock_resource_helpers.py
@@ -54,7 +54,7 @@ async def save_and_publish_event_airlock_request(airlock_request: AirlockRequest
 async def update_status_and_publish_event_airlock_request(airlock_request: AirlockRequest, airlock_request_repo: AirlockRequestRepository, user: User, new_status: AirlockRequestStatus, workspace: Workspace, error_message: str = None):
     try:
         logging.debug(f"Updating airlock request item: {airlock_request.id}")
-        updated_airlock_request = airlock_request_repo.update_airlock_request_status(airlock_request=airlock_request, new_status=new_status, user=user, error_message=error_message)
+        updated_airlock_request = airlock_request_repo.update_airlock_request_status(airlock_request, new_status, user, error_message)
     except Exception as e:
         logging.error(f'Failed updating airlock_request item {airlock_request}: {e}')
         # If the validation failed, the error was not related to the saving itself
