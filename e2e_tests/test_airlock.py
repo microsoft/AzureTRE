425beed0fe6dc18880e265155e99c488258729f9
Guy Bertental <gubert@microsoft.com>
2022-08-03 12:45:53 +0000

    remove hardcoded workspace

--- e2e_tests/test_airlock.py
+++ e2e_tests/test_airlock.py
@@ -18,24 +18,22 @@ LOGGER = logging.getLogger(__name__)
 @pytest.mark.timeout(1600)
 async def test_airlock_import_flow(admin_token, verify) -> None:
 
-    workspace_id = "3ccbb56d-5cd8-48b5-9e64-7279dc196e47"
-    workspace_path = f"/workspaces/{workspace_id}"
     # 1. create workspace
-    # payload = {
-    #     "templateName": "tre-workspace-base",
-    #     "properties": {
-    #         "display_name": "E2E test airlock flow",
-    #         "description": "workspace for E2E airlock flow",
-    #         "address_space_size": "small",
-    #         "client_id": f"{config.TEST_WORKSPACE_APP_ID}",
-    #         "client_secret": f"{config.TEST_WORKSPACE_APP_SECRET}",
-    #     }
-    # }
+    payload = {
+        "templateName": "tre-workspace-base",
+        "properties": {
+            "display_name": "E2E test airlock flow",
+            "description": "workspace for E2E airlock flow",
+            "address_space_size": "small",
+            "client_id": f"{config.TEST_WORKSPACE_APP_ID}",
+            "client_secret": f"{config.TEST_WORKSPACE_APP_SECRET}",
+        }
+    }
 
-    # if config.TEST_WORKSPACE_APP_PLAN != "":
-    #     payload["properties"]["app_service_plan_sku"] = config.TEST_WORKSPACE_APP_PLAN
+    if config.TEST_WORKSPACE_APP_PLAN != "":
+        payload["properties"]["app_service_plan_sku"] = config.TEST_WORKSPACE_APP_PLAN
 
-    # workspace_path, workspace_id = await post_resource(payload, resource_strings.API_WORKSPACES, access_token=admin_token, verify=verify)
+    workspace_path, workspace_id = await post_resource(payload, resource_strings.API_WORKSPACES, access_token=admin_token, verify=verify)
     workspace_owner_token, scope_uri = await get_workspace_auth_details(admin_token=admin_token, workspace_id=workspace_id, verify=verify)
 
     # 2. create airlock request
@@ -95,4 +93,4 @@ async def test_airlock_import_flow(admin_token, verify) -> None:
     await wait_for_status(airlock_strings.APPROVED_STATUS, workspace_owner_token, workspace_path, request_id, verify)
 
     # 7. delete workspace
-    # await disable_and_delete_resource(f'/api{workspace_path}', admin_token, verify)
+    await disable_and_delete_resource(f'/api{workspace_path}', admin_token, verify)
