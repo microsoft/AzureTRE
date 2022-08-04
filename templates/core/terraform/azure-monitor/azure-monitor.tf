efb9414e047bb680da232034879b902a2294dda2
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-08-04 18:43:59 +0300

    Refactor Azure Monitor in core (#2375)
    * destroy stale cicd environments
    
    * fix
    
    * fix
    
    * setup azmonitor & appinsights natively as possible
    
    * update version
    
    * updates
    
    * version change

--- templates/core/terraform/azure-monitor/azure-monitor.tf
+++ templates/core/terraform/azure-monitor/azure-monitor.tf
@@ -1,95 +1,115 @@
+# Log Analytics
 resource "azurerm_log_analytics_workspace" "core" {
-  name                = "log-${var.tre_id}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  retention_in_days   = 30
-  sku                 = "PerGB2018"
-  tags                = local.tre_core_tags
+  name                       = "log-${var.tre_id}"
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
+  retention_in_days          = 30
+  sku                        = "PerGB2018"
+  tags                       = var.tre_core_tags
+  internet_ingestion_enabled = false
 
   lifecycle { ignore_changes = [tags] }
 }
 
-# Storage account for Application Insights
+# Storage account for Azure Monitor ingestion
 # Because Private Link is enabled on Application Performance Management (APM), Bring Your Own Storage (BYOS) approach is required
-resource "azurerm_storage_account" "app_insights" {
-  name                            = lower(replace("stappinsights${var.tre_id}", "-", ""))
+resource "azurerm_storage_account" "az_monitor" {
+  name                            = lower(replace("stazmonitor${var.tre_id}", "-", ""))
   resource_group_name             = var.resource_group_name
   location                        = var.location
   account_kind                    = "StorageV2"
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
-  tags                            = local.tre_core_tags
+  tags                            = var.tre_core_tags
 
   lifecycle { ignore_changes = [tags] }
 }
 
-data "local_file" "app_insights_arm_template" {
-  filename = "${path.module}/app_insights.json"
+resource "azurerm_log_analytics_linked_storage_account" "workspace_storage_ingestion" {
+  data_source_type      = "ingestion"
+  resource_group_name   = var.resource_group_name
+  workspace_resource_id = azurerm_log_analytics_workspace.core.id
+  storage_account_ids   = [azurerm_storage_account.az_monitor.id]
 }
 
-# Application Insights
-# Deployed using ARM template, because Terraform's azurerm_application_insights does not support linked storage account
-resource "azurerm_resource_group_template_deployment" "app_insights_core" {
-  name                = local.app_insights_name
-  resource_group_name = var.resource_group_name
-  deployment_mode     = "Incremental"
-  template_content    = data.local_file.app_insights_arm_template.content
-
-  parameters_content = jsonencode({
-    "app_insights_name" = {
-      value = local.app_insights_name
-    }
-    "location" = {
-      value = var.location
-    }
-    "log_analytics_workspace_id" = {
-      value = azurerm_log_analytics_workspace.core.id
-    }
-    "application_type" = {
-      value = "web"
-    }
-    "storage_account_name" = {
-      value = azurerm_storage_account.app_insights.name
-    }
-    "tre_core_tags" = {
-      value = local.tre_core_tags
-    }
-  })
+resource "azurerm_log_analytics_linked_storage_account" "workspace_storage_customlogs" {
+  data_source_type      = "customlogs"
+  resource_group_name   = var.resource_group_name
+  workspace_resource_id = azurerm_log_analytics_workspace.core.id
+  storage_account_ids   = [azurerm_storage_account.az_monitor.id]
 }
 
-data "local_file" "ampls_arm_template" {
-  filename = "${path.module}/ampls.json"
-}
-
-# Azure Monitor Private Link Scope
-# See https://docs.microsoft.com/azure/azure-monitor/logs/private-link-security
-resource "azurerm_resource_group_template_deployment" "ampls_core" {
+resource "azurerm_monitor_private_link_scope" "ampls_core" {
   name                = "ampls-${var.tre_id}"
   resource_group_name = var.resource_group_name
-  deployment_mode     = "Incremental"
-  template_content    = data.local_file.ampls_arm_template.content
+  tags                = var.tre_core_tags
 
+  lifecycle { ignore_changes = [tags] }
+}
+
+resource "azurerm_monitor_private_link_scoped_service" "ampls_log_anaytics" {
+  name                = "ampls-log-anaytics-service"
+  resource_group_name = var.resource_group_name
+  scope_name          = azurerm_monitor_private_link_scope.ampls_core.name
+  linked_resource_id  = azurerm_log_analytics_workspace.core.id
+}
+
+
+
+# Application Insights
+
+resource "azurerm_application_insights" "core" {
+  name                                = "appi-${var.tre_id}"
+  location                            = var.location
+  resource_group_name                 = var.resource_group_name
+  workspace_id                        = azurerm_log_analytics_workspace.core.id
+  application_type                    = "web"
+  internet_ingestion_enabled          = false
+  force_customer_storage_for_profiler = true
+  tags                                = var.tre_core_tags
+
+  lifecycle { ignore_changes = [tags] }
+}
+
+resource "azurerm_monitor_private_link_scoped_service" "ampls_app_insights" {
+  name                = "ampls-app-insights-service"
+  resource_group_name = var.resource_group_name
+  scope_name          = azurerm_monitor_private_link_scope.ampls_core.name
+  linked_resource_id  = azurerm_application_insights.core.id
+}
+
+data "local_file" "app_insights_byo_storage_arm_template" {
+  filename = "${path.module}/app_insights_byo_storage.json"
+}
+
+# Deployed using ARM template, because Terraform's azurerm_application_insights does not support linked storage account
+# https://docs.microsoft.com/en-us/azure/azure-monitor/app/profiler-bring-your-own-storage
+resource "azurerm_resource_group_template_deployment" "app_insights_byo_storage" {
+  name                = azurerm_application_insights.core.name
+  resource_group_name = var.resource_group_name
+  deployment_mode     = "Incremental"
+  template_content    = data.local_file.app_insights_byo_storage_arm_template.content
 
   parameters_content = jsonencode({
-    "private_link_scope_name" = {
-      value = "ampls-${var.tre_id}"
-    }
-    "workspace_name" = {
-      value = azurerm_log_analytics_workspace.core.name
-    }
     "app_insights_name" = {
-      value = local.app_insights_name
+      value = azurerm_application_insights.core.name
     }
-    "tre_core_tags" = {
-      value = local.tre_core_tags
+    "storage_account_resource_id" = {
+      value = azurerm_storage_account.az_monitor.id
     }
   })
 
   depends_on = [
-    azurerm_log_analytics_workspace.core,
-    azurerm_resource_group_template_deployment.app_insights_core
+    azurerm_application_insights.core
   ]
+}
+
+# Per https://docs.microsoft.com/en-us/azure/azure-monitor/profiler/profiler-bring-your-own-storage#grant-access-to-diagnostic-services-to-your-storage-account
+resource "azurerm_role_assignment" "appinsights_storage_permission" {
+  scope                = azurerm_storage_account.az_monitor.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = "6243488d-10d8-4ea0-884e-c2d5d1b7462d" # id of: Diagnostic Services Trusted Storage Access
 }
 
 resource "azurerm_private_endpoint" "azure_monitor_private_endpoint" {
@@ -97,12 +117,15 @@ resource "azurerm_private_endpoint" "azure_monitor_private_endpoint" {
   resource_group_name = var.resource_group_name
   location            = var.location
   subnet_id           = var.shared_subnet_id
-  tags                = local.tre_core_tags
+  tags                = var.tre_core_tags
 
   lifecycle { ignore_changes = [tags] }
+  depends_on = [
+    azurerm_monitor_private_link_scoped_service.ampls_app_insights,
+  ]
 
   private_service_connection {
-    private_connection_resource_id = jsondecode(azurerm_resource_group_template_deployment.ampls_core.output_content).resourceId.value
+    private_connection_resource_id = azurerm_monitor_private_link_scope.ampls_core.id
     name                           = "psc-ampls-${var.tre_id}"
     subresource_names              = ["azuremonitor"]
     is_manual_connection           = false
