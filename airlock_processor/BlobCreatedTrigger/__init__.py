d53b718cd80eb9a5d6e53a147e09cc99009df051
Tanya Borisova <tborisova@microsoft.com>
2022-08-09 15:35:34 +0100

    send blob to delete in the event

--- airlock_processor/BlobCreatedTrigger/__init__.py
+++ airlock_processor/BlobCreatedTrigger/__init__.py
@@ -1,5 +1,5 @@
 from distutils.util import strtobool
-from shared_code import constants, storage_accounts
+from shared_code import constants, blob_operations
 import logging
 
 import azure.functions as func
@@ -21,6 +21,7 @@ def main(msg: func.ServiceBusMessage,
     json_body = json.loads(body)
 
     # Example of a topic: "/subscriptions/<subscription_id>/resourceGroups/rg-tanyaair1/providers/Microsoft.Storage/storageAccounts/stalimiptanyaair1"
+    # Example of a subject: "/blobServices/default/containers/67824d82-b62c-4ab8-88c3-a5ffd20a012f/blobs/new_model.txt"
     topic = json_body["topic"]
     request_id = re.search(r'/blobServices/default/containers/(.*?)/blobs', json_body["subject"]).group(1)
 
@@ -56,30 +57,6 @@ def main(msg: func.ServiceBusMessage,
         completed_step = constants.STAGE_BLOCKING_INPROGRESS
         new_status = constants.STAGE_BLOCKED_BY_SCAN
 
-    if constants.STORAGE_ACCOUNT_NAME_EXPORT_PREFIX in topic:
-        request_type = constants.IMPORT_TYPE
-    elif constants.STORAGE_ACCOUNT_NAME_IMPORT_PREFIX in topic:
-        request_type = constants.EXPORT_TYPE
-    else:
-        logging.error(f"Can't determine type of request from topic {topic}")
-        raise
-
-    src = storage_accounts.source_storage_account_from_dest(
-        request_type=request_type,
-        # TODO: other arguments?????
-    )
-
-    toDeleteEvent.set(
-        func.EventGridOutputEvent(
-            id=str(uuid.uuid4()),
-            data={},  # TODO(tanya): what information do we need? # request_id, account_name, account_rg
-            subject=request_id,
-            event_type="Airlock.ToDelete",
-            event_time=datetime.datetime.utcnow(),
-            data_version="1.0"
-        )
-    )
-
     # reply with a step completed event
     stepResultEvent.set(
         func.EventGridOutputEvent(
@@ -89,3 +66,32 @@ def main(msg: func.ServiceBusMessage,
             event_type="Airlock.StepResult",
             event_time=datetime.datetime.utcnow(),
             data_version=constants.STEP_RESULT_EVENT_DATA_VERSION))
+
+    # create blob client
+    blob_container = get_container_client(json_body)
+    blob_metadata = source_blob.get_blob_properties()["metadata"]
+    copied_from = json.loads(blob_metadata["copied_from"])
+
+    # signal that the container where we copied from can now be deleted
+    toDeleteEvent.set(
+        func.EventGridOutputEvent(
+            id=str(uuid.uuid4()),
+            data={"blob_to_delete": copied_from[-1]},  # last container in copied_from is the one we just copied from
+            subject=request_id,
+            event_type="Airlock.ToDelete",
+            event_time=datetime.datetime.utcnow(),
+            data_version="1.0"
+        )
+    )
+
+
+def get_blob_client_from_request_data(json_body: str):
+    storage_account_name = re.search(r'providers/Microsoft.Storage/storageAccounts/(.*?)"', json_body["topic"]).group(1)
+    container_name = re.search(r'/blobServices/default/containers/(.*?)/blobs', json_body["subject"]).group(1)
+    source_blob_service_client = BlobServiceClient(account_url=f"https://{storage_account_name}.blob.core.windows.net/",
+                                                   credential=blob_operations.get_credential())
+    source_container_client = source_blob_service_client.get_container_client(container_name)
+    blob_name = re.search(r'blobs/(.*?)', json_body["subject"]).group(1)
+
+    return get_blob_client(source_container_client.get_blob_client(blob_name))
+
