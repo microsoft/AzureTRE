614b75e53ac157d337bc67b47bfea8ab945674b0
marrobi <marrobi@microsoft.com>
2022-08-18 08:21:34 +0000

    Failing on firewall step

--- templates/workspace_services/azureml/terraform/output.tf
+++ templates/workspace_services/azureml/terraform/output.tf
@@ -13,3 +13,16 @@ output "azureml_storage_account_id" {
 output "connection_uri" {
   value = "https://ml.azure.com/?wsid=${azapi_resource.aml_workspace.id}&tid=${var.arm_tenant_id}"
 }
+
+output "workspace_services_subnet_address_prefix" {
+  value = data.azurerm_subnet.services.address_prefix
+}
+
+data "azurerm_network_service_tags" "storage_tag" {
+  location        = azurerm_storage_account.aml.location
+  service         = "Storage"
+  location_filter = azurerm_storage_account.aml.location
+}
+output "storage_tag" {
+  value = data.azurerm_network_service_tags.storage_tag.id
+}
