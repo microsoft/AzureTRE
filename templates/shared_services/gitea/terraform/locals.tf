6c8238b60539d395dde32036e0b65f44f1788dd5
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-08-28 12:31:08 +0000

    lint

--- templates/shared_services/gitea/terraform/locals.tf
+++ templates/shared_services/gitea/terraform/locals.tf
@@ -2,7 +2,6 @@ locals {
   core_vnet                = "vnet-${var.tre_id}"
   core_resource_group_name = "rg-${var.tre_id}"
   webapp_name              = "gitea-${var.tre_id}"
-  firewall_name            = "fw-${var.tre_id}"
   storage_account_name     = lower(replace("stg-${var.tre_id}", "-", ""))
   keyvault_name            = "kv-${var.tre_id}"
   version                  = replace(replace(replace(data.local_file.version.content, "__version__ = \"", ""), "\"", ""), "\n", "")
