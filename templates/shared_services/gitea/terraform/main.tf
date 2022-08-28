84e2a3311be8879ff8c68350bc276124ae30d26a
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-08-28 18:27:07 +0300

    Gitea shared service supports app-service standard SKUs (#2523)
    * Gitea shared service supports app-service S* SKUs
    
    * update changelog
    
    * lint

--- templates/shared_services/gitea/terraform/main.tf
+++ templates/shared_services/gitea/terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.16.0"
+      version = "=3.20.0"
     }
     local = {
       source  = "hashicorp/local"
@@ -11,7 +11,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.2.0"
+      version = "~> 3.3.0"
     }
   }
 
