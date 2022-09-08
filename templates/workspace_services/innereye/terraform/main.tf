19196a932898438112196751ead6d47e7a7945a5
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-09-08 06:48:14 +0000

    update tf definitions

--- templates/workspace_services/innereye/terraform/main.tf
+++ templates/workspace_services/innereye/terraform/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.97.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.3.0"
+    }
   }
   backend "azurerm" {
   }
--- templates/workspace_services/mlflow/terraform/main.tf
+++ templates/workspace_services/mlflow/terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.97.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.3.0"
+    }
   }
 
   backend "azurerm" {
