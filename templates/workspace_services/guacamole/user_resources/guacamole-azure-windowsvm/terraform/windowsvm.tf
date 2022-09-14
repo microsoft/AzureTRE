60c1789a867e6f4ccbb9af90d8ebec36b7864ec8
Tanya Borisova <tborisova@microsoft.com>
2022-09-14 15:18:23 +0100

    Revert "wip: does not work"
    This reverts commit 353385830b46179951bb70595e3dad00318ec6e8.

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/windowsvm.tf
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/windowsvm.tf
@@ -92,7 +92,7 @@ data "template_file" "vm_config" {
   template = file("${path.module}/vm_config.ps1")
   vars = {
     nexus_proxy_url     = local.nexus_proxy_url
-    SharedStorageAccess = var.shared_storage_access ? 1 : 0
+    SharedStorageAccess = tobool(var.shared_storage_access) ? 1 : 0
     StorageAccountName  = data.azurerm_storage_account.stg.name
     StorageAccountKey   = data.azurerm_storage_account.stg.primary_access_key
     FileShareName       = var.shared_storage_access ? data.azurerm_storage_share.shared_storage[0].name : ""
