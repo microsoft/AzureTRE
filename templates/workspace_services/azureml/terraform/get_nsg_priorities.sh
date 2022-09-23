8b5d0e97dd5a9cadbbf1939781391c17eea44456
marrobi <marrobi@microsoft.com>
2022-09-23 13:14:11 +0000

    FIx bad linting.

--- templates/workspace_services/azureml/terraform/get_nsg_priorities.sh
+++ templates/workspace_services/azureml/terraform/get_nsg_priorities.sh
@@ -11,9 +11,11 @@ eval "$(jq -r '@sh "nsg_name=\(.nsg_name) resource_group_name=\(.resource_group_
 if NSG_RULE=$(az network nsg rule show -g "$resource_group_name" --nsg-name "$nsg_name" --name "$nsg_rule_name" -o json); then
     NSG_RULE_PRIORITY=$(echo "$NSG_RULE" | jq '.priority')
 else
-  # shellcheck disable=SC2016
+    # shellcheck disable=SC2016
     NSG_RULE_MAX_PRIORITY=$(az network nsg rule list -g "$resource_group_name" --nsg-name  "$nsg_name" --query 'not_null(max_by([?direction==`Outbound` && access==`Allow`],&priority).priority) || `100`')
-    NSG_RULE_PRIORITY=$($NSG_RULE_MAX_PRIORITY + 1)
+    # without $(()) command fails
+    # shellcheck disable=SC2004
+    NSG_RULE_PRIORITY=$(($NSG_RULE_MAX_PRIORITY + 1))
 fi
 
 # Safely produce a JSON object containing the result value.
