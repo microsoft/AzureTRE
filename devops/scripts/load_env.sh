8252f77100b19cbfd4e8e78f7e7ee12b3ee33f55
Stuart Leeks <stuartle@microsoft.com>
2022-09-27 12:24:34 +0000

    PR feedback for load_env.sh etc

--- devops/scripts/load_env.sh
+++ devops/scripts/load_env.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# set -o errexit
-set -o pipefail
-set -o nounset
-# set -o xtrace
+# not setting script options as we're sourcing this script
+# so options set here end up affecting the caller's context
 
 if [ ! -f "$1" ]; then
   if [ -z "${USE_ENV_VARS_NOT_FILES:-}" ]; then
@@ -10,16 +8,20 @@ if [ ! -f "$1" ]; then
     #exit
   fi
 else
+  # Loop over the relevant lines in the file specified in $1 (passed in after the loop)
+  # The loop source filters the lines in the source file to those that should be treated
+  # as variable definitions
   while read -r line
   do
+    # split the line into name/value
     name=$(echo $line | cut -d= -f1)
     value=$(echo $line | cut -d= -f2)
+    # if the value is quote-delimited then strip that as we quote in the declare statement
     if [[ ("${value:0:1}" == "'" &&  "${value: -1:1}" == "'") || (("${value:0:1}" == "\"" &&  "${value: -1:1}" == "\"")) ]]; then
       value=${value:1:-1}
     fi
+    # declare the variable and export to the caller's context
     declare -g $name="$value"
     export "${name?}"
   done < <(grep -v -e '^[[:space:]]*$' -e '^#' "$1" ) # feed in via Process Substition to avoid bash subshell (http://mywiki.wooledge.org/ProcessSubstitution)
 fi
-
-set +o nounset
