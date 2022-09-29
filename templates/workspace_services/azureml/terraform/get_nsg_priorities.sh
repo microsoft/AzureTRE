e16de8248a2f1484ae8a5c4a1bbe14ff566332ec
marrobi <marrobi@microsoft.com>
2022-09-29 11:21:44 +0000

    Fix PR comments with better solution

--- templates/workspace_services/azureml/terraform/get_nsg_priorities.sh
+++ templates/workspace_services/azureml/terraform/get_nsg_priorities.sh
@@ -6,12 +6,11 @@ set -o nounset
 
 eval "$(jq -r '@sh "nsg_name=\(.nsg_name) resource_group_name=\(.resource_group_name) nsg_rule_name=\(.nsg_rule_name) direction=\(.direction)"')"
 
-# This variables are loaded in for us
-# shellcheck disable=SC2154
-if NSG_RULE=$(az network nsg rule show -g "$resource_group_name" --nsg-name "$nsg_name" --name "$nsg_rule_name" -o json); then
+# These variables are loaded in for us
+if NSG_RULE=$(az network nsg rule show -g "${resource_group_name?}" --nsg-name "${nsg_name?}" --name "${nsg_rule_name?}" -o json); then
     NSG_RULE_PRIORITY=$(echo "$NSG_RULE" | jq '.priority')
 else
-    NSG_RULE_MAX_PRIORITY=$(az network nsg rule list -g "$resource_group_name" --nsg-name  "$nsg_name" --query "not_null(max_by([?direction=='$direction' && access=='Allow'],&priority).priority) || '100'" -o json)
+    NSG_RULE_MAX_PRIORITY=$(az network nsg rule list -g "${resource_group_name?}" --nsg-name  "${resource_group_name?}" --query "not_null(max_by([?direction=='${direction?}' && access=='Allow'],&priority).priority) || '100'" -o json)
     # without $(()) command fails
     # shellcheck disable=SC2004
     NSG_RULE_PRIORITY=$(($NSG_RULE_MAX_PRIORITY + 1))
