4d252703e55d996e5822fecdc445236f43921f69
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-10-02 12:41:09 +0300

    Remove doker env variables (#2675)
    * terraform init in core/ops shouldn't use -upgrade
    
    * update terraform in devcontainer
    
    * always refresh
    
    * tflint
    
    * update changelog
    
    * Remove doker env variables
    
    * update changelog

--- templates/workspace_services/gitea/terraform/destroy.sh
+++ templates/workspace_services/gitea/terraform/destroy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-set -e
 
-# This script assumes you have created an .env from the sample and the variables
-# will come from there.
-# shellcheck disable=SC2154
-export TF_VAR_docker_registry_server="$TF_VAR_acr_name.azurecr.io"
-export TF_VAR_docker_registry_username=$TF_VAR_acr_name
-TF_VAR_docker_registry_password=$(az acr credential show --name "${TF_VAR_acr_name}" --query passwords[0].value -o tsv | sed 's/"//g')
-export TF_VAR_docker_registry_password
+set -o errexit
+set -o pipefail
+set -o nounset
+# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+# set -o xtrace
 
 export TF_LOG=""
 
@@ -18,6 +15,6 @@ terraform init -input=false -backend=true -reconfigure \
     -backend-config="resource_group_name=$TF_VAR_mgmt_resource_group_name" \
     -backend-config="storage_account_name=$TF_VAR_mgmt_storage_account_name" \
     -backend-config="container_name=$TF_VAR_terraform_state_container_name" \
-    -backend-config="key=tre-workspace-service-gitea-$TF_VAR_id"
+    -backend-config="key=tre-workspace-service-gitea-${TF_VAR_id}"
 
 terraform destroy -auto-approve
