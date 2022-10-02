13f356a81d448ea27da08aac81df2ad402a2db8b
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-10-02 06:16:28 +0000

    tflint

--- templates/core/terraform/main.tf
+++ templates/core/terraform/main.tf
@@ -9,10 +9,6 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.4.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
-    }
     local = {
       source  = "hashicorp/local"
       version = "~> 2.2.0"
