e08d14f603f78c34b84f43eaea633b56368ad4a5
Tanya Borisova <tborisova@microsoft.com>
2022-10-10 15:32:36 +0100

    WIP: just comments

---
+++ api_app/api/routes/airlock/review_vm.py
@@ -0,0 +1,21 @@
+from models.schemas.operation import OperationInList, OperationInResponse
+# TODO:
+
+# - method for creating review VM
+# - method for removing review VM
+
+def remove_review_vm(request_id: str) -> OperationInResponse:
+    # Find a user resource that has:
+    # - template name that is export review vm
+    # - request ID property
+    # - this request ID as property
+    # Issue a delete operation
+    review_vm = resource_repo.query
+
+    pass
+    # How to watch an operation
+    # - decided not to watch an operation, if the delete fails, it will be visible in the UI
+
+# - method for looking up a corresponding workspace
+# - method for looking up a corresponding workspace service
+
