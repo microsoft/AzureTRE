a11e37f6879d02f1fa324e4f9b9105299c469885
JaimieWi <92854957+JaimieWi@users.noreply.github.com>
2022-10-11 16:19:18 +0000

    versions

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/vm_config.ps1
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/vm_config.ps1
@@ -2,7 +2,7 @@ if( ${SharedStorageAccess} -eq 1 )
 {
   $Command = "net use z: \\${StorageAccountName}.file.core.windows.net\${FileShareName} /u:AZURE\${StorageAccountName} ${StorageAccountKey}"
   $Command | Out-File  "C:\ProgramData\Start Menu\Programs\StartUp\attatch_storage.cmd" -encoding ascii
-  # Remove-Item -LiteralPath "C:\AzureData" -Force -Recurse
+  Remove-Item -LiteralPath "C:\AzureData" -Force -Recurse
 }
 
 $PipConfigFolderPath = "C:\ProgramData\pip\"
