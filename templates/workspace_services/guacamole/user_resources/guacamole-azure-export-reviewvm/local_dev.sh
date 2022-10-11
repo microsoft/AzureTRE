f1b5d14160798a830e4160a12a21c99d478ab241
Tanya Borisova <tborisova@microsoft.com>
2022-10-11 23:26:21 +0100

    Remove testing script

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-export-reviewvm/local_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-AZURE_TRE_VERSION="0.5.0"
-
-curl -o azuretre.tar.gz -L "https://github.com/microsoft/AzureTRE/archive/refs/tags/v${AZURE_TRE_VERSION}.tar.gz" \
-  && tar -xzf azuretre.tar.gz "AzureTRE-${AZURE_TRE_VERSION}/templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm"  --strip-components=6 --skip-old-files \
-  && rm -rf azuretre.tar.gz
-
-patch -p0 < ./windowsvm.diff
