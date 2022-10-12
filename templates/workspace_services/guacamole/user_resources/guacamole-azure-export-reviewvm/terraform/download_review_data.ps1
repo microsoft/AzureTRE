58f57aa248a4cccffc468a1c1b2cdbbca501fefb
Tanya Borisova <tborisova@microsoft.com>
2022-10-12 11:13:45 +0100

    Fix path so that data ends up on desktop

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-export-reviewvm/terraform/download_review_data.ps1
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-export-reviewvm/terraform/download_review_data.ps1
@@ -1,3 +1,3 @@
-$DownloadPath = "C:\Users\ReviewData"
+$DownloadPath = $env:Public + "\Desktop\ReviewData"
 mkdir $DownloadPath
 az storage blob download-batch -d $DownloadPath -s '"${airlock_request_sas_url}"'
