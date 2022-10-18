c7976f2a7e3737d5f6a7c846c1b840659b45d212
Stuart Leeks <stuartle@microsoft.com>
2022-10-18 13:23:00 +0000

    Fix table output for airlock list

--- cli/tre/commands/workspaces/airlock/requests.py
+++ cli/tre/commands/workspaces/airlock/requests.py
@@ -5,7 +5,7 @@ from tre.api_client import ApiClient
 from tre.commands.workspaces.contexts import pass_workspace_context
 from tre.output import output, output_option, query_option
 
-_default_table_query_list = r"airlockRequests[].{id:id,workspace_id:workspaceId,type:requestType,status:status,business_justification:businessJustification}"
+_default_table_query_list = r"airlockRequests[].airlockRequest.{id:id,workspace_id:workspaceId,type:requestType,status:status,business_justification:businessJustification}"
 _default_table_query_item = r"airlockRequest.{id:id,workspace_id:workspaceId,type:requestType,status:status,business_justification:businessJustification}"
 
 
