d1fc00ed7c0146f8ca43628b72880aec4c3681d9
guybartal <guybartal@gmail.com>
2022-10-23 15:01:52 +0000

    test missing resource tags

--- templates/workspaces/base/terraform/storage.tf
+++ templates/workspaces/base/terraform/storage.tf
@@ -4,7 +4,6 @@ resource "azurerm_storage_account" "stg" {
   location                 = azurerm_resource_group.ws.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-  tags                     = local.tre_workspace_tags
 
   lifecycle { ignore_changes = [tags] }
 }
