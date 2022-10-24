82fe1d523b11c3b7d21c1adacae4500644132646
Stuart Leeks <stuartle@microsoft.com>
2022-10-24 15:30:24 +0000

    Add ping to core_router

--- api_app/api/routes/api.py
+++ api_app/api/routes/api.py
@@ -34,6 +34,7 @@ router.include_router(ping.router, tags=["health"])
 # Core API
 core_router = APIRouter(prefix=config.API_PREFIX)
 core_router.include_router(health.router, tags=["health"])
+core_router.include_router(ping.router, tags=["health"])
 core_router.include_router(workspace_templates.workspace_templates_admin_router, tags=["workspace templates"])
 core_router.include_router(workspace_service_templates.workspace_service_templates_core_router, tags=["workspace service templates"])
 core_router.include_router(user_resource_templates.user_resource_templates_core_router, tags=["user resource templates"])
