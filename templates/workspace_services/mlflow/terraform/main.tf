fd8b404f98d794265fcf76bb74ccd677e8cd8731
marrobi <marrobi@microsoft.com>
2022-11-15 21:52:32 +0000

    Fix linting

--- templates/workspace_services/mlflow/terraform/main.tf
+++ templates/workspace_services/mlflow/terraform/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = "=3.4.2"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "=2.2.3"
+    }
   }
 
   backend "azurerm" {
