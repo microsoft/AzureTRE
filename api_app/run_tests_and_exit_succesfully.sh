3876fe5a1f530738f4390d84dbc99ca506ba80eb
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2022-11-27 13:05:05 +0000

    fix pyetst asyncio

--- api_app/run_tests_and_exit_succesfully.sh
+++ api_app/run_tests_and_exit_succesfully.sh
@@ -4,7 +4,7 @@
 # 2. Run pytest, but capture the exit code so we always succeed
 # 3. Output a file if the tests are not successful.
 rm -f ../test-results/pytest_unit_failed
-pytest_result=$(pytest --junit-xml ../test-results/pytest_api_unit.xml --ignore e2e_tests)
+pytest_result=$(pytest --asyncio-mode=auto --junit-xml ../test-results/pytest_api_unit.xml --ignore e2e_tests)
 
 if [ $? != 0 ]; then
   touch ../test-results/pytest_api_unit_failed
