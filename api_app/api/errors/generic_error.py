71fc930e5100e1f826f7b7d988c8c05b50e816cd
Yuval Yaron <yuvalyaron@microsoft.com>
2022-12-26 13:58:43 +0000

    remove possible reveal of sensitive information (CodeQL alerted)

--- api_app/api/errors/generic_error.py
+++ api_app/api/errors/generic_error.py
@@ -1,16 +1,12 @@
 import logging
-import traceback
+from resources import strings
 
 from fastapi import Request
 from fastapi.responses import PlainTextResponse
-
-from core import config
-from resources import strings
 
 
 async def generic_error_handler(_: Request, exception: Exception) -> PlainTextResponse:
     logging.debug("=====================================")
     logging.exception(exception)
     logging.debug("=====================================")
-    error_string = traceback.format_exc() if config.DEBUG else strings.UNABLE_TO_PROCESS_REQUEST
-    return PlainTextResponse(error_string, status_code=500)
+    return PlainTextResponse(strings.UNABLE_TO_PROCESS_REQUEST, status_code=500)
