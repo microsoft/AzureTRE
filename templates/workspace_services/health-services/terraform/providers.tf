434bd2fe817e77d5927bf933b52e0582bf9e9fb5
LizaShak <iliza@outlook.com>
2023-01-04 07:26:26 +0000

    Fix TF format

--- templates/workspace_services/health-services/terraform/providers.tf
+++ templates/workspace_services/health-services/terraform/providers.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.37.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "=3.4.2"
-    }
   }
 
   backend "azurerm" {}
