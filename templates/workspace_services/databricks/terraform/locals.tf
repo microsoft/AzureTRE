2f97dd3b8bbd1a25f7ef9475c778a53f6895bbf9
guybartal <guybartal@gmail.com>
2023-01-12 16:36:56 +0000

    add service suffix to all DB resources

--- templates/workspace_services/databricks/terraform/locals.tf
+++ templates/workspace_services/databricks/terraform/locals.tf
@@ -5,19 +5,19 @@ locals {
   public_subnet_address_space  = local.databricks_subnets[1] # .128 - .254
 
   short_workspace_id             = substr(var.workspace_id, -4, -1)
-  workspace_resource_name_suffix = "${var.tre_id}-ws-${local.short_workspace_id}"
-  resource_group_name            = "rg-${local.workspace_resource_name_suffix}"
-  keyvault_name                  = "kv-${local.workspace_resource_name_suffix}"
-  storage_name                   = "stg${replace(local.workspace_resource_name_suffix, "-", "")}"
-  virtual_network_name           = "vnet-${local.workspace_resource_name_suffix}"
+  service_resource_name_suffix   = "${var.tre_id}-ws-${local.short_workspace_id}-svc-${local.short_service_id}"
+  resource_group_name            = "rg-${local.service_resource_name_suffix}"
+  keyvault_name                  = "kv-${local.service_resource_name_suffix}"
+  storage_name                   = "stg${replace(local.service_resource_name_suffix, "-", "")}"
+  virtual_network_name           = "vnet-${local.service_resource_name_suffix}"
   core_resource_group_name       = "rg-${var.tre_id}"
   firewall_name                  = "fw-${var.tre_id}"
-  databricks_workspace_name      = "dbw-${local.workspace_resource_name_suffix}"
-  managed_resource_group_name    = "mrg-db-${local.workspace_resource_name_suffix}"
-  public_subnet_name             = "public-${local.workspace_resource_name_suffix}"
-  private_subnet_name            = "private-${local.workspace_resource_name_suffix}"
-  network_security_group_name    = "nsg-${local.workspace_resource_name_suffix}"
-  route_table_name               = "rt-${local.workspace_resource_name_suffix}"
+  databricks_workspace_name      = "dbw-${local.service_resource_name_suffix}"
+  managed_resource_group_name    = "mrg-db-${local.service_resource_name_suffix}"
+  public_subnet_name             = "public-${local.service_resource_name_suffix}"
+  private_subnet_name            = "private-${local.service_resource_name_suffix}"
+  network_security_group_name    = "nsg-${local.service_resource_name_suffix}"
+  route_table_name               = "rt-${local.service_resource_name_suffix}"
 
   map_location_url_config = jsondecode(file("${path.module}/databricks-udr.json"))
 
