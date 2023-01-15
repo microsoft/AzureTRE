03ff36e50ff2bb939e1a6ace588b14c29468e3d6
guybartal <guybartal@gmail.com>
2023-01-15 09:57:36 +0000

    remove unused local keys

--- templates/workspace_services/databricks/terraform/locals.tf
+++ templates/workspace_services/databricks/terraform/locals.tf
@@ -7,8 +7,6 @@ locals {
   workspace_resource_name_suffix = "${var.tre_id}-ws-${local.short_workspace_id}"
   service_resource_name_suffix   = "${var.tre_id}-ws-${local.short_workspace_id}-svc-${local.short_service_id}"
   resource_group_name            = "rg-${var.tre_id}-ws-${local.short_workspace_id}"
-  keyvault_name                  = lower("kv-${substr(local.workspace_resource_name_suffix, -20, -1)}")
-  storage_name                   = lower(replace("stg${substr(local.service_resource_name_suffix, -8, -1)}", "-", ""))
   virtual_network_name           = "vnet-${local.workspace_resource_name_suffix}"
   core_resource_group_name       = "rg-${var.tre_id}"
   firewall_name                  = "fw-${var.tre_id}"
