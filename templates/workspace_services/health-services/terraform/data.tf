42d386842c23d6ea47563be2fa8ccfc5da1174f8
LizaShak <iliza@outlook.com>
2023-01-15 11:04:07 +0000

    Move to pipeline

--- templates/workspace_services/health-services/terraform/data.tf
+++ templates/workspace_services/health-services/terraform/data.tf
@@ -17,25 +17,6 @@ data "azurerm_key_vault_secret" "aad_tenant_id" {
   key_vault_id = data.azurerm_key_vault.ws.id
 }
 
-data "azurerm_firewall" "fw" {
-  name                = "fw-${var.tre_id}"
-  resource_group_name = "rg-${var.tre_id}"
-}
-
-data "external" "rule_priorities" {
-  program = ["bash", "-c", "./get_firewall_priorities.sh"]
-
-  query = {
-    firewall_name          = data.azurerm_firewall.fw.name
-    resource_group_name    = data.azurerm_firewall.fw.resource_group_name
-    collection_name_suffix = "${local.service_resource_name_suffix}-aml"
-  }
-  depends_on = [
-    null_resource.az_login_sp,
-    null_resource.az_login_msi
-  ]
-}
-
 data "azurerm_subnet" "services" {
   name                 = "ServicesSubnet"
   virtual_network_name = data.azurerm_virtual_network.ws.name
