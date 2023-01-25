2b31cd7be5f2236c13a6f0979f4342fee7f0368b
guybartal <guybartal@gmail.com>
2023-01-25 11:06:18 +0000

    add remark and reference for databricks udr

--- templates/workspace_services/databricks/terraform/locals.tf
+++ templates/workspace_services/databricks/terraform/locals.tf
@@ -16,6 +16,7 @@ locals {
   container_subnet_name          = "adb-container-subnet-${local.service_resource_name_suffix}"
   network_security_group_name    = "nsg-${local.service_resource_name_suffix}"
   route_table_name               = "rt-${local.service_resource_name_suffix}"
+  # databricks-udr.json was build according to this page https://learn.microsoft.com/en-us/azure/databricks/administration-guide/cloud-configurations/azure/udr
   map_location_url_config        = jsondecode(file("${path.module}/databricks-udr.json"))
   storage_name                   = lower(replace("stgdbfs${substr(local.service_resource_name_suffix, -8, -1)}", "-", ""))
 
