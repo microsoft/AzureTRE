2cd85f7ce4ea76e321b4c490531b62470100a083
guybartal <guybartal@gmail.com>
2023-01-25 20:40:43 +0000

    ignore SC2154

--- templates/workspace_services/databricks/terraform/deploy.sh
+++ templates/workspace_services/databricks/terraform/deploy.sh
@@ -3,12 +3,14 @@
 set -o errexit
 set -o pipefail
 set -o nounset
+
 # Uncomment this line to see each command for debugging (careful: this will show secrets!)
-set -o xtrace
+#set -o xtrace
 
 export TF_LOG="TRACE"
 export TF_LOG_PATH="/home/adminuser/tf.log"
 
+# shellcheck disable=SC2154
 terraform init -input=false -backend=true -reconfigure \
     -backend-config="resource_group_name=$TF_VAR_mgmt_resource_group_name" \
     -backend-config="storage_account_name=$TF_VAR_mgmt_storage_account_name" \
