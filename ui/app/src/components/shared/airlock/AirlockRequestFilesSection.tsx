39820b9cfa207ba72ce987e5ef2c28dd0a9c05d5
Yuval Yaron <yuvalyaron@microsoft.com>
2023-02-02 15:50:34 +0000

    add CLI section to file upload options (no styling to command yet)

--- ui/app/src/components/shared/airlock/AirlockRequestFilesSection.tsx
+++ ui/app/src/components/shared/airlock/AirlockRequestFilesSection.tsx
@@ -1,5 +1,5 @@
 import { MessageBar, MessageBarType, Pivot, PivotItem, PrimaryButton, Stack, TextField } from "@fluentui/react";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { HttpMethod, useAuthApiCall } from "../../../hooks/useAuthApiCall";
 import { AirlockRequest, AirlockRequestStatus } from "../../../models/airlock";
 import { ApiEndpoint } from "../../../models/apiEndpoints";
@@ -12,10 +12,29 @@ interface AirlockRequestFilesSectionProps {
 }
 
 export const AirlockRequestFilesSection: React.FunctionComponent<AirlockRequestFilesSectionProps> = (props: AirlockRequestFilesSectionProps) => {
+
   const [filesLink, setFilesLink] = useState<string>();
+  const [cliCommand, setCliCommand] = useState<string>();
+
   const [filesLinkError, setFilesLinkError] = useState(false);
   const [apiFilesLinkError, setApiFilesLinkError] = useState({} as APIError);
+  const [cliCommandError, setCliCommandError] = useState(false);
+  const [apiCliCommandError, setApiCliCommandError] = useState({} as APIError);
+
   const apiCall = useAuthApiCall();
+
+  const parseSasUrl = (sasUrl: string) => {
+    const match = sasUrl.match(/https:\/\/(.*?).blob.core.windows.net\/(.*)\?(.*)$/);
+    if (!match) {
+      return
+    }
+
+    return {
+      StorageAccountName: match[1],
+      containerName: match[2],
+      sasToken: match[3]
+    }
+  }
 
   // Retrieve a link to view/edit the airlock files
   const generateFilesLink = useCallback(async () => {
@@ -34,6 +53,40 @@ export const AirlockRequestFilesSection: React.FunctionComponent<AirlockRequestF
       }
     }
   }, [apiCall, props.request, props.workspaceApplicationIdURI]);
+
+  const generateAzureCliCommand = useCallback(async () => {
+    if (props.request && props.request.workspaceId) {
+      try {
+        const linkObject = await apiCall(
+          `${ApiEndpoint.Workspaces}/${props.request.workspaceId}/${ApiEndpoint.AirlockRequests}/${props.request.id}/${ApiEndpoint.AirlockLink}`,
+          HttpMethod.Get,
+          props.workspaceApplicationIdURI
+        );
+
+        let containerDetails = parseSasUrl(linkObject.containerUrl)
+        if (!containerDetails) {
+          return
+        }
+
+        let cliCommand = "";
+        if (props.request.status === AirlockRequestStatus.Draft) {
+          cliCommand = `az storage blob upload --file <~/destination/path/for/file> --name <filename.filetype> --account-name ${containerDetails.StorageAccountName} --type block --container-name ${containerDetails.containerName} --sas-token "${containerDetails.sasToken}"`
+        } else if (props.request.status === AirlockRequestStatus.Approved) {
+          cliCommand = `az storage blob download-batch --destination <~/destination/path/for/file> --source ${containerDetails.containerName} --pattern \\* --account-name ${containerDetails.StorageAccountName} --sas-token "${containerDetails.sasToken}"`
+        }
+
+        setCliCommand(cliCommand);
+      } catch (err: any) {
+        err.userMessage = 'Error generating CLI command';
+        setApiCliCommandError(err);
+        setCliCommandError(true);
+      }
+    }
+  }, [apiCall, props.request, props.workspaceApplicationIdURI]);
+
+  useEffect(() => {
+    generateAzureCliCommand()
+  }, [generateAzureCliCommand]);
 
   return (
     <Pivot aria-label="Storage options">
@@ -69,7 +122,36 @@ export const AirlockRequestFilesSection: React.FunctionComponent<AirlockRequestF
         </Stack>
       </PivotItem>
       <PivotItem headerText="CLI">
-        
+        <Stack>
+          <Stack.Item style={{ paddingTop: '10px', paddingBottom: '10px' }}>
+            <small>Use Azure command-line interface (Azure CLI) to interact with the storage container.</small>
+            <hr style={{ border: "1px solid #ddd", borderRadius: "1px" }} />
+          </Stack.Item>
+          <Stack.Item style={{ paddingTop: '10px', paddingBottom: '10px' }}>
+            {
+              props.request.status === AirlockRequestStatus.Draft
+                ? <small>Upload a file to the storage container.</small>
+                : <small>Download the file from the storage container.</small>
+            }
+            {!cliCommandError &&
+              <>
+                <Stack horizontal styles={{ root: { alignItems: 'center', paddingTop: '7px' } }}>
+                  <Stack.Item grow>
+                    <TextField readOnly value={cliCommand} defaultValue="" multiline autoAdjustHeight={true} resizable={false} />
+                  </Stack.Item>
+                  <PrimaryButton
+                    iconProps={{ iconName: 'copy' }}
+                    styles={{ root: { minWidth: '40px' } }}
+                    onClick={() => { cliCommand && navigator.clipboard.writeText(cliCommand) }}
+                  />
+                </Stack>
+              </>
+            }
+          </Stack.Item>
+          {
+            cliCommandError && <ExceptionLayout e={apiCliCommandError} />
+          }
+        </Stack>
       </PivotItem>
     </Pivot>
   );
