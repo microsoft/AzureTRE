73ef74e72b1ffb0c4d4c1337aaa8462e65c35b56
Guy Bertental <guybartal@gmail.com>
2023-02-06 14:24:17 +0000

    remove auth private endpoint from ADB ws service

--- templates/workspace_services/databricks/terraform/network.tf
+++ templates/workspace_services/databricks/terraform/network.tf
@@ -187,27 +187,27 @@ resource "azurerm_private_endpoint" "databricks_control_plane_private_endpoint" 
   }
 }
 
-resource "azurerm_private_endpoint" "databricks_auth_private_endpoint" {
-  name                = "pe-adb-auth-${local.service_resource_name_suffix}"
-  location            = data.azurerm_resource_group.ws.location
-  resource_group_name = data.azurerm_resource_group.ws.name
-  subnet_id           = data.azurerm_subnet.services.id
-  tags                = local.tre_workspace_service_tags
+# resource "azurerm_private_endpoint" "databricks_auth_private_endpoint" {
+#   name                = "pe-adb-auth-${local.service_resource_name_suffix}"
+#   location            = data.azurerm_resource_group.ws.location
+#   resource_group_name = data.azurerm_resource_group.ws.name
+#   subnet_id           = data.azurerm_subnet.services.id
+#   tags                = local.tre_workspace_service_tags
 
-  lifecycle { ignore_changes = [tags] }
+#   lifecycle { ignore_changes = [tags] }
 
-  private_service_connection {
-    name                           = "private-service-connection-databricks-auth-${local.service_resource_name_suffix}"
-    private_connection_resource_id = azurerm_databricks_workspace.databricks.id
-    is_manual_connection           = false
-    subresource_names              = ["browser_authentication"]
-  }
+#   private_service_connection {
+#     name                           = "private-service-connection-databricks-auth-${local.service_resource_name_suffix}"
+#     private_connection_resource_id = azurerm_databricks_workspace.databricks.id
+#     is_manual_connection           = false
+#     subresource_names              = ["browser_authentication"]
+#   }
 
-  private_dns_zone_group {
-    name                 = "private-dns-zone-group-databricks-auth-${local.service_resource_name_suffix}"
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.databricks.id]
-  }
-}
+#   private_dns_zone_group {
+#     name                 = "private-dns-zone-group-databricks-auth-${local.service_resource_name_suffix}"
+#     private_dns_zone_ids = [data.azurerm_private_dns_zone.databricks.id]
+#   }
+# }
 
 resource "azurerm_private_endpoint" "databricks_filesystem_private_endpoint" {
   name                = "pe-adb-fs-${local.service_resource_name_suffix}"
