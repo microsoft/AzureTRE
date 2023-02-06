15d9f27cb7ad25069575b252a6f99c4c34404878
Yuval Yaron <43217306+yuvalyaron@users.noreply.github.com>
2023-02-06 19:28:13 +0200

    Add Azure CLI command to Airlock Files Section (#3196)
    * replace invalid statuses with valid statuses - exclude submitted, cancelled, and other in_progresses statuses
    
    * move the logic of the airlock request files section to its own file
    
    * add pivot table for switching between file upload options
    
    * add CLI section to file upload options (no styling to command yet)
    
    * fix cli command text box height
    
    * add styling and finishes to the cli command
    
    * move the logic of the  cli command to its own component
    
    * improve regex for matching all forms of cli commands
    
    * add comments to explain the regexes
    
    * remove redundant pattern param from cli command
    
    * dependabot PR #3192
    
    * update changelog and version
    
    * remove SAS URL reference from info message
    
    * change comment in upload cli command
    
    * Update ui/app/src/components/shared/airlock/AirlockRequestFilesSection.tsx
    
    Co-authored-by: James Griffin <me@JamesGriff.in>
    
    * fix CR comments
    
    * remove generate link button
    
    * add tooltip to copy SAS URL button
    
    ---------
    
    Co-authored-by: James Griffin <me@JamesGriff.in>

---
+++ ui/app/src/components/shared/CliCommand.tsx
@@ -0,0 +1,78 @@
+import { IconButton, Spinner, Stack, TooltipHost } from "@fluentui/react";
+import React, { useState } from "react";
+import { Text } from '@fluentui/react/lib/Text';
+
+interface CliCommandProps {
+  command: string,
+  title: string,
+  isLoading: boolean
+}
+
+export const CliCommand: React.FunctionComponent<CliCommandProps> = (props: CliCommandProps) => {
+
+  const COPY_TOOL_TIP_DEFAULT_MESSAGE = "Copy to clipboard"
+  const [copyToolTipMessage, setCopyToolTipMessage] = useState<string>(COPY_TOOL_TIP_DEFAULT_MESSAGE);
+
+  const handleCopyCommand = () => {
+    navigator.clipboard.writeText(props.command);
+    setCopyToolTipMessage("Copied")
+    setTimeout(() => setCopyToolTipMessage(COPY_TOOL_TIP_DEFAULT_MESSAGE), 3000);
+  }
+
+  const renderCommand = () => {
+    // regex to match only the command part (without the parameters)
+    const commandMatches = props.command.match(/^((?! -).)*/);
+
+    if (!commandMatches) {
+      return
+    }
+
+    const commandWithoutParams = commandMatches[0]
+    const paramsOnly = props.command.replace(commandWithoutParams, '')
+    // regex to match all the parameters, along with their assigned values
+    const paramsList = paramsOnly.match(/(?<= )-{1,2}[\w-]+(?:(?!( -){1,2}).)*/g)
+
+    return <Stack styles={{ root: { padding: "15px", backgroundColor: "#f2f2f2", border: '1px solid #e6e6e6' } }}>
+      <code style={{ color: "blue", fontSize: "13px" }}>
+        {commandWithoutParams}
+      </code>
+      <Stack.Item style={{ paddingLeft: "30px" }}>
+        {paramsList?.map((paramWithValue) => {
+          // split the parameter from it's value
+          const splitParam = paramWithValue.split(/\s(.*)/)
+
+          const param = splitParam[0];
+          const paramValue = ` ${splitParam[1] || ''}`;
+          const paramValueIsComment = paramValue?.match(/<.*?>/);
+
+          return (
+            <div style={{ wordBreak: "break-all", fontSize: "13px" }}>
+              <code style={{ color: "teal" }}>{param}</code>
+              <code style={{ color: paramValueIsComment ? "firebrick" : "black" }}>{paramValue}</code>
+            </div>
+          );
+        })}
+      </Stack.Item>
+    </Stack >
+  }
+
+  return (
+    <Stack>
+      <Stack horizontal style={{ backgroundColor: "#e6e6e6", alignItems: 'center' }}>
+        <Stack.Item grow style={{ paddingLeft: "10px", height: "100%" }}>
+          <Text >{props.title}</Text>
+        </Stack.Item>
+        <Stack.Item align="end">
+          <TooltipHost content={copyToolTipMessage}>
+            <IconButton
+              iconProps={{ iconName: 'copy' }}
+              styles={{ root: { minWidth: '40px' } }}
+              onClick={() => { props.command && handleCopyCommand() }} />
+          </TooltipHost>
+        </Stack.Item>
+      </Stack>
+      {(!props.isLoading) ? renderCommand() :
+        <Spinner label="Generating command..." style={{ padding: "15px", backgroundColor: "#f2f2f2", border: '1px solid #e6e6e6' }} />}
+    </Stack>
+  );
+}
