f0290b9f3ce73f9a0a7841b0c2c780683971ae4e
Yuval Yaron <43217306+yuvalyaron@users.noreply.github.com>
2023-03-13 16:42:19 +0200

    Azure CLI should login to the right cloud (#3336)
    * add env var AZ_CLOUD_ENVIRONMENT to control the az cli environment with default value of AzureCloud
    
    * set Azure CLI cloud environment in the resource processor/vmss
    
    * set azure cloud environment in templates
    
    * set azure cloud in 'az login' of resource processor
    
    * remove az_cloud_environment from config.yaml - instead infer it from arm_environment
    
    * update template versions
    
    * update version
    
    * remove 'az cloud set' call and export AZ_CLOUD_ENVIRONMENT
    
    * bump patch version instead of minor version
    
    * add AZ_CLOUD_ENVIRONMENT to parameters.json
    
    * update version
    
    * update core version

--- devops/scripts/load_and_validate_env.sh
+++ devops/scripts/load_and_validate_env.sh
@@ -65,8 +65,14 @@ else
     # Export as Terraform keys env vars
     # shellcheck disable=SC2046
     export $(yq e "$GET_LEAF_KEYS|$TF_KEYS| $FORMAT_FOR_ENV_EXPORT" config.yaml)
+
     TRE_URL=${TRE_URL:-https://${TRE_ID}.${LOCATION}.cloudapp.azure.com}
     export TRE_URL
+
+    # Set AZ_CLOUD_ENVIRONMENT based on the ARM_ENVIRONMENT env var
+    declare -A azure_environments=( ["public"]="AzureCloud" ["usgovernment"]="AzureUSGovernment")
+    export AZ_CLOUD_ENVIRONMENT="${azure_environments[${ARM_ENVIRONMENT}]}"
+
 fi
 
 set +o nounset
