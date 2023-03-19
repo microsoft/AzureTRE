16fb664c956df6da0a43301002cb56f06a852965
Yuval Yaron <yuvalyaron@microsoft.com>
2023-03-19 11:44:50 +0000

    remove OmsAgentForLinux extension

--- core/terraform/resource_processor/vmss_porter/main.tf
+++ core/terraform/resource_processor/vmss_porter/main.tf
@@ -84,25 +84,6 @@ resource "azurerm_linux_virtual_machine_scale_set" "vm_linux" {
     )
   }
 
-  extension {
-    auto_upgrade_minor_version = true
-    automatic_upgrade_enabled  = false
-    name                       = "OmsAgentForLinux"
-    publisher                  = "Microsoft.EnterpriseCloud.Monitoring"
-    type                       = "OmsAgentForLinux"
-    type_handler_version       = "1.0"
-
-    protected_settings = jsonencode({
-      "workspaceKey" = var.log_analytics_workspace_primary_key
-    })
-
-    settings = jsonencode({
-      "workspaceId"               = var.log_analytics_workspace_workspace_id
-      "stopOnMultipleConnections" = false
-      "skipDockerProviderInstall" = true
-    })
-  }
-
   automatic_os_upgrade_policy {
     disable_automatic_rollback  = false
     enable_automatic_os_upgrade = true
