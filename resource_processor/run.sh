2c634939a8f76b6cfc3982eaa9e14fad3c48dd1e
Miguel da Silva <miguel.dasilva@emea.nttdata.com>
2023-03-23 23:51:33 +0000

    Removing comments from run.sh

--- resource_processor/run.sh
+++ resource_processor/run.sh
@@ -32,30 +32,30 @@ vault = "${KEY_VAULT_NAME}"
 EOF
 
 # TODO: Remove porter v0 https://github.com/microsoft/AzureTRE/issues/2990
-# echo "Azure cli login..."
-# az login --identity -u "${VMSS_MSI_ID}"
+echo "Azure cli login..."
+az login --identity -u "${VMSS_MSI_ID}"
 
-# echo "Checking if porter v0 state exists..."
-# exits=$(az storage table exists --account-name "${MGMT_STORAGE_ACCOUNT_NAME}" --name "porter" --auth-mode "login" --output tsv)
-# if [ "${exits}" = "True" ]; then
-#   echo "v0 state exists. Checking if migration was completed once before..."
-#   migration_complete_container_name="porter-migration-completed"
-#   exits=$(az storage container exists --account-name "${MGMT_STORAGE_ACCOUNT_NAME}" --name "${migration_complete_container_name}" --auth-mode "login" --output tsv)
-#   if [ "${exits}" = "False" ]; then
-#       echo "${migration_complete_container_name} container doesn't exist. Running porter migration..."
-#       porter storage migrate --old-home "${PORTER_HOME_V0}" --old-account "azurestorage"
-#       echo "Porter migration complete. Creating ${migration_complete_container_name} container to prevert migrating again in the future..."
-#       az storage container create --account-name "${MGMT_STORAGE_ACCOUNT_NAME}" --name "${migration_complete_container_name}" --auth-mode "login" --fail-on-exist
-#       echo "Migration is done."
-#   else
-#       echo "${migration_complete_container_name} container is present. Skipping porter migration."
-#   fi
-# else
-#   echo "Porter v0 state doesn't exist."
-# fi
+echo "Checking if porter v0 state exists..."
+exits=$(az storage table exists --account-name "${MGMT_STORAGE_ACCOUNT_NAME}" --name "porter" --auth-mode "login" --output tsv)
+if [ "${exits}" = "True" ]; then
+  echo "v0 state exists. Checking if migration was completed once before..."
+  migration_complete_container_name="porter-migration-completed"
+  exits=$(az storage container exists --account-name "${MGMT_STORAGE_ACCOUNT_NAME}" --name "${migration_complete_container_name}" --auth-mode "login" --output tsv)
+  if [ "${exits}" = "False" ]; then
+      echo "${migration_complete_container_name} container doesn't exist. Running porter migration..."
+      porter storage migrate --old-home "${PORTER_HOME_V0}" --old-account "azurestorage"
+      echo "Porter migration complete. Creating ${migration_complete_container_name} container to prevert migrating again in the future..."
+      az storage container create --account-name "${MGMT_STORAGE_ACCOUNT_NAME}" --name "${migration_complete_container_name}" --auth-mode "login" --fail-on-exist
+      echo "Migration is done."
+  else
+      echo "${migration_complete_container_name} container is present. Skipping porter migration."
+  fi
+else
+  echo "Porter v0 state doesn't exist."
+fi
 
-# echo "Azure cli logout..."
-# az logout
+echo "Azure cli logout..."
+az logout
 
 # Can't be in the image since DB connection is needed.
 echo "Applying credential sets..."
