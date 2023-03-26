ec7ea855e0a9e0011aa82c6af46cb6023f4c3d24
LizaShak <iliza@outlook.com>
2023-03-26 08:32:33 +0000

    Change module repo

--- core/terraform/network/network.tf
+++ core/terraform/network/network.tf
@@ -174,6 +174,6 @@ resource "azurerm_ip_group" "webapp" {
 }
 
 module "terraform_azurerm_environment_configuration" {
-  source          = "github.com/microsoft/AzureTRE-modules/terraform_azurerm_environment_configuration"
+  source          = "github.com/microsoft/terraform-azurerm-environment-configuration"
   arm_environment = var.arm_environment
 }
