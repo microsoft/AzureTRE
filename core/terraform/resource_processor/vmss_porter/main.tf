16fa72e9d40188d83e234a92f7237cc8d71c5adf
Liza Shakury <42377481+LizaShak@users.noreply.github.com>
2023-04-02 14:32:01 +0300

    Upgrade terraform_azurerm_environment module version (#3402)
    * Upgrade module version
    
    * Upgrade core version

--- core/terraform/resource_processor/vmss_porter/main.tf
+++ core/terraform/resource_processor/vmss_porter/main.tf
@@ -197,6 +197,6 @@ resource "azurerm_key_vault_access_policy" "resource_processor" {
 }
 
 module "terraform_azurerm_environment_configuration" {
-  source          = "git::https://github.com/microsoft/terraform-azurerm-environment-configuration.git?ref=fa7a3809a24f97d43737eaf72ed13eaef70fb369"
+  source          = "git::https://github.com/microsoft/terraform-azurerm-environment-configuration.git?ref=0.2.0"
   arm_environment = var.arm_environment
 }
