4d0f321d18eb90cd03a9a3f65bd7698c4ca23d4b
LizaShak <iliza@outlook.com>
2023-04-16 09:22:34 +0000

    Fix msal version in cli

--- cli/setup.py
+++ cli/setup.py
@@ -42,7 +42,7 @@ setup(
     install_requires=[
         "click==8.1.3",
         "httpx~=0.23.1",
-        "msal >= 1.17.0",
+        "msal == 1.20.0",
         "jmespath==1.0.1",
         "tabulate==0.9.0",
         "pygments==2.14.0",
