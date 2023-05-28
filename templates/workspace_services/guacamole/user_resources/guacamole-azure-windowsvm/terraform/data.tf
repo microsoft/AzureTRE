975e5931a1d53d301b76ba2244fdaada5030f3c6
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2023-05-28 05:07:29 +0000

    use host in windows mount script

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/data.tf
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/data.tf
@@ -31,3 +31,14 @@ data "azurerm_public_ip" "app_gateway_ip" {
   name                = "pip-agw-${var.tre_id}"
   resource_group_name = data.azurerm_resource_group.core.name
 }
+
+data "azurerm_storage_account" "stg" {
+  name                = local.storage_name
+  resource_group_name = data.azurerm_resource_group.ws.name
+}
+
+data "azurerm_storage_share" "shared_storage" {
+  count                = var.shared_storage_access ? 1 : 0
+  name                 = var.shared_storage_name
+  storage_account_name = data.azurerm_storage_account.stg.name
+}
