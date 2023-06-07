9e76366f39cb41eb47ae05e731db0d7f3c7e8c1b
Yuval Yaron <yuvalyaron@microsoft.com>
2023-06-07 08:27:44 +0000

    add ohdsi workspace service (before adjustments to the OSS)

---
+++ templates/workspace_services/ohdsi/scripts/atlas_db_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+admin_user_password="${OHDSI_ADMIN_PASSWORD}${OHDSI_ADMIN_USERNAME}"
+app_user_password="${OHDSI_APP_PASSWORD}${OHDSI_APP_USERNAME}"
+export admin_md5="'md5$(echo -n "$admin_user_password" | md5sum | awk '{ print $1 }')'"
+export app_md5="'md5$(echo -n "$app_user_password" | md5sum | awk '{ print $1 }')'"
+
+printf 'Creating roles and users'
+echo "$(cat ../sql/atlas_create_roles_users.sql)" | envsubst | psql -v ON_ERROR_STOP=0 -e "$MAIN_CONNECTION_STRING"
+printf 'Creating roles and users: done.'
+
+printf 'Creating schema'
+echo "$(cat ../sql/atlas_create_schema.sql)" | envsubst | psql -v ON_ERROR_STOP=1 -e "$OHDSI_ADMIN_CONNECTION_STRING"
+printf 'Creating schema: done.'
+
+printf 'Done'
