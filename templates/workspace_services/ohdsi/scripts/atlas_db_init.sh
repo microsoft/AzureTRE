c5347a95f89313d5c265e36ace10a31d0bdde172
Yuval Yaron <43217306+yuvalyaron@users.noreply.github.com>
2023-06-11 17:46:15 +0300

    Add OHDSI workspace service (#3552)
    * remove todos
    
    * link core vnet to postgres private dns zone when deploying core
    
    * remove synapse references, and add other data sources to the options
    
    * remove postgres_core_dns_link references
    
    * revert synapse reference deletions
    
    * remove non supported dialects
    
    * add execute permission to scripts
    
    * make some of the daimons required
    
    * add required zone field to postgres:
    https://github.com/hashicorp/terraform-provider-azurerm/issues/16888
    
    * add fw rule to allow open id authentication in atlas
    
    * fix firewall step
    
    * add README
    
    * update README
    
    * fix linting errors
    
    * fix linting errors

--- templates/workspace_services/ohdsi/scripts/atlas_db_init.sh
+++ templates/workspace_services/ohdsi/scripts/atlas_db_init.sh
@@ -5,15 +5,17 @@ set -o nounset
 
 admin_user_password="${OHDSI_ADMIN_PASSWORD}${OHDSI_ADMIN_USERNAME}"
 app_user_password="${OHDSI_APP_PASSWORD}${OHDSI_APP_USERNAME}"
-export admin_md5="'md5$(echo -n "$admin_user_password" | md5sum | awk '{ print $1 }')'"
-export app_md5="'md5$(echo -n "$app_user_password" | md5sum | awk '{ print $1 }')'"
+admin_md5=("md5$(echo -n "$admin_user_password" | md5sum | awk '{ print $1 }')")
+export admin_md5
+app_md5=("md5$(echo -n "$app_user_password" | md5sum | awk '{ print $1 }')'")
+export app_md5
 
 printf 'Creating roles and users'
-echo "$(cat ../sql/atlas_create_roles_users.sql)" | envsubst | psql -v ON_ERROR_STOP=0 -e "$MAIN_CONNECTION_STRING"
+envsubst < ../sql/atlas_create_roles_users.sql | psql -v ON_ERROR_STOP=0 -e "$MAIN_CONNECTION_STRING"
 printf 'Creating roles and users: done.'
 
 printf 'Creating schema'
-echo "$(cat ../sql/atlas_create_schema.sql)" | envsubst | psql -v ON_ERROR_STOP=1 -e "$OHDSI_ADMIN_CONNECTION_STRING"
+envsubst < ../sql/atlas_create_schema.sql | psql -v ON_ERROR_STOP=1 -e "$OHDSI_ADMIN_CONNECTION_STRING"
 printf 'Creating schema: done.'
 
 printf 'Done'
