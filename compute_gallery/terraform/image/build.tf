c0cbfe6faef08b938817d7020c55c9542163c027
tom <tom.young.316@gmail.com>
2023-06-12 18:31:36 +0100

    maybe works

--- compute_gallery/terraform/image/build.tf
+++ compute_gallery/terraform/image/build.tf
@@ -4,54 +4,53 @@ resource "azapi_resource" "image_template" {
   parent_id = data.azurerm_resource_group.compute_gallery.id
   location  = var.location
 
-  body = <<EOF
-{
-  "identity": {
-      "type": "UserAssigned",
-      "userAssignedIdentities": {
-              "${var.image_builder_id}": {}
-          }
-  },
-  "properties": {
-      "buildTimeoutInMinutes" : 90,
-
-      "vmProfile":
-          {
-          "vmSize": "Standard_DS2_v2",
-          "osDiskSizeGB": 30
-          },
-
-      "source": {
-          "type": "PlatformImage",
-              "publisher": "canonical",
-              "offer": "0001-com-ubuntu-server-jammy",
-              "sku": "22_04-lts-gen2",
-              "version": "latest"
-
-      },
-      "customize": [
-        {
-            "type": "Shell",
-            "name": "setupVM",
-            "inline": ${jsonencode(split("\n", file("${local.path_to_scripts}/init.sh")))}
-        }
-      ],
-      "distribute": [
-        {
-            "type": "SharedImage",
-            "galleryImageId": "${azurerm_shared_image.image.id}",
-            "runOutputName": "${var.image_definition}",
-            "artifactTags": {
-                "source": "azureVmImageBuilder",
-                "baseosimg": "ubuntu2204"
-            },
-            "replicationRegions": ["uksouth"],
-            "storageAccountType": "Standard_LRS"
-        }
-      ]
+  body = jsonencode({
+  identity = {
+      type = "UserAssigned"
+      userAssignedIdentities = {
+          tostring(var.image_builder_id) = {}
+      }
   }
-}
-  EOF
+  properties = {
+    buildTimeoutInMinutes = 90,
+
+    vmProfile = {
+        vmSize = "Standard_DS2_v2",
+        osDiskSizeGB = 30
+    },
+
+    source = {
+        type = "PlatformImage",
+            publisher = "canonical",
+            offer = "0001-com-ubuntu-server-jammy",
+            sku = "22_04-lts-gen2",
+            version = "latest"
+
+    },
+    customize = [
+      {
+          type = "Shell",
+          name = "setupVM",
+          inline = concat(
+              ["bash"],
+              split("\n", file("${local.path_to_scripts}/init.sh"))
+          )
+      }
+    ],
+    distribute = [
+      {
+          type = "SharedImage",
+          galleryImageId = "${azurerm_shared_image.image.id}",
+          runOutputName = "${var.image_definition}",
+          artifactTags = {
+              source = "azureVmImageBuilder",
+              baseosimg = "ubuntu2204"
+          },
+          replicationRegions = [var.location],
+          storageAccountType = "Standard_LRS"
+      }
+    ]
+  }})
 
   tags = {
     "useridentity" = "enabled"
