5e329f36a95e9f289065c7a314dfbe960b7c1b94
nayeem01 <nayeemabdullah00@gmail.com>
2023-08-13 18:27:36 +0600

    update guacamole variables.tf with description

--- templates/workspace_services/guacamole/terraform/variables.tf
+++ templates/workspace_services/guacamole/terraform/variables.tf
@@ -1,51 +1,68 @@
 variable "workspace_id" {
   type = string
+  description = "The workspace ID"
 }
 variable "aad_authority_url" {
-  type = string
+  type        = string
+  description = "The Azure AD authority URL"
 }
 variable "tre_id" {
-  type = string
+  type        = string
+  description = "The TRE ID"
 }
 variable "mgmt_resource_group_name" {
-  type = string
+  type        = string
+  description = "The management resource group name"
 }
 variable "mgmt_acr_name" {
-  type = string
+  type        = string
+  description = "The management ACR name"
 }
 variable "image_name" {
-  type = string
+  type        = string
+  description = "The Guacamole image name"
 }
 variable "image_tag" {
-  type = string
+  type        = string
+  description = "The Guacamole image tag"
 }
 variable "guac_disable_copy" {
-  type = bool
+  type        = bool
+  description = "Disable copy from the Guacamole workspace"
 }
 variable "guac_disable_paste" {
-  type = bool
+  type        = bool
+  description = "Disable paste to the Guacamole workspace"
 }
 variable "guac_enable_drive" {
-  type = bool
+  type        = bool
+  description = "Enable drive redirection"
 }
 variable "guac_drive_name" {
-  type = string
+  type        = string
+  description = "The drive name"
 }
 variable "guac_drive_path" {
-  type = string
+  type        = string
+  description = "The drive path"
 }
 variable "guac_disable_download" {
-  type = bool
+  type        = bool
+  description = "Disable download from the Guacamole workspace"
 }
 variable "guac_disable_upload" {
-  type = bool
+  type        = bool
+  description = "Disable upload to the Guacamole workspace"
 }
 variable "is_exposed_externally" {
-  type = bool
+  type        = bool
+  description = "Is the Guacamole workspace to be exposed externally?"
 }
 variable "tre_resource_id" {
-  type = string
+  type        = string
+  description = "The workspace service ID"
 }
 variable "arm_environment" {
-  type = string
+  type        = string
+  description = "The ARM cloud environment"
 }
