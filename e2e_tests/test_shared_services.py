487ee91f8ed64af6066daf13dea014a2969bf8ab
marrobi <marrobi@microsoft.com>
2023-08-18 11:24:26 +0000

    Make note re nexus e2e shared service tests

--- e2e_tests/test_shared_services.py
+++ e2e_tests/test_shared_services.py
@@ -92,7 +92,8 @@ async def test_patch_firewall(verify):
 shared_service_templates_to_create = [
     strings.GITEA_SHARED_SERVICE,
     strings.ADMIN_VM_SHARED_SERVICE,
-
+    # Tested in test_create_certs_nexus_shared_service
+    # strings.NEXUS_SHARED_SERVICE,
     strings.AIRLOCK_NOTIFIER_SHARED_SERVICE,
 
     # TODO: fix cyclecloud and enable this
