1143f2ac9f8227c60d5067bf5448cf4dde378bda
Marcus Robinson <marrobi@microsoft.com>
2023-10-03 13:48:13 +0100

    Output is_exposed_externally, for all resources with URI (#3656)

--- templates/workspace_services/mlflow/terraform/outputs.tf
+++ templates/workspace_services/mlflow/terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "internal_connection_uri" {
+output "connection_uri" {
   value = "https://${azurerm_linux_web_app.mlflow.default_hostname}"
+}
+
+output "is_exposed_externally" {
+  value = false
 }
