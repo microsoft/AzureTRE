5e7a41757980561c6aed1dff48698dca167af98f
marrobi <marrobi@microsoft.com>
2023-12-20 17:31:53 +0000

    remove DB create as this is done in terraform

--- api_app/db/events.py
+++ api_app/db/events.py
@@ -2,7 +2,6 @@ from azure.cosmos.aio import CosmosClient
 
 from api.dependencies.database import get_db_client
 from db.repositories.resources import ResourceRepository
-from core import config
 from services.logging import logger
 
 
@@ -10,7 +9,6 @@ async def bootstrap_database(app) -> bool:
     try:
         client: CosmosClient = await get_db_client(app)
         if client:
-            await client.create_database_if_not_exists(id=config.STATE_STORE_DATABASE)
             # Test access to database
             await ResourceRepository.create(client)
             return True
