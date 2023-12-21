11f0f16fd5b3dce31d7e7eaf3ec92df4db8fe036
marrobi <marrobi@microsoft.com>
2023-12-21 12:07:59 +0000

    fix test

--- api_app/tests_ma/test_db/test_events.py
+++ api_app/tests_ma/test_db/test_events.py
@@ -1,9 +1,11 @@
 from unittest.mock import AsyncMock, MagicMock, patch
 from azure.core.exceptions import AzureError
+import pytest
 from api_app.db import events
 
+pytestmark = pytest.mark.asyncio
 
-@patch("api_app.db.events.get_credential_async")
+@patch("api_app.db.events.get_credential")
 @patch("api_app.db.events.CosmosDBManagementClient")
 async def test_bootstrap_database_success(cosmos_db_mgmt_client_mock, get_credential_async_mock):
     get_credential_async_mock.return_value = AsyncMock()
@@ -14,11 +16,11 @@ async def test_bootstrap_database_success(cosmos_db_mgmt_client_mock, get_creden
     assert result is True
 
 
-@patch("api_app.db.events.get_credential_async")
+@patch("api_app.db.events.get_credential")
 @patch("api_app.db.events.CosmosDBManagementClient")
 async def test_bootstrap_database_failure(cosmos_db_mgmt_client_mock, get_credential_async_mock):
     get_credential_async_mock.return_value = AsyncMock()
-    cosmos_db_mgmt_client_mock.side_effect = AzureError()
+    cosmos_db_mgmt_client_mock.side_effect = AzureError("some error")
 
     result = await events.bootstrap_database()
 
