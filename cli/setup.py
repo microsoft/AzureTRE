2b59bf141a591cecb4779dd2973f0a684e3f5155
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2024-02-20 08:18:25 +0000

    update dependencies

--- cli/setup.py
+++ cli/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 PROJECT = 'azure-tre-cli'
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 
 try:
     long_description = open('README.md', 'rt').read()
@@ -49,7 +49,7 @@ setup(
         "PyJWT==2.8.0",
         "azure-cli-core==2.50.0",
         "azure-identity==1.14.1",
-        "aiohttp==3.9.0"
+        "aiohttp==3.9.3"
     ],
 
     namespace_packages=[],
