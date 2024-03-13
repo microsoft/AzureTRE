61bf008780246eee1c54be3acdc75f37bdaf4a7e
nasaldrops <hellokevinoconnor@gmail.com>
2024-03-13 18:59:01 +0000

    removed public_network_access_enabled = false

--- templates/shared_services/gitea/terraform/mysql.tf
+++ templates/shared_services/gitea/terraform/mysql.tf
@@ -15,7 +15,6 @@ resource "azurerm_mysql_flexible_server" "gitea" {
   version                       = "8.0.21"
   backup_retention_days         = 7
   geo_redundant_backup_enabled  = false
-  public_network_access_enabled = false
   tags                          = local.tre_shared_service_tags
 
   lifecycle { ignore_changes = [tags] }
