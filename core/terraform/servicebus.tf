66bfcc8e9499ae110af20ee3ad873a554017e55b
PABenedictEvans <130573860+PABenedictEvans@users.noreply.github.com>
2024-03-19 14:29:12 +0000

    changed service sku to Standard

--- core/terraform/servicebus.tf
+++ core/terraform/servicebus.tf
@@ -2,7 +2,7 @@ resource "azurerm_servicebus_namespace" "sb" {
   name                = "sb-${var.tre_id}"
   location            = azurerm_resource_group.core.location
   resource_group_name = azurerm_resource_group.core.name
-  sku                 = "Premium"
+  sku                 = "Standard"
   capacity            = "1"
   tags                = local.tre_core_tags
 
