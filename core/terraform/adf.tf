02e5b8a7ce96b9cda84a38c54532c80aa661c938
PABenedictEvans <130573860+PABenedictEvans@users.noreply.github.com>
2024-03-27 16:50:58 +0000

    create the integration runtime

--- core/terraform/adf.tf
+++ core/terraform/adf.tf
@@ -1,3 +1,6 @@
+# This script is to create the adf that is used for the data transfer
+# The private endpoint will need approving from the storage account
+# Create the data factory
 resource "azurerm_data_factory" "adf_core" {
   name                            = "adf-${var.tre_id}"
   location                        = azurerm_resource_group.core.location
@@ -5,17 +8,24 @@ resource "azurerm_data_factory" "adf_core" {
   managed_virtual_network_enabled = true
 }
 
+# Create a managed integration runtime
+resource "azurerm_data_factory_integration_runtime_azure" "adf_ir" {
+  name                    = "adf-ir-${var.tre_id}"
+  data_factory_id         = azurerm_data_factory.adf_core.id
+  location                = azurerm_resource_group.core.location
+  virtual_network_enabled = true
+}
+
+# Create a private endpoint to the storage account
 resource "azurerm_data_factory_managed_private_endpoint" "adf_dataplatform_pe" {
   name                = "pe-adf-sadataplatform"
-  data_factory_id     = azurerm_data_factory.adfcore.id
+  data_factory_id     = azurerm_data_factory.adf_core.id
   target_resource_id  = data.azurerm_storage_account.sadataplatform.id
   subresource_name    = "blob"
 }
 
-# resource "azurerm_data_factory_integration_runtime_managed" "adf_ir" {
-#   name            = "adf-ir-${var.tre_id}
-#   data_factory_id = azurerm_data_factory.adf_core.id
-#   location        = azurerm_resource_group.core.location
-#   node
+# TODO - could potentially create a linked service here
+# resource "azurerm_data_factory_linked_service_azure_blob_storage" "adf_ls_dataplatform" {
+#   name            = "adf-ls-${var.tre_id}"
+#   data_factory_id = azurerm_
 # }
-
