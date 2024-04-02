4451488a876535146dc05a2b29e318907f997aba
PABenedictEvans <130573860+PABenedictEvans@users.noreply.github.com>
2024-04-02 14:50:50 +0000

    first end to end tested version

--- templates/workspaces/base-adf/terraform/adf_link.tf
+++ templates/workspaces/base-adf/terraform/adf_link.tf
@@ -9,12 +9,26 @@ resource "azurerm_data_factory_managed_private_endpoint" "adf_dataplatform_pe" {
   name                = "pe-adf-ws-${local.workspace_resource_name_suffix}"
   data_factory_id     = data.azurerm_data_factory.adf_core.id
   target_resource_id  = azurerm_storage_account.stg.id
+  subresource_name    = "file"
 }
 
-# Create a linked service to the file storage
+# TODO - trial the below bash script (null_resource) that approves the private endpoint connection
+resource "null_resource" "approve_private_endpoint" {
+  triggers = {
+    endpoint_id = azurerm_data_factory_managed_private_endpoint.adf_dataplatform_pe.id
+  }
+
+  provisioner "local-exec" {
+    command = "az datafactory private-endpoint-connection approve --id ${azurerm_data_factory_managed_private_endpoint.adf_dataplatform_pe.id} --resource-group ${azurerm_resource_group.ws.name}"
+  }
+}
+
+# Create a linked service in the data factory to the file storage
 resource "azurerm_data_factory_linked_service_azure_file_storage" "ls_shared_file" {
   name                     = "ls-adf-shared_storage-${local.workspace_resource_name_suffix}"
-  data_factory_id          = data.azurerm_data_factory.adf_core
+  data_factory_id          = data.azurerm_data_factory.adf_core.id
   connection_string        = azurerm_storage_account.stg.primary_connection_string
   integration_runtime_name = "adf-ir-${var.tre_id}"
+  file_share               = "vm-shared-storage"
 }
+
