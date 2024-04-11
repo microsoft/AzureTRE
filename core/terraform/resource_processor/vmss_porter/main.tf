3a9eecbf08be075a14a2875cfa97cd21073b4258
tim-allen-ck <tim.allen@cloudkubed.com>
2024-04-11 12:12:16 +0100

    testing

--- core/terraform/resource_processor/vmss_porter/main.tf
+++ core/terraform/resource_processor/vmss_porter/main.tf
@@ -108,7 +108,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vm_linux" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
