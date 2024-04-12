ba9ae5088cdb902953381c9f3909a10d19eca617
tim-allen-ck <tim.allen@cloudkubed.com>
2024-04-12 15:10:09 +0100

    update

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/get_apt_keys.sh
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/get_apt_keys.sh
@@ -6,6 +6,10 @@ set -o nounset
 # Uncomment this line to see each command for debugging (careful: this will show secrets!)
 # set -o xtrace
 
+#remove key if they already exist
+sudo rm-f /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg || true
+sudo rm-f /etc/apt/trusted.gpg.d/microsoft.gpg || true
+
 # Get Docker Public key from Nexus
 curl -fsSL "${NEXUS_PROXY_URL}"/repository/docker-public-key/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
 
