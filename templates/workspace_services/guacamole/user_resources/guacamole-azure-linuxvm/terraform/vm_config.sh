995cdd1ae7011f22de275bed278363b51f94ee11
tim-allen-ck <tim.allen@cloudkubed.com>
2024-05-07 16:53:05 +0100

    update bash

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/vm_config.sh
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/vm_config.sh
@@ -19,7 +19,6 @@ sudo apt-get update || true
 ## Desktop
 echo "init_vm.sh: Desktop"
 sudo systemctl start gdm3 || true
-DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure gdm3 || true
 sudo apt install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
 echo /usr/sbin/gdm3 > /etc/X11/default-display-manager
