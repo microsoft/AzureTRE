92dad6046742f0301a9981e3a1814b4a29320a76
jonnyry <jonnyry@users.noreply.github.com>
2024-06-14 09:50:39 +0100

    Fix lint / build validation issues

--- templates/shared_services/firewall/terraform/locals.tf
+++ templates/shared_services/firewall/terraform/locals.tf
@@ -16,6 +16,6 @@ locals {
 
   firewall_policy_name = "fw-policy-${var.tre_id}"
 
-  default_firewall_sku = "Standard"
+  default_firewall_sku   = "Standard"
   effective_firewall_sku = coalesce(var.firewall_sku, local.default_firewall_sku)
 }
