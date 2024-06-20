0c57d7b5b096e889a68a333bbdbe09ddcd2e8e4f
tim-allen-ck <tim.allen@cloudkubed.com>
2024-06-20 16:52:06 +0100

    update type

--- core/terraform/servicebus.tf
+++ core/terraform/servicebus.tf
@@ -3,7 +3,7 @@ resource "azurerm_servicebus_namespace" "sb" {
   location                     = azurerm_resource_group.core.location
   resource_group_name          = azurerm_resource_group.core.name
   sku                          = "Premium"
-  premium_messaging_partitions = 1
+  premium_messaging_partitions = "1"
   capacity                     = "1"
   tags                         = local.tre_core_tags
 
