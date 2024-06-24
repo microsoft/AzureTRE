fa9c0fc0cf5b5658966b0e8f789ff4250d56e40c
tim-allen-ck <tim.allen@cloudkubed.com>
2024-06-24 09:36:47 +0100

    testing

--- templates/workspaces/base/terraform/providers.tf
+++ templates/workspaces/base/terraform/providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.73.0"
+      version = "=3.108.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "=2.20.0"
+      version = ">=2.20.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "=1.5.0"
+      version = ">=1.5.0"
     }
   }
 
