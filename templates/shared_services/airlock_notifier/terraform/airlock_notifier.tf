69c2129841af1967a9e651d4b09b5677a243e5ee
tim-allen-ck <tim.allen@cloudkubed.com>
2024-07-26 11:00:47 +0100

    airlock initial changes

--- templates/shared_services/airlock_notifier/terraform/airlock_notifier.tf
+++ templates/shared_services/airlock_notifier/terraform/airlock_notifier.tf
@@ -1,10 +1,13 @@
-resource "azurerm_service_plan" "notifier_plan" {
+resource "azurerm_app_service_plan" "notifier_plan" {
   name                = "airlock-notifier-plan-${var.tre_id}"
   resource_group_name = data.azurerm_resource_group.core.name
   location            = data.azurerm_resource_group.core.location
-  sku_name            = "WS1"
-  os_type             = "Windows"
-  tags                = local.tre_shared_service_tags
+  kind                = "elastic"
+  sku {
+    size = "WS1"
+    tier = "WorkflowStandard"
+  }
+  tags = local.tre_shared_service_tags
   lifecycle { ignore_changes = [tags] }
 }
 
@@ -32,7 +35,7 @@ resource "azurerm_logic_app_standard" "logic_app" {
   name                       = "airlock-notifier-app-${var.tre_id}"
   location                   = data.azurerm_resource_group.core.location
   resource_group_name        = data.azurerm_resource_group.core.name
-  app_service_plan_id        = azurerm_service_plan.notifier_plan.id
+  app_service_plan_id        = azurerm_app_service_plan.notifier_plan.id
   storage_account_name       = data.azurerm_storage_account.storage.name
   storage_account_access_key = data.azurerm_storage_account.storage.primary_access_key
   virtual_network_subnet_id  = data.azurerm_subnet.airlock_notification.id
--- templates/shared_services/airlock_notifier/terraform/providers.tf
+++ templates/shared_services/airlock_notifier/terraform/providers.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.57.0"
+      version = "=3.113.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "=2.4.0"
+      version = "=2.5.1"
     }
   }
 
