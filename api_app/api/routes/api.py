da4c850ab2fdb1f50d6dbd3449ef10bb7896f16e
Marcus Robinson <marrobi@microsoft.com>
2024-08-06 11:59:58 +0000

    remove unused metadata

--- api_app/api/routes/api.py
+++ api_app/api/routes/api.py
@@ -24,8 +24,7 @@ workspace_tags_metadata = [
     {"name": "workspaces", "description": " **Workspace Owners and Researchers** can view their own workspaces"},
     {"name": "workspace services", "description": "**Workspace Owners** administer workspace services, **Workspace Owners and Researchers** can view services in the workspaces they belong to"},
     {"name": "user resources", "description": "**Researchers** administer and can view their own researchers, **Workspace Owners** can view/update/delete all user resources in their workspaces"},
-    {"name": "shared services", "description": "**TRE administratiors** administer shared services"},
-    {"name": "users", "description": "Retrieve users assigned to a workspace"},
+    {"name": "shared services", "description": "**TRE administratiors** administer shared services"}
 ]
 
 # Root
