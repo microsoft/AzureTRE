9ef1cb1498b0142583d232e71096b93b222e9710
tim-allen-ck <tim.allen@cloudkubed.com>
2024-08-09 10:39:08 +0100

    update

--- templates/shared_services/airlock_notifier/terraform/providers.tf
+++ templates/shared_services/airlock_notifier/terraform/providers.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.113.0"
     }
-    azapi = {
-      source  = "azure/azapi"
-      version = "=1.14.0"
-    }
   }
   backend "azurerm" {}
 }
