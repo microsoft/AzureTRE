f319f1bb47bb7b0f9fec255879a65cfd8054cd2b
Harry Young <harry.young@hippodigital.co.uk>
2024-08-21 15:53:04 +0100

    Revert back to microsoft environment configuration with new version

--- templates/workspaces/base/terraform/network/network.tf
+++ templates/workspaces/base/terraform/network/network.tf
@@ -105,7 +105,6 @@ resource "azurerm_subnet_route_table_association" "rt_webapps_subnet_association
 }
 
 module "terraform_azurerm_environment_configuration" {
-  #  source          = "git::https://github.com/microsoft/terraform-azurerm-environment-configuration.git?ref=0.2.0"
-  source          = "github.com/hippo-digital/terraform-azurerm-environment-configuration"
+  source          = "git::https://github.com/microsoft/terraform-azurerm-environment-configuration.git?ref=0.6.0"
   arm_environment = var.arm_environment
 }
