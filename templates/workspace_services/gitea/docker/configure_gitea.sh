d1b4c23217cf1f0c5752896ceff7c76ffc820a5f
Miguel da Silva <miguel.dasilva@emeal.nttdata.com>
2024-11-04 14:30:53 +0000

    Fixing Gitea Workspace service configuration script

--- templates/workspace_services/gitea/docker/configure_gitea.sh
+++ templates/workspace_services/gitea/docker/configure_gitea.sh
@@ -19,14 +19,19 @@ echo "Configuring OIDC"
 echo "gitea admin auth add-oauth --name oidc --provider openidConnect --key '${GITEA_OPENID_CLIENT_ID}' --secret '${GITEA_OPENID_CLIENT_SECRET}' --auto-discover-url '${GITEA_OPENID_AUTHORITY}/.well-known/openid-configuration' --group-claim-name 'roles' --admin-group 'WorkspaceOwner'"
 su gitea -c "gitea admin auth add-oauth --name oidc --provider openidConnect --key '${GITEA_OPENID_CLIENT_ID}' --secret '${GITEA_OPENID_CLIENT_SECRET}' --auto-discover-url '${GITEA_OPENID_AUTHORITY}/.well-known/openid-configuration' --group-claim-name 'roles' --admin-group 'WorkspaceOwner'"
 
-echo "Configuring Allowed Domains"
+echo "Configuring Allowed Domains."
 CONFIG_FILE="/data/gitea/conf/app.ini"
 
 # Add the migrations section to the app.ini file if it doesn't exist
-if ! grep -q "\[migrations]" $CONFIG_FILE; then
-echo -e "\n[migrations]" >> $CONFIG_FILE
-fi
 # Add the allowed domains line to the migrations section
 if ! grep -q "ALLOWED_DOMAINS = $GITEA__ALLOWED_DOMAIN" $CONFIG_FILE; then
-echo -e "ALLOWED_DOMAINS = $GITEA__ALLOWED_DOMAIN" >> $CONFIG_FILE
+    echo "Adding Allowed Domains."
+    echo -e "\n[migrations]" >> $CONFIG_FILE
+    echo -e "ALLOWED_DOMAINS = $GITEA__ALLOWED_DOMAIN" >> $CONFIG_FILE
+    sleep 30
+    # Restart gitea
+    echo "Restarting Gitea for enabling new Allowed Domains."
+    su gitea -c "gitea manager restart"
+else
+    echo "Allowed Domains already configured."
 fi
