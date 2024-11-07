a1773d0da51cf76d86db18f7bdb94082b3f7fbc6
Marcus Robinson <marrobi@microsoft.com>
2024-11-07 12:58:51 +0000

    Fix linting.

--- devops/scripts/upgrade.sh
+++ devops/scripts/upgrade.sh
@@ -10,8 +10,33 @@ DIR=$1
 # Load environment variables from .env file
 if [ -f "$DIR/.env" ]; then
   set -a
+  # shellcheck source=/dev/null
   . "$DIR/.env"
   set +a
+fi
+
+# Ensure TF_VAR_mgmt_resource_group_name is set
+if [ -z "${TF_VAR_mgmt_resource_group_name}" ]; then
+  echo "Error: TF_VAR_mgmt_resource_group_name is not set."
+  exit 1
+fi
+
+# Ensure TF_VAR_mgmt_storage_account_name is set
+if [ -z "${TF_VAR_mgmt_storage_account_name}" ]; then
+  echo "Error: TF_VAR_mgmt_storage_account_name is not set."
+  exit 1
+fi
+
+# Ensure TF_VAR_terraform_state_container_name is set
+if [ -z "${TF_VAR_terraform_state_container_name}" ]; then
+  echo "Error: TF_VAR_terraform_state_container_name is not set."
+  exit 1
+fi
+
+# Ensure TRE_ID is set
+if [ -z "${TRE_ID}" ]; then
+  echo "Error: TRE_ID is not set."
+  exit 1
 fi
 
 # Infer the key from the directory names
