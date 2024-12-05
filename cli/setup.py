4c1d20d65a36701a5ee4db1494db93b98afcca63
Tim Allen <tallenp@me.com>
2024-12-05 00:00:02 +0000

    update

--- cli/setup.py
+++ cli/setup.py
@@ -42,7 +42,7 @@ setup(
     install_requires=[
         "click==8.1.3",
         "httpx==0.25.0",
-        "msal==1.26.0",
+        "msal==1.31.0",
         "jmespath==1.0.1",
         "tabulate==0.9.0",
         "pygments==2.16.1",
