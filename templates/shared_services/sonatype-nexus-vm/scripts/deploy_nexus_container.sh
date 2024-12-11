d1d047829b9bd57f884bda1234fbd6e61312ea45
Tony Wildish <TonyWildish@users.noreply.github.com>
2024-12-11 14:55:03 +0000

    Upgrade Nexus VM to larger machine. Update deployment of Nexus container to set Java VM memory limits based on available RAM. Bump version number by 0.0.1

--- templates/shared_services/sonatype-nexus-vm/scripts/deploy_nexus_container.sh
+++ templates/shared_services/sonatype-nexus-vm/scripts/deploy_nexus_container.sh
@@ -20,7 +20,17 @@ while true; do
   ((docker_pull_timeout--));
 done
 
+# Deduce memory available to Java. Either 3/4 of the system RAM, or a set minimum
+mem_total_mb=$(( $(cat /proc/meminfo | head -1 | awk '{ print $2 }') / 1024 ))
+java_mem=2703
+if [ $mem_total_mb -gt 4096 ]; then
+  java_mem=$(( $mem_total_mb * 3 / 4 ))
+fi
+
+echo "System memory: ${mem_total_mb} MB. Java memory: ${java_mem} MB"
+
 docker run -d -p 80:8081 -p 443:8443 -p 8083:8083 -v /etc/nexus-data:/nexus-data \
+    -e INSTALL4J_ADD_VM_PARAMS="-Xmx${java_mem}m -Xms${java_mem}m" \
     --restart always \
     --name nexus \
     --log-driver local \
