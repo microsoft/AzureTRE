50b75106c6070f98ae471896c3ddeb702b3c1f37
Tim Allen <tallenp@me.com>
2024-12-13 16:44:07 +0000

    update aiohttp

--- cli/setup.py
+++ cli/setup.py
@@ -49,7 +49,7 @@ setup(
         "PyJWT==2.9.0",
         "azure-cli-core==2.65.0",
         "azure-identity==1.19.0",
-        "aiohttp==3.10.10"
+        "aiohttp==3.11.10"
     ],
 
     namespace_packages=[],
