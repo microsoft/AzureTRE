14630dcde5a68ec411dcc35d6b1e4626c780b766
Tamir Kamara <takamara@microsoft.com>
2024-12-23 18:52:05 +0000

    small tf fix

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/main.tf
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-linuxvm/terraform/main.tf
@@ -36,32 +36,3 @@ provider "azurerm" {
   }
   storage_use_azuread = true
 }
-
-data "azurerm_resource_group" "ws" {
-  name = "rg-${var.tre_id}-ws-${local.short_workspace_id}"
-}
-
-data "azurerm_resource_group" "core" {
-  name = "rg-${var.tre_id}"
-}
-
-data "azurerm_virtual_network" "ws" {
-  name                = "vnet-${var.tre_id}-ws-${local.short_workspace_id}"
-  resource_group_name = data.azurerm_resource_group.ws.name
-}
-
-data "azurerm_subnet" "services" {
-  name                 = "ServicesSubnet"
-  virtual_network_name = data.azurerm_virtual_network.ws.name
-  resource_group_name  = data.azurerm_resource_group.ws.name
-}
-
-data "azurerm_key_vault" "ws" {
-  name                = local.keyvault_name
-  resource_group_name = data.azurerm_resource_group.ws.name
-}
-
-data "azurerm_public_ip" "app_gateway_ip" {
-  name                = "pip-agw-${var.tre_id}"
-  resource_group_name = data.azurerm_resource_group.core.name
-}
