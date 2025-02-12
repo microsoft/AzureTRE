a920e318c89ddb5026d3ccb91b29a1b43fa1d26c
Marcus Robinson <marrobi@microsoft.com>
2025-02-12 16:03:43 +0000

    Move to Vite build engine

---
+++ ui/app/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react(), tsconfigPaths(), svgr()],
+  server: {
+    port: 3000,
+  }
+})
