8fb4d8fa278afd24cf598fe0564bd25a8d8d8753
Marcus Robinson <marrobi@microsoft.com>
2025-02-19 17:28:03 +0000

    Fix sku and version

--- core/terraform/bastion.tf
+++ core/terraform/bastion.tf
@@ -13,15 +13,12 @@ resource "azurerm_bastion_host" "bastion" {
   name                = "bas-${var.tre_id}"
   resource_group_name = azurerm_resource_group.core.name
   location            = azurerm_resource_group.core.location
-
+  sku                 = var.bastion_sku
+  
   ip_configuration {
     name                 = "configuration"
     subnet_id            = module.network.bastion_subnet_id
     public_ip_address_id = azurerm_public_ip.bastion.id
-  }
-
-  sku {
-    name = var.bastion_sku
   }
 
   tags = local.tre_core_tags
