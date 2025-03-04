f24c008c4b3ec0a93cd36a55d6ca8c2e7d34c643
Miguel da Silva <miguel.dasilva@emeal.nttdata.com>
2025-03-04 09:59:26 +0100

    Synchronizing Terraform module version

--- core/terraform/airlock_auto_cleaner/main.tf
+++ core/terraform/airlock_auto_cleaner/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.58.0"
+      version = ">3.58.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "=3.4.5"
+      version = "~> 3.2.0"
     }
   }
 }
