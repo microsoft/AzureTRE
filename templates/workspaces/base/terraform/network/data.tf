f6d428dcbdc38bafae558caa1786afa87e76eaf1
askew <2727893+askew@users.noreply.github.com>
2025-03-31 12:01:00 +0000

    tf formatting

--- templates/workspaces/base/terraform/network/data.tf
+++ templates/workspaces/base/terraform/network/data.tf
@@ -13,35 +13,35 @@ data "azurerm_virtual_network" "core" {
 }
 
 data "azurerm_subnet" "core_webapps" {
-  provider            = azurerm.core
+  provider             = azurerm.core
   name                 = "WebAppSubnet"
   virtual_network_name = "vnet-${var.tre_id}"
   resource_group_name  = "rg-${var.tre_id}"
 }
 
 data "azurerm_subnet" "shared" {
-  provider            = azurerm.core
+  provider             = azurerm.core
   resource_group_name  = local.core_resource_group_name
   virtual_network_name = local.core_vnet
   name                 = "SharedSubnet"
 }
 
 data "azurerm_subnet" "bastion" {
-  provider            = azurerm.core
+  provider             = azurerm.core
   resource_group_name  = local.core_resource_group_name
   virtual_network_name = local.core_vnet
   name                 = "AzureBastionSubnet"
 }
 
 data "azurerm_subnet" "resourceprocessor" {
-  provider            = azurerm.core
+  provider             = azurerm.core
   resource_group_name  = local.core_resource_group_name
   virtual_network_name = local.core_vnet
   name                 = "ResourceProcessorSubnet"
 }
 
 data "azurerm_subnet" "airlockprocessor" {
-  provider            = azurerm.core
+  provider             = azurerm.core
   resource_group_name  = local.core_resource_group_name
   virtual_network_name = local.core_vnet
   name                 = "AirlockProcessorSubnet"
--- templates/workspaces/base/terraform/network/providers.tf
+++ templates/workspaces/base/terraform/network/providers.tf
@@ -2,8 +2,8 @@ terraform {
   # In modules we should only specify the min version
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.24.0"
+      source                = "hashicorp/azurerm"
+      version               = ">= 4.24.0"
       configuration_aliases = [azurerm, azurerm.core]
     }
   }
