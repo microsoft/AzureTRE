47147117224f562a55e5fd8d07d2f8b436a9e140
askew <2727893+askew@users.noreply.github.com>
2025-04-02 14:28:22 +0000

    User resources also need the workspace subscription id

--- api_app/api/routes/workspaces.py
+++ api_app/api/routes/workspaces.py
@@ -428,6 +428,12 @@ async def create_user_resource(
         logger.exception("User not authorized to use template")
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(e))
 
+    # Get the workspace subscription id (if set)
+    if workspace.properties.get("workspace_subscription_id"):
+        if not resource_template.properties.get("workspace_subscription_id"):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=strings.WORKSPACE_SERVICE_TEMPATE_DOES_NOT_SUPPORT_SUBSCRIPTION)
+        workspace_service.properties["workspace_subscription_id"] = workspace.properties["workspace_subscription_id"]
+
     operation = await save_and_deploy_resource(
         resource=user_resource,
         resource_repo=user_resource_repo,
