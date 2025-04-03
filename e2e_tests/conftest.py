dda6ab679025b5a045ba402839be041598514902
Marcus Robinson <marrobi@microsoft.com>
2025-04-03 14:27:17 +0000

    Add ability to reuse workspace across parallel test proceses

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -1,4 +1,8 @@
+import json
+import os
+from pathlib import Path
 import random
+from filelock import FileLock
 import pytest
 import asyncio
 from typing import Tuple
@@ -17,6 +21,16 @@ pytestmark = pytest.mark.asyncio(loop_scope="session")
 
 def pytest_addoption(parser):
     parser.addoption("--verify", action="store", default="true")
+
+
+def pytest_configure(config):
+    worker_id = os.environ.get("PYTEST_XDIST_WORKER")
+    if worker_id is not None:
+        logging.basicConfig(
+            format=config.getini("log_file_format"),
+            filename=f"tests_{worker_id}.log",
+            level=config.getini("log_file_level"),
+        )
 
 
 @pytest.fixture(scope="session")
@@ -102,21 +116,60 @@ async def clean_up_test_workspace_service(pre_created_workspace_service_id: str,
         await disable_and_delete_ws_resource(workspace_service_path, workspace_id, verify)
 
 
-# Session scope isn't in effect with python-xdist: https://github.com/microsoft/AzureTRE/issues/2868
+def read_json_file(file_path: Path) -> dict:
+    return json.loads(file_path.read_text().replace("'", '"'))
+
+
+def write_json_file(file_path: Path, data: dict):
+    file_path.write_text(json.dumps(data))
+
+
 @pytest.fixture(scope="session")
-async def setup_test_workspace(verify) -> Tuple[str, str, str]:
-    pre_created_workspace_id = config.TEST_WORKSPACE_ID
-    # Set up - uses a pre created app reg as has appropriate roles assigned
-    workspace_path, workspace_id = await create_or_get_test_workspace(
-        auth_type="Manual", verify=verify, pre_created_workspace_id=pre_created_workspace_id, client_id=config.TEST_WORKSPACE_APP_ID, client_secret=config.TEST_WORKSPACE_APP_SECRET)
+async def setup_test_workspace(verify, worker_id, tmp_path_factory) -> Tuple[str, str, str]:
+    LOCK_SUFFIX = ".lock"
+    WORKSPACE_DETAILS_FILE = "setup_test_workspace.json"
+
+    root_tmp_dir = tmp_path_factory.getbasetemp().parent
+    fn = root_tmp_dir / WORKSPACE_DETAILS_FILE
+    lock_fn = root_tmp_dir / (WORKSPACE_DETAILS_FILE + LOCK_SUFFIX)
+    worker_lock = root_tmp_dir / "worker_locks" / worker_id
+
+    worker_lock.parent.mkdir(parents=True, exist_ok=True)
+    worker_lock.touch()
+
+    if worker_id == "master":
+        # Set up - uses a pre-created app reg as it has appropriate roles assigned
+        workspace_path, workspace_id = await create_or_get_test_workspace(
+            auth_type="Manual" if config.TEST_WORKSPACE_APP_ID else "Automatic", verify=verify, pre_created_workspace_id=config.TEST_WORKSPACE_ID, client_id=config.TEST_WORKSPACE_APP_ID, client_secret=config.TEST_WORKSPACE_APP_SECRET)
+    else:
+        with FileLock(lock_fn):
+            if fn.is_file():
+                data = read_json_file(fn)
+                workspace_path = data["workspace_path"]
+                workspace_id = data["workspace_id"]
+            else:
+                # Set up - uses a pre-created app reg as it has appropriate roles assigned
+                workspace_path, workspace_id = await create_or_get_test_workspace(
+                    auth_type="Manual" if config.TEST_WORKSPACE_APP_ID else "Automatic", verify=verify, pre_created_workspace_id=config.TEST_WORKSPACE_ID, client_id=config.TEST_WORKSPACE_APP_ID, client_secret=config.TEST_WORKSPACE_APP_SECRET)
 
     yield workspace_path, workspace_id
 
     # Tear-down
-    await clean_up_test_workspace(pre_created_workspace_id=pre_created_workspace_id, workspace_path=workspace_path, verify=verify)
+    # Check if there are any other worker locks
+    if len(list(worker_lock.glob("*"))) > 1:
+        (root_tmp_dir / "worker_locks" / (worker_id)).unlink(missing_ok=True)
+        while True:
+            # Wait for other workers to finish
+            if len(list(worker_lock.glob("*"))) > 0:
+                await asyncio.sleep(10)
+            else:
+                break
+    # This is the last worker, clean up the workspace
+    elif len(list(worker_lock.glob("*"))) == 1:
+        await clean_up_test_workspace(pre_created_workspace_id=config.TEST_WORKSPACE_ID, workspace_path=workspace_path, verify=verify)
+        (root_tmp_dir / "worker_locks" / (worker_id)).unlink(missing_ok=True)
 
 
-# Session scope isn't in effect with python-xdist: https://github.com/microsoft/AzureTRE/issues/2868
 @pytest.fixture(scope="session")
 async def setup_test_workspace_and_guacamole_service(setup_test_workspace, verify):
     # Set up
@@ -135,7 +188,6 @@ async def setup_test_workspace_and_guacamole_service(setup_test_workspace, verif
     await clean_up_test_workspace_service(pre_created_workspace_service_id, workspace_service_path, workspace_id, verify)
 
 
-# Session scope isn't in effect with python-xdist: https://github.com/microsoft/AzureTRE/issues/2868
 @pytest.fixture(scope="session")
 async def setup_test_aad_workspace(verify) -> Tuple[str, str, str]:
     pre_created_workspace_id = config.TEST_AAD_WORKSPACE_ID
@@ -164,7 +216,6 @@ async def disable_and_delete_tre_resource(resource_path, verify):
     await disable_and_delete_resource(f'/api{resource_path}', admin_token, verify)
 
 
-# Session scope isn't in effect with python-xdist: https://github.com/microsoft/AzureTRE/issues/2868
 @pytest.fixture(scope="session")
 async def setup_test_airlock_import_review_workspace_and_guacamole_service(verify) -> Tuple[str, str, str, str, str]:
     pre_created_workspace_id = config.TEST_AIRLOCK_IMPORT_REVIEW_WORKSPACE_ID
