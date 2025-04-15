0b0b69cefadb201456868ac57d5ff3ade91ea3b2
Siobhan Baynes <siba@microsoft.com>
2025-04-15 13:26:44 +0000

    Format error message

--- ui/app/src/components/shared/ResourceOperationListItem.tsx
+++ ui/app/src/components/shared/ResourceOperationListItem.tsx
@@ -1,8 +1,9 @@
-import { DefaultPalette, IStackItemStyles, Stack } from "@fluentui/react";
-
+import { DefaultPalette, IStackItemStyles, Stack, Panel, PanelType, Link } from "@fluentui/react";
+import React from "react";
+import stripAnsi from 'strip-ansi';
 interface ResourceOperationListItemProps {
-  header: String;
-  val: String;
+  header: string;
+  val: string;
 }
 
 export const ResourceOperationListItem: React.FunctionComponent<
@@ -14,15 +15,51 @@ export const ResourceOperationListItem: React.FunctionComponent<
       color: DefaultPalette.neutralSecondary,
     },
   };
+  const [isOpen, setIsOpen] = React.useState(false);
+  const cleanupError = (error: string) => {
+    // Remove ANSI escape codes
+    let cleanedError = stripAnsi(error);
+
+    //Replace various vertical bars with new lines
+    cleanedError = cleanedError.replace(/[│╷╵]/g, "\n");
+
+    // Remove leading and trailing whitespace
+    return cleanedError.trim();
+  };
+
+  // Check if the value is an error message
+  const isError = props.val.includes("Error:") || props.val.includes("error:");
+
   return (
     <>
       <Stack wrap horizontal>
         <Stack.Item styles={stackItemStyles} style={{ width: "20%" }}>
           {props.header}
         </Stack.Item>
-        <Stack.Item styles={stackItemStyles} style={{ width: "80%" }}>
-          : {props.val}
-        </Stack.Item>
+
+        {isError ? (
+          <>
+            <Stack.Item styles={stackItemStyles} style={{ width: "80%" }}>
+              <Link onClick={() => setIsOpen(true)}>An error occurred, click to view the error details</Link>
+            </Stack.Item>
+            <Panel
+              headerText="Error Details"
+              isOpen={isOpen}
+              type={PanelType.large}
+              closeButtonAriaLabel="Close"
+              isLightDismiss
+              onDismiss={() => setIsOpen(false)}
+            >
+              <div style={{ width: "100%", whiteSpace: "pre-wrap", fontFamily: "monospace", backgroundColor: "#000", color: "#fff", padding: "10px" }}>
+                {cleanupError(props.val)}
+              </div>
+            </Panel>
+          </>
+        ) : (
+          <Stack.Item styles={stackItemStyles} style={{ width: "80%" }}>
+            {props.val}
+          </Stack.Item>
+        )}
       </Stack>
     </>
   );
