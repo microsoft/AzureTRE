8607134235d9825d1de1b83ad2f23b21b6016bb8
Vijayaraghavan Prathap Sankaramangalam <vsanka@m42.ae>
2025-04-17 13:18:06 +0530

    Use rp_bundle_values_all to pass value to resource processor

--- core/terraform/resource_processor/vmss_porter/locals.tf
+++ core/terraform/resource_processor/vmss_porter/locals.tf
@@ -19,6 +19,7 @@ locals {
     enable_airlock_malware_scanning        = var.enable_airlock_malware_scanning
     airlock_malware_scan_result_topic_name = var.airlock_malware_scan_result_topic_name
     core_api_client_id                     = var.core_api_client_id
+    disable_acr_public_access             = var.disable_acr_public_access
   })
   rp_bundle_values_dic       = [for key in keys(local.rp_bundle_values_all) : "RP_BUNDLE_${key}=${local.rp_bundle_values_all[key]}"]
   rp_bundle_values_formatted = join("\n      ", local.rp_bundle_values_dic)
