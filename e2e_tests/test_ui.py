ab28fbbe08cfd5a8413fcc0466a266a266b3a8d4
Marcus Robinson <marrobi@microsoft.com>
2025-05-20 11:54:17 +0000

    Remove verify.

--- e2e_tests/test_ui.py
+++ e2e_tests/test_ui.py
@@ -12,6 +12,6 @@ async def test_ui(verify) -> None:
     endpoint = f"{config.TRE_URL}"
 
     async with AsyncClient(verify) as client:
-        response = await client.get(endpoint, verify=verify)
+        response = await client.get(endpoint)
         assert response.status_code == 200
         assert "<title>Azure TRE</title>" in response.text
