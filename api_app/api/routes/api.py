e4abdc87af43486ccee18984e9aa9c0b6f06f474
Loïc Barbe | element61 <loic.barbe@element61.be>
2025-07-07 09:44:58 +0000

    Enhance Swagger API with User Endpoints and Refactor Router Inclusion
    - Added user-related API calls to Swagger documentation
    - Refactored API router inclusion for better modularity and maintainability
    - Improved code formatting for enhanced readability and consistency

--- api_app/api/routes/api.py
+++ api_app/api/routes/api.py
@@ -14,17 +14,22 @@ from resources import strings
 
 core_tags_metadata = [
     {"name": "health", "description": "Verify that the TRE is up and running"},
-    {"name": "workspace templates", "description": "**TRE admin** registers and can access templates"},
-    {"name": "workspace service templates", "description": "**TRE admin** registers templates and can access templates"},
-    {"name": "user resource templates", "description": "**TRE admin** registers templates and can access templates"},
+    {"name": "workspace templates",
+        "description": "**TRE admin** registers and can access templates"},
+    {"name": "workspace service templates",
+        "description": "**TRE admin** registers templates and can access templates"},
+    {"name": "user resource templates",
+        "description": "**TRE admin** registers templates and can access templates"},
     {"name": "workspaces", "description": "**TRE admin** administers workspaces, **TRE Users** can view their own workspaces"},
 ]
 
 workspace_tags_metadata = [
-    {"name": "workspaces", "description": " **Workspace Owners and Researchers** can view their own workspaces"},
+    {"name": "workspaces",
+        "description": " **Workspace Owners and Researchers** can view their own workspaces"},
     {"name": "workspace services", "description": "**Workspace Owners** administer workspace services, **Workspace Owners and Researchers** can view services in the workspaces they belong to"},
     {"name": "user resources", "description": "**Researchers** administer and can view their own researchers, **Workspace Owners** can view/update/delete all user resources in their workspaces"},
-    {"name": "shared services", "description": "**TRE administratiors** administer shared services"}
+    {"name": "shared services",
+        "description": "**TRE administratiors** administer shared services"}
 ]
 
 # Root
@@ -38,27 +43,42 @@ core_router = APIRouter(prefix=config.API_PREFIX)
 core_router.include_router(health.router, tags=["health"])
 core_router.include_router(ping.router, tags=["health"])
 core_router.include_router(metadata.router, tags=["metadata"])
-core_router.include_router(workspace_templates.workspace_templates_admin_router, tags=["workspace templates"])
-core_router.include_router(workspace_service_templates.workspace_service_templates_core_router, tags=["workspace service templates"])
-core_router.include_router(user_resource_templates.user_resource_templates_core_router, tags=["user resource templates"])
-core_router.include_router(shared_service_templates.shared_service_templates_core_router, tags=["shared service templates"])
-core_router.include_router(shared_services.shared_services_router, tags=["shared services"])
+core_router.include_router(
+    workspace_templates.workspace_templates_admin_router, tags=["workspace templates"])
+core_router.include_router(workspace_service_templates.workspace_service_templates_core_router, tags=[
+                           "workspace service templates"])
+core_router.include_router(user_resource_templates.user_resource_templates_core_router, tags=[
+                           "user resource templates"])
+core_router.include_router(shared_service_templates.shared_service_templates_core_router, tags=[
+                           "shared service templates"])
+core_router.include_router(
+    shared_services.shared_services_router, tags=["shared services"])
 core_router.include_router(operations.operations_router, tags=["operations"])
-core_router.include_router(workspaces.workspaces_core_router, tags=["workspaces"])
-core_router.include_router(workspaces.workspaces_shared_router, tags=["workspaces"])
-core_router.include_router(migrations.migrations_core_router, tags=["migrations"])
+core_router.include_router(
+    workspaces.workspaces_core_router, tags=["workspaces"])
+core_router.include_router(
+    workspaces.workspaces_shared_router, tags=["workspaces"])
+core_router.include_router(
+    migrations.migrations_core_router, tags=["migrations"])
 core_router.include_router(costs.costs_core_router, tags=["costs"])
 core_router.include_router(costs.costs_workspace_router, tags=["costs"])
 core_router.include_router(requests.router, tags=["requests"])
-core_router.include_router(workspace_users.workspaces_users_shared_router, tags=["users"])
+core_router.include_router(
+    workspace_users.workspaces_users_shared_router, tags=["users"])
 
 if config.USER_MANAGEMENT_ENABLED:
-    core_router.include_router(workspace_users.workspaces_users_admin_router, tags=["users"])
+    core_router.include_router(
+        workspace_users.workspaces_users_admin_router, tags=["users"])
+    core_router.include_router(
+        workspace_users.workspaces_users_admin_router, tags=["assignable-users"])
+    core_router.include_router(
+        workspace_users.workspaces_users_admin_router, tags=["roles"])
 
 core_swagger_router = APIRouter()
 swagger_disabled_router = APIRouter()
 
-openapi_definitions: DefaultDict[str, Optional[Dict[str, Any]]] = defaultdict(lambda: None)
+openapi_definitions: DefaultDict[str, Optional[Dict[str, Any]]] = defaultdict(
+    lambda: None)
 
 
 @core_swagger_router.get("/openapi.json", include_in_schema=False, name="core_openapi")
@@ -100,16 +120,22 @@ async def get_swagger(request: Request):
 
 @swagger_disabled_router.get("/docs", include_in_schema=False, name="swagger_disabled")
 async def get_disabled_swagger():
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=strings.SWAGGER_DISABLED)
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                        detail=strings.SWAGGER_DISABLED)
 
 
 # Workspace API
 workspace_router = APIRouter(prefix=config.API_PREFIX)
-workspace_router.include_router(workspaces.workspaces_shared_router, tags=["workspaces"])
-workspace_router.include_router(workspaces.workspace_services_workspace_router, tags=["workspace services"])
-workspace_router.include_router(workspaces.user_resources_workspace_router, tags=["user resources"])
-workspace_router.include_router(costs.costs_workspace_router, tags=["workspace costs"])
-workspace_router.include_router(airlock.airlock_workspace_router, tags=["airlock"])
+workspace_router.include_router(
+    workspaces.workspaces_shared_router, tags=["workspaces"])
+workspace_router.include_router(
+    workspaces.workspace_services_workspace_router, tags=["workspace services"])
+workspace_router.include_router(
+    workspaces.user_resources_workspace_router, tags=["user resources"])
+workspace_router.include_router(
+    costs.costs_workspace_router, tags=["workspace costs"])
+workspace_router.include_router(
+    airlock.airlock_workspace_router, tags=["airlock"])
 
 workspace_swagger_router = APIRouter()
 workspace_swagger_disabled_router = APIRouter()
@@ -171,7 +197,8 @@ async def get_workspace_swagger(workspace_id, request: Request, workspace_repo=D
 
 @workspace_swagger_disabled_router.get("/workspaces/{workspace_id}/docs", include_in_schema=False, name="workspace_swagger_disabled")
 async def get_disabled_workspace_swagger():
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=strings.SWAGGER_DISABLED)
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                        detail=strings.SWAGGER_DISABLED)
 
 
 if config.ENABLE_SWAGGER:
