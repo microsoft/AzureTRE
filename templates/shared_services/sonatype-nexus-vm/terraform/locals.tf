9a1611d6f0a7aae1e9f06b1c4ce269d839e6e758
dusan-ilic-mhra <Dusan.Ilic@mhra.gov.uk>
2025-07-10 13:40:02 +0000

    Added new FQDNs for certificate authority on Nexus

--- templates/shared_services/sonatype-nexus-vm/terraform/locals.tf
+++ templates/shared_services/sonatype-nexus-vm/terraform/locals.tf
@@ -3,7 +3,7 @@ locals {
   core_resource_group_name        = "rg-${var.tre_id}"
   nexus_allowed_fqdns             = "pypi.org,*.pypi.org,files.pythonhosted.org,security.ubuntu.com,archive.ubuntu.com,keyserver.ubuntu.com,repo.anaconda.com,*.docker.com,*.docker.io,conda.anaconda.org,azure.archive.ubuntu.com,packages.microsoft.com,repo.almalinux.org,download-ib01.fedoraproject.org,cran.r-project.org,cloud.r-project.org"
   nexus_allowed_fqdns_list        = distinct(compact(split(",", replace(local.nexus_allowed_fqdns, " ", ""))))
-  workspace_vm_allowed_fqdns      = "r3.o.lencr.org,x1.c.lencr.org,e5.o.lencr.org,e5.i.lencr.org,e6.o.lencr.org,e6.i.lencr.org"
+  workspace_vm_allowed_fqdns      = "r3.o.lencr.org,x1.c.lencr.org,e5.o.lencr.org,e5.i.lencr.org,e6.o.lencr.org,e6.i.lencr.org,e5.c.lencr.org,e6.c.lencr.org"
   workspace_vm_allowed_fqdns_list = distinct(compact(split(",", replace(local.workspace_vm_allowed_fqdns, " ", ""))))
   storage_account_name            = lower(replace("stg-${var.tre_id}", "-", ""))
   tre_shared_service_tags = {
