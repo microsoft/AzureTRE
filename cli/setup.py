a790051f8a6ee460ef12dc1b3b6e49255524801f
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2025-07-31 07:41:28 +0000

    update versions

--- cli/setup.py
+++ cli/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 PROJECT = 'azure-tre-cli'
-VERSION = '0.2.5'
+VERSION = '0.2.6'
 
 try:
     long_description = open('README.md', 'rt').read()
