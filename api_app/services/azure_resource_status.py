d45b42f2e8c3b3474d3a71551abe8d0216d7fd5b
hazarathmhra <Hazarath.Boligerla@mhra.gov.uk>
2025-08-12 14:27:18 +0000

    Log statements added

--- api_app/services/azure_resource_status.py
+++ api_app/services/azure_resource_status.py
@@ -16,9 +16,12 @@ def get_azure_resource_status(resource_id):
             vm_instance_view: models.VirtualMachineInstanceView = get_azure_vm_instance_view(resource_name, resource_group_name)
             power_state = None
             if vm_instance_view.statuses is not None:
+                logging.info("statuses object available for VM")
                 power_states = [x for x in vm_instance_view.statuses if x.code is not None and x.code.startswith('PowerState')]
                 if len(power_states) > 0:
+                    logging.info("power_states object available for VM")
                     power_state = power_states[0].display_status
+                    logging.info(power_state)
             return {"powerState": power_state}
     except ResourceNotFoundError:
         logging.warning(f"Unable to query resource status for {resource_id}, as the resource was not found.")
