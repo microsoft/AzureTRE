b791eebaafcf752fa6c0c0154ebefff63a6fd53c
Miguel da Silva <miguel.dasilva@emeal.nttdata.com>
2025-09-30 13:11:33 +0200

    Updating Ubuntu Server VMSS image

--- core/terraform/resource_processor/vmss_porter/main.tf
+++ core/terraform/resource_processor/vmss_porter/main.tf
@@ -128,8 +128,8 @@ resource "azurerm_linux_virtual_machine_scale_set" "vm_linux" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
