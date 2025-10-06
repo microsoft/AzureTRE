691881a8beae5b1fe4dbb1665c42ecfd76bbe3f1
dusan-ilic-mhra <Dusan.Ilic@mhra.gov.uk>
2025-10-06 12:03:41 +0000

    Lowercase privateIpAddress in add_a_records.sh

--- core/terraform/add_a_records.sh
+++ core/terraform/add_a_records.sh
@@ -9,16 +9,16 @@ PRIVATE_IP_SQL=$(az vm list-ip-addresses -g "$DATA_SHARED_RG" -n "$SQL_VM_NAME" 
 # Add DNS Record for SQL Server VM
 az network private-dns record-set a add-record --ipv4-address "$PRIVATE_IP_SQL" --record-set-name "$SQL_RECORD_SET_NAME" --resource-group "$CORE_RESOURCE_GROUP" --zone-name "$SQL_ZONE_NAME"
 
-# Find IP of Synapse SQL Private Endpoint - privateIPAddress needs to be with an uppercase IP because of the newer version of az-cli
+# Find IP of Synapse SQL Private Endpoint - privateIPAddress needs to be with a lowercase IP because of the newer version of az-cli
 NIC_ID_PE_SYNAPSE_SQL=$(az network private-endpoint show --name "$PE_SYNAPSE_SQL" --resource-group "$DATA_SHARED_RG" --query "networkInterfaces[0].id" -o tsv)
-PRIVATE_IP_SYNAPSE_SQL=$(az network nic show --ids "$NIC_ID_PE_SYNAPSE_SQL" --query "ipConfigurations[0].privateIPAddress" -o tsv)
+PRIVATE_IP_SYNAPSE_SQL=$(az network nic show --ids "$NIC_ID_PE_SYNAPSE_SQL" --query "ipConfigurations[0].privateIpAddress" -o tsv)
 
 # Add DNS Record for Synapse SQL Private Endpoint
 az network private-dns record-set a add-record --ipv4-address "$PRIVATE_IP_SYNAPSE_SQL" --record-set-name "$SYNAPSE_SQL_RECORD_SET_NAME" --resource-group "$CORE_RESOURCE_GROUP" --zone-name "$SYNAPSE_ZONE_NAME"
 
-# Find IP of Synapse SQL Ondemand Private Endpoint - privateIPAddress needs to be with an uppercase IP because of the newer version of az-cli
+# Find IP of Synapse SQL Ondemand Private Endpoint - privateIPAddress needs to be with a lowercase IP because of the newer version of az-cli
 NIC_ID_PE_SYNAPSE_SQL_ONDEMAND=$(az network private-endpoint show --name "$PE_SYNAPSE_SQL_ONDEMAND" --resource-group "$DATA_SHARED_RG" --query "networkInterfaces[0].id" -o tsv)
-PRIVATE_IP_SYNAPSE_SQL_ONDEMAND=$(az network nic show --ids "$NIC_ID_PE_SYNAPSE_SQL_ONDEMAND" --query "ipConfigurations[0].privateIPAddress" -o tsv)
+PRIVATE_IP_SYNAPSE_SQL_ONDEMAND=$(az network nic show --ids "$NIC_ID_PE_SYNAPSE_SQL_ONDEMAND" --query "ipConfigurations[0].privateIpAddress" -o tsv)
 
 # Add DNS Record for Synapse Ondemand SQL Private Endpoint
 az network private-dns record-set a add-record --ipv4-address "$PRIVATE_IP_SYNAPSE_SQL_ONDEMAND" --record-set-name "$SYNAPSE_SQL_ONDEMAND_RECORD_SET_NAME" --resource-group "$CORE_RESOURCE_GROUP" --zone-name "$SYNAPSE_ZONE_NAME"
