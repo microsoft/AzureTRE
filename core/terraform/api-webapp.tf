bfc683bf96700d2188039e7db7ff04a26a0e836f
Miguel da Silva <miguel.dasilva@emeal.nttdata.com>
2025-10-14 13:43:21 +0000

    Updating docker image for API

--- core/terraform/api-webapp.tf
+++ core/terraform/api-webapp.tf
@@ -82,7 +82,7 @@ resource "azurerm_linux_web_app" "api" {
     ftps_state                                    = "Disabled"
 
     application_stack {
-      docker_image_name = "${local.docker_registry_server}/${var.api_image_repository}:${local.version}"
+      docker_image_name = "${var.api_image_repository}:${local.version}"
     }
 
     cors {
