e5408635918c51c1c2468bc2aabdb0dec25769d9
James Chapman <196318169+JC-wk@users.noreply.github.com>
2025-10-16 11:01:07 +0100

    add loading spinner to shared services page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,18 +11,19 @@ ENHANCEMENTS:
 * Add `make plan-core` command for testing core deployments ([#4029](https://github.com/microsoft/AzureTRE/issues/4029))
 * Streamline Azure bootstrap by replacing legacy dependency checks with `bootstrap_azure_env.sh` ([#2993](https://github.com/microsoft/AzureTRE/issues/2993))
 * Create Azure provider registration script ([#2993](https://github.com/microsoft/AzureTRE/issues/4653))
-* Update oauth2-proxy and Tomcat versions to latest in Guacamole container ([#4688](https://github.com/microsoft/AzureTRE/pull/4688)])
+* Update oauth2-proxy and Tomcat versions to latest in Guacamole container ([#4688](https://github.com/microsoft/AzureTRE/pull/4688))
 * Standardize database query parameter handling across repository classes ([#4697](https://github.com/microsoft/AzureTRE/issues/4697))
 
 BUG FIXES:
 * Remove deprecated ms-teams-notification action from E2E test workflows ([#4716](https://github.com/microsoft/AzureTRE/issues/4716))
 * Fix disable public network access for stwebcertsTREID is still flagging in Defender ([#4640](https://github.com/microsoft/AzureTRE/issues/4640))
 * Fix error 'resource with the ID pip-fw-management already exists' during firewall migration ([#4661](https://github.com/microsoft/AzureTRE/issues/4661))
-* Add missing image_gallery_id parameter to porter.yaml for guacamole export review vm ([#4678](https://github.com/microsoft/AzureTRE/pull/4678)]
+* Add missing image_gallery_id parameter to porter.yaml for guacamole export review vm ([#4678](https://github.com/microsoft/AzureTRE/pull/4678))
 * Update Starlette and FastAPI versions ([#4683](https://github.com/microsoft/AzureTRE/pull/4683))
 * Fix recreation of route table associations ([#4666](https://github.com/microsoft/AzureTRE/issues/4666))
 * Fix exit trap error "unexpected EOF while looking for matching `''" in storage_enable_public_access.sh ([#4691](https://github.com/microsoft/AzureTRE/issues/4691))
 * Add tm-azurefd.net to allowed dns list ([#4705](https://github.com/microsoft/AzureTRE/pull/4705))
+* Add Loading spinner to shared services page (UI) ([]())
 
 
 ## 0.25.0 (July 18, 2025)
--- ui/app/src/components/shared/SharedServices.tsx
+++ ui/app/src/components/shared/SharedServices.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Resource } from "../../models/resource";
 import { ResourceCardList } from "../shared/ResourceCardList";
-import { PrimaryButton, Stack } from "@fluentui/react";
+import { PrimaryButton, Stack, Spinner, SpinnerSize } from "@fluentui/react";
 import { ResourceType } from "../../models/resourceType";
 import { SharedService } from "../../models/sharedService";
 import { HttpMethod, useAuthApiCall } from "../../hooks/useAuthApiCall";
@@ -21,13 +21,18 @@ export const SharedServices: React.FunctionComponent<SharedServiceProps> = (
   const [sharedServices, setSharedServices] = useState(
     [] as Array<SharedService>,
   );
+  const [isLoading, setIsLoading] = useState(true);
   const apiCall = useAuthApiCall();
 
   useEffect(() => {
     const getSharedServices = async () => {
-      const ss = (await apiCall(ApiEndpoint.SharedServices, HttpMethod.Get))
-        .sharedServices;
-      setSharedServices(ss);
+      try {
+        const ss = (await apiCall(ApiEndpoint.SharedServices, HttpMethod.Get))
+          .sharedServices;
+        setSharedServices(ss);
+      } finally {
+        setIsLoading(false);
+      }
     };
     getSharedServices();
   }, [apiCall]);
@@ -79,17 +84,28 @@ export const SharedServices: React.FunctionComponent<SharedServiceProps> = (
           </Stack>
         </Stack.Item>
         <Stack.Item>
-          <ResourceCardList
-            resources={sharedServices}
-            updateResource={(r: Resource) =>
-              updateSharedService(r as SharedService)
-            }
-            removeResource={(r: Resource) =>
-              removeSharedService(r as SharedService)
-            }
-            emptyText="This TRE has no shared services."
-            readonly={props.readonly}
-          />
+          {isLoading ? (
+            <div style={{ marginTop: "20px" }}>
+              <Spinner
+                label="Loading Shared Services"
+                ariaLive="assertive"
+                labelPosition="top"
+                size={SpinnerSize.large}
+              />
+            </div>
+          ) : (
+            <ResourceCardList
+              resources={sharedServices}
+              updateResource={(r: Resource) =>
+                updateSharedService(r as SharedService)
+              }
+              removeResource={(r: Resource) =>
+                removeSharedService(r as SharedService)
+              }
+              emptyText="This TRE has no shared services."
+              readonly={props.readonly}
+            />
+          )}
         </Stack.Item>
       </Stack>
     </>
