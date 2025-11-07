4df7f133902135b10de05fa84dee93f2edf11794
James Chapman <196318169+JC-wk@users.noreply.github.com>
2025-11-07 15:00:45 +0000

    Add loading spinner to shared services page (#4718)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -29,6 +29,7 @@ BUG FIXES:
 * Fix exit trap error "unexpected EOF while looking for matching `''" in storage_enable_public_access.sh ([#4691](https://github.com/microsoft/AzureTRE/issues/4691))
 * Add tm-azurefd.net to allowed dns list ([#4705](https://github.com/microsoft/AzureTRE/pull/4705))
 * Fix issue where multiple lists in config.yaml incorrectly caused a validation error ([#4711](https://github.com/microsoft/AzureTRE/pull/4711))
+* Add Loading spinner to shared services page ([#4718](https://github.com/microsoft/AzureTRE/pull/4718))
 * Updated config_schema.json to include missing values. ([#4712](https://github.com/microsoft/AzureTRE/issues/4712))([#4714](https://github.com/microsoft/AzureTRE/issues/4714))
 * Remove workspace upgrade step from databricks template ([#4726](https://github.com/microsoft/AzureTRE/pull/4726))
 * Update Starlette and FastAPI versions ([#4738](https://github.com/microsoft/AzureTRE/pull/4738))
--- ui/app/src/components/shared/SharedServices.tsx
+++ ui/app/src/components/shared/SharedServices.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Resource } from "../../models/resource";
 import { ResourceCardList } from "../shared/ResourceCardList";
-import { PrimaryButton, Stack } from "@fluentui/react";
+import { PrimaryButton, Stack, Spinner, SpinnerSize } from "@fluentui/react";
 import { ResourceType } from "../../models/resourceType";
 import { SharedService } from "../../models/sharedService";
 import { HttpMethod, useAuthApiCall } from "../../hooks/useAuthApiCall";
+import { LoadingState } from "../../models/loadingState";
 import { ApiEndpoint } from "../../models/apiEndpoints";
 import { CreateUpdateResourceContext } from "../../contexts/CreateUpdateResourceContext";
 import { RoleName } from "../../models/roleNames";
@@ -21,13 +22,19 @@ export const SharedServices: React.FunctionComponent<SharedServiceProps> = (
   const [sharedServices, setSharedServices] = useState(
     [] as Array<SharedService>,
   );
+  const [loadingState, setLoadingState] = useState(LoadingState.Loading);
   const apiCall = useAuthApiCall();
 
   useEffect(() => {
     const getSharedServices = async () => {
-      const ss = (await apiCall(ApiEndpoint.SharedServices, HttpMethod.Get))
-        .sharedServices;
-      setSharedServices(ss);
+      try {
+        const ss = (await apiCall(ApiEndpoint.SharedServices, HttpMethod.Get))
+          .sharedServices;
+        setSharedServices(ss);
+        setLoadingState(LoadingState.Ok);
+      } catch (err) {
+        setLoadingState(LoadingState.Error);
+      }
     };
     getSharedServices();
   }, [apiCall]);
@@ -52,46 +59,64 @@ export const SharedServices: React.FunctionComponent<SharedServiceProps> = (
     setSharedServices(ssList);
   };
 
-  return (
-    <>
-      <Stack className="tre-panel">
-        <Stack.Item>
-          <Stack horizontal horizontalAlign="space-between">
-            <h1>Shared Services</h1>
-            {!props.readonly && (
-              <SecuredByRole
-                allowedAppRoles={[RoleName.TREAdmin]}
-                element={
-                  <PrimaryButton
-                    iconProps={{ iconName: "Add" }}
-                    text="Create new"
-                    onClick={() => {
-                      createFormCtx.openCreateForm({
-                        resourceType: ResourceType.SharedService,
-                        onAdd: (r: Resource) =>
-                          addSharedService(r as SharedService),
-                      });
-                    }}
-                  />
-                }
-              />
-            )}
-          </Stack>
-        </Stack.Item>
-        <Stack.Item>
-          <ResourceCardList
-            resources={sharedServices}
-            updateResource={(r: Resource) =>
-              updateSharedService(r as SharedService)
-            }
-            removeResource={(r: Resource) =>
-              removeSharedService(r as SharedService)
-            }
-            emptyText="This TRE has no shared services."
-            readonly={props.readonly}
+  switch (loadingState) {
+    case LoadingState.Ok:
+      return (
+        <Stack className="tre-panel">
+          <Stack.Item>
+            <Stack horizontal horizontalAlign="space-between">
+              <h1>Shared Services</h1>
+              {!props.readonly && (
+                <SecuredByRole
+                  allowedAppRoles={[RoleName.TREAdmin]}
+                  element={
+                    <PrimaryButton
+                      iconProps={{ iconName: "Add" }}
+                      text="Create new"
+                      onClick={() => {
+                        createFormCtx.openCreateForm({
+                          resourceType: ResourceType.SharedService,
+                          onAdd: (r: Resource) =>
+                            addSharedService(r as SharedService),
+                        });
+                      }}
+                    />
+                  }
+                />
+              )}
+            </Stack>
+          </Stack.Item>
+          <Stack.Item>
+            <ResourceCardList
+              resources={sharedServices}
+              updateResource={(r: Resource) =>
+                updateSharedService(r as SharedService)
+              }
+              removeResource={(r: Resource) =>
+                removeSharedService(r as SharedService)
+              }
+              emptyText="This TRE has no shared services."
+              readonly={props.readonly}
+            />
+          </Stack.Item>
+        </Stack>
+      );
+    case LoadingState.Error:
+      return (
+        <div style={{ marginTop: "20px" }}>
+          <span>Error loading shared services</span>
+        </div>
+      );
+    default:
+      return (
+        <div style={{ marginTop: "20px" }}>
+          <Spinner
+            label="Loading Shared Services"
+            ariaLive="assertive"
+            labelPosition="top"
+            size={SpinnerSize.large}
           />
-        </Stack.Item>
-      </Stack>
-    </>
-  );
+        </div>
+      );
+  }
 };
