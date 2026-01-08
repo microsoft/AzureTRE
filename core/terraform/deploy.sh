fe09dd84a40ca5c4df79d07b0e5120fb2592a158
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2026-01-08 14:37:24 +0000

    change comment

--- core/terraform/deploy.sh
+++ core/terraform/deploy.sh
@@ -23,6 +23,10 @@ TS=$(date +"%s")
 PLAN_FILE="${TS}-tre-core.tfplan"
 LOG_FILE="${TS}-tre-core.log"
 
+# Note: Terraform parallelism is increased from the default (10) to 25 to speed up
+# core deployments. This may increase the risk of hitting Azure API rate limits.
+# If deployment reliability issues arise (e.g., frequent throttling), consider
+# lowering the parallelism value.
 # This variables are loaded in for us
 # shellcheck disable=SC2154
 ../../devops/scripts/terraform_wrapper.sh \
@@ -32,11 +36,6 @@ LOG_FILE="${TS}-tre-core.log"
   -n "${TF_VAR_terraform_state_container_name}" \
   -k "${TRE_ID}" \
   -l "${LOG_FILE}" \
-  # Note: Terraform parallelism is increased from the default (10) to 25 to speed up
-  # core infrastructure deployments, which provision many resources. This may increase
-  # the risk of hitting Azure API rate limits and can make error diagnosis harder.
-  # If deployment reliability issues arise (e.g., frequent throttling), consider
-  # lowering the parallelism value.
   -c "terraform plan --parallelism=25 -out ${PLAN_FILE} && \
   terraform apply -input=false -auto-approve --parallelism=25 ${PLAN_FILE} && \
   terraform output -json > ../tre_output.json"
