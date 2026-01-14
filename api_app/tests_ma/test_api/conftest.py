e6164c6315167b90e8d8101bae4cb9e0094f0fcf
Vishal <vishal.goyal7711@gmail.com>
2026-01-14 21:35:57 +0530

    Resolve merge conflict in conftest.py

--- api_app/tests_ma/test_api/conftest.py
+++ api_app/tests_ma/test_api/conftest.py
@@ -1,14 +1,16 @@
 import pytest
 import pytest_asyncio
 
+
 from mock import patch
+
 from fastapi import FastAPI
 from httpx import AsyncClient
 
 from models.domain.authentication import User
 
 
-@pytest.fixture(autouse=True, scope="module")
+@pytest.fixture(autouse=True, scope='module')
 def no_lifespan_events():
     with patch("main.lifespan"):
         yield
@@ -16,15 +18,9 @@ def no_lifespan_events():
 
 @pytest.fixture(autouse=True)
 def no_auth_token():
-    """Overrides validating and decoding tokens for all tests"""
-    with patch(
-        "services.aad_authentication.AccessService.__call__",
-        return_value="token",
-    ):
-        with patch(
-            "services.aad_authentication.AzureADAuthorization._decode_token",
-            return_value="decoded_token",
-        ):
+    """ overrides validating and decoding tokens for all tests"""
+    with patch('services.aad_authentication.AccessService.__call__', return_value="token"):
+        with patch('services.aad_authentication.AzureADAuthorization._decode_token', return_value="decoded_token"):
             yield
 
 
@@ -40,7 +36,7 @@ def create_test_user() -> User:
         name="Test User",
         email="test@user.com",
         roles=[],
-        roleAssignments=[],
+        roleAssignments=[]
     )
 
 
@@ -76,41 +72,67 @@ def create_workspace_airlock_manager_user() -> User:
     return user
 
 
-@pytest.fixture(scope="module")
+def override_get_user():
+    user = create_test_user()
+    user.roles = []
+    user.roleAssignments = [("ab123", "ab124")]
+    return user
+
+
+def get_required_roles(endpoint):
+    dependencies = list(filter(lambda x: hasattr(x.dependency, 'require_one_of_roles'), endpoint.__defaults__))
+    required_roles = dependencies[0].dependency.require_one_of_roles
+    return required_roles
+
+
+@pytest.fixture(scope='module')
 def admin_user():
-    return create_admin_user
-
-
-@pytest.fixture(scope="module")
-def non_admin_user():
-    return create_non_admin_user
-
-
-@pytest.fixture(scope="module")
-def owner_user():
-    return create_workspace_owner_user
-
-
-@pytest.fixture(scope="module")
-def researcher_user():
-    return create_workspace_researcher_user
-
-
-@pytest.fixture(scope="module")
-def airlock_manager_user():
-    return create_workspace_airlock_manager_user
-
-
-@pytest.fixture(scope="module")
-def no_workspace_role_user():
     def inner():
-        return create_test_user()
+        return create_admin_user()
     return inner
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
+def non_admin_user():
+    def inner():
+        return create_non_admin_user()
+    return inner
+
+
+@pytest.fixture(scope='module')
+def owner_user():
+    def inner():
+        return create_workspace_owner_user()
+    return inner
+
+
+@pytest.fixture(scope='module')
+def researcher_user():
+    def inner():
+        return create_workspace_researcher_user()
+    return inner
+
+
+@pytest.fixture(scope='module')
+def airlock_manager_user():
+    def inner():
+        return create_workspace_airlock_manager_user()
+    return inner
+
+
+@pytest.fixture(scope='module')
+def no_workspace_role_user():
+    def inner():
+        user = create_test_user()
+        return user
+    return inner
+
+
+@pytest.fixture(scope='module')
 def app() -> FastAPI:
     from main import get_application
+
+    
     return get_application()
 
 
@@ -122,3 +144,5 @@ async def client(app: FastAPI) -> AsyncClient:
         headers={"Content-Type": "application/json"},
     ) as client:
         yield client
+
+
