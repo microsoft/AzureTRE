dbaa4dcf8a2362a1145c548d29680d95bb7b5c1a
James Chapman <196318169+JC-wk@users.noreply.github.com>
2026-09-08 10:49:19 +0100

    Fix pytest deprecation warnings (#5069)
    * Fix pytest deprecation warnings
    
    * bump api version
    
    * changelog

--- api_app/tests_ma/test_api/test_routes/test_workspace_templates.py
+++ api_app/tests_ma/test_api/test_routes/test_workspace_templates.py
@@ -39,7 +39,8 @@ def workspace_template_without_enriching():
 class TestWorkspaceTemplate:
 
     @pytest.fixture(autouse=True, scope='class')
-    def _prepare(self, app, admin_user):
+    @classmethod
+    def _prepare(cls, app, admin_user):
         app.dependency_overrides[require_tre_user_or_admin] = admin_user
         app.dependency_overrides[require_tre_admin] = admin_user
         yield
